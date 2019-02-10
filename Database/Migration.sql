cabf8349b41d472003f2ef3a67abda37e00d61ee
Edi Wang <Edi.Wang@outlook.com>
2019-02-10 23:02:01 +0800

    update Migration.sql

--- Database/Migration.sql
+++ Database/Migration.sql
@@ -1,4 +1,2 @@
-﻿--INSERT BlogConfiguration (Id, CfgKey, CfgValue, LastModifiedTimeUtc) VALUES (22, 'BloggerAvatarBase64', N'', GETDATE())
-
-ALTER TABLE BlogConfiguration ALTER COLUMN CfgValue NVARCHAR(MAX)
+﻿ALTER TABLE BlogConfiguration ALTER COLUMN CfgValue NVARCHAR(MAX)
 GO
