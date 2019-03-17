3a80bfca124b3fa98d3c9ee9bca36ad5a70ebf92
Edi Wang <Edi.Wang@outlook.com>
2019-03-17 13:00:27 -0700

    Add build script

---
+++ src/Build.ps1
@@ -0,0 +1,9 @@
+Write-Host "Generating Build Number"
+$baseDate = [datetime]"01/01/2000"
+$currentDate = $(Get-Date)
+$interval = NEW-TIMESPAN -Start $baseDate -End $currentDate
+$days = $interval.Days
+# Write-Host "##vso[task.setvariable variable=buildNumber]10.0.$days.1024"
+
+dotnet clean -c Release
+dotnet build -c Release -p:Version=10.0.$days.1024
