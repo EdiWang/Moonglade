2fe6681ff239bbc1faae911e96a7fea82eff882b
Edi Wang <Edi.Wang@outlook.com>
2019-03-24 09:44:42 +0800

    Add TODO

--- src/Build.ps1
+++ src/Build.ps1
@@ -1,6 +1,6 @@
 Write-Host "Generating Build Number"
 $baseDate = [datetime]"01/01/2000"
-$currentDate = $(Get-Date)
+$currentDate = $(Get-Date) # TODO: Use UTC Time
 $interval = NEW-TIMESPAN -Start $baseDate -End $currentDate
 $days = $interval.Days
 # Write-Host "##vso[task.setvariable variable=buildNumber]10.0.$days.1024"
