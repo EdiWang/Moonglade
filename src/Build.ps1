e4f76968a79c1a6c73c6f02d344a10c12787b5dc
Edi Wang <Edi.Wang@outlook.com>
2019-03-26 08:55:10 +0800

    update build version script

--- src/Build.ps1
+++ src/Build.ps1
@@ -1,9 +1,9 @@
 Write-Host "Generating Build Number"
 $baseDate = [datetime]"01/01/2000"
-$currentDate = $(Get-Date) # TODO: Use UTC Time
+$currentDate = $(Get-Date).ToUniversalTime()
 $interval = NEW-TIMESPAN -Start $baseDate -End $currentDate
 $days = $interval.Days
-# Write-Host "##vso[task.setvariable variable=buildNumber]10.0.$days.1024"
+# Write-Host "##vso[task.setvariable variable=buildNumber]10.0.$days"
 
 dotnet clean -c Release
-dotnet build -c Release -p:Version=10.0.$days.1024
+dotnet build -c Release -p:Version=10.0.$days
