762e439ed6c696cd4b01e6126eeb77a3d9739ff7
Edi Wang <Edi.Wang@outlook.com>
2019-04-19 12:57:41 +0800

    add Powershell script for setup .net core sdk

---
+++ DotNetSetup.ps1
@@ -0,0 +1,28 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+$testchoco = powershell choco -v
+if (-not($testchoco)) {
+    Write-Host ""
+    Write-Host "Installing Chocolate for Windows..." -ForegroundColor Green
+    Write-Host "------------------------------------" -ForegroundColor Green
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+else {
+    Write-Host "Choco is already installed, skip installation."
+}
+
+$testdotnet = powershell dotnet --version
+if (-not($testchoco)) {
+    Write-Host ""
+    Write-Host "Installing .NET Core SDK..." -ForegroundColor Green
+    Write-Host "------------------------------------" -ForegroundColor Green
+    choco install dotnetcore-sdk -y
+}
+else {
+    Write-Host ".NET Core SDK is already installed, checking new version..."
+
+    if (!$testdotnet.StartsWith("2.2")) {
+        # Only update 2.2 SDK
+        choco update dotnetcore-sdk -y
+    }
+}
