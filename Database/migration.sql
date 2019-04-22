5b3332277a2e6e03c9305e633f894cf109543628
Edi Wang <Edi.Wang@outlook.com>
2019-04-22 21:42:16 +0800

    Fix broken image

--- Database/migration.sql
+++ Database/migration.sql
@@ -1,4 +1,1 @@
-﻿INSERT BlogConfiguration VALUES(5,'BloggerName','Edi Wang', GETDATE())
-INSERT BlogConfiguration VALUES(99,'GeneralSettings',N'{"SiteTitle":"Edi.Wang","LogoText":"edi.wang","MetaKeyword":"edi.wang, Edi Wang, 汪宇杰","Copyright":"&copy; 2019 - 2019 edi.wang"}', GETDATE())
-
-DELETE FROM BlogConfiguration WHERE CfgKey IN ('SiteTitle', 'MetaKeyword')
+﻿--
