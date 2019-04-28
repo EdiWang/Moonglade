f250970b50fc4fb3a212c4f9d11f202a9d813465
Edi Wang <Edi.Wang@outlook.com>
2019-04-28 15:12:39 +0800

    Update scripts

--- src/Build.ps1
+++ src/Build.ps1
@@ -5,5 +5,14 @@ $interval = NEW-TIMESPAN -Start $baseDate -End $currentDate
 $days = $interval.Days
 # Write-Host "##vso[task.setvariable variable=buildNumber]10.0.$days"
 
+$ErrorActionPreference = 'Continue'
+taskkill /T /F /IM dotnet.exe
+taskkill /T /F /IM iisexpress.exe
+taskkill /T /F /IM iisexpresstray.exe
+taskkill /T /F /IM w3wp.exe
+taskkill /T /F /IM msbuild.exe
+taskkill /T /F /IM vbcscompiler.exe
+iisreset /restart
+
 dotnet clean -c Release
 dotnet build -c Release -p:Version=10.0.$days
