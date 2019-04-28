6bbde98170bb99b366697c4353821679b002676c
Edi Wang <Edi.Wang@outlook.com>
2019-04-28 14:06:14 +0800

    Fix setup script

--- DotNetSetup.ps1
+++ DotNetSetup.ps1
@@ -1,31 +1,32 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
-$testchoco = powershell choco -v
-if (-not($testchoco)) {
+function Check-Command($cmdname) {
+    return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
+}
+
+if (Check-Command -cmdname 'choco') {
+    Write-Host "Choco is already installed, skip installation."
+}
+else {
     Write-Host ""
     Write-Host "Installing Chocolate for Windows..." -ForegroundColor Green
     Write-Host "------------------------------------" -ForegroundColor Green
     Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
-else {
-    Write-Host "Choco is already installed, skip installation."
-}
 
-$testdotnet = powershell dotnet --version
-# this detection doesn't work :(
-if (-not($testdotnet)) {
-    Write-Host ""
-    Write-Host "Installing .NET Core SDK..." -ForegroundColor Green
-    Write-Host "------------------------------------" -ForegroundColor Green
-    choco install dotnetcore-sdk -y
-}
-else {
+if (Check-Command -cmdname 'dotnet') {
     Write-Host ".NET Core SDK is already installed, checking new version..."
-
+    $testdotnet = powershell dotnet --version
     if ($testdotnet.StartsWith("2.2")) {
         # Only update 2.2 SDK
         choco update dotnetcore-sdk -y
     }
+}
+else {
+    Write-Host ""
+    Write-Host "Installing .NET Core SDK..." -ForegroundColor Green
+    Write-Host "------------------------------------" -ForegroundColor Green
+    choco install dotnetcore-sdk -y
 }
 
 Read-Host -Prompt "Setup is done, press [ENTER] to quit."
