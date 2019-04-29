74c4b1bf08a6a286fcff253093420e015b3ef25b
Edi Wang <Edi.Wang@outlook.com>
2019-04-29 13:05:23 +0800

    add regex for PostEditModel

--- src/Moonglade.Web/Models/PostEditViewModel.cs
+++ src/Moonglade.Web/Models/PostEditViewModel.cs
@@ -15,6 +15,7 @@ namespace Moonglade.Web.Models
         public string Title { get; set; }
 
         [Required(ErrorMessage = "Please enter the slug.")]
+        [RegularExpression(@"[a-z0-9\-]+", ErrorMessage = "Only lower case letters and hyphens are allowed.")]
         public string Slug { get; set; }
 
         [JsonIgnore]
@@ -47,6 +48,7 @@ namespace Moonglade.Web.Models
 
         [Required(ErrorMessage = "Please enter language code.")]
         [Display(Name = "Content Language")]
+        [RegularExpression(@"^[a-z]{2}-[a-zA-Z]{2}$", ErrorMessage = "Incorrect language code format. e.g. en-us")]
         public string ContentLanguageCode { get; set; }
 
         public PostEditModel()
