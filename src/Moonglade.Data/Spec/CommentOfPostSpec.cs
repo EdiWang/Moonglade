c2b5abb0acc0c8acb5800e555ef712d4f19f82d3
Edi Wang <Edi.Wang@outlook.com>
2019-05-06 14:12:04 +0800

    Optimize PostCommentList

--- src/Moonglade.Data/Spec/CommentOfPostSpec.cs
+++ src/Moonglade.Data/Spec/CommentOfPostSpec.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Text;
 using Microsoft.EntityFrameworkCore;
 using Moonglade.Data.Entities;
 using Moonglade.Data.Infrastructure;
@@ -10,10 +7,6 @@ namespace Moonglade.Data.Spec
 {
     public sealed class CommentOfPostSpec : BaseSpecification<Comment>
     {
-        //public CommentOfPostSpec(Expression<Func<Comment, bool>> criteria) : base(criteria)
-        //{
-        //}
-
         public CommentOfPostSpec(Guid postId) :base(c => c.PostId == postId &&
                                                    c.IsApproved)
         {
--- src/Moonglade.Web/Startup.cs
+++ src/Moonglade.Web/Startup.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
 using System.Web;
 using Edi.Blog.Pingback;
