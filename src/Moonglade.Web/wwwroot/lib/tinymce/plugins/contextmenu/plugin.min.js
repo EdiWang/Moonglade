22d3f9f251114b124c26689db656579256485d31
Edi Wang <Edi.Wang@outlook.com>
2019-05-09 16:19:39 +0800

    Libman restore for tinymce

--- src/Moonglade.Web/wwwroot/lib/tinymce/plugins/contextmenu/plugin.min.js
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) Tiny Technologies, Inc. All rights reserved.
- * Licensed under the LGPL or a commercial license.
- * For LGPL see License.txt in the project root for license information.
- * For commercial licenses see https://www.tiny.cloud/
- *
- * Version: 5.0.5 (2019-05-09)
- */
-!function(n){"use strict";tinymce.util.Tools.resolve("tinymce.PluginManager").add("contextmenu",function(){n.console.warn("Context menu plugin is now built in to the core editor, please remove it from your editor configuration")}),function o(){}}(window);
