e5fca53a6cff67eabc9facc552756f7475acec16
Edi Wang <Edi.Wang@outlook.com>
2019-05-24 19:24:19 +0800

    Bug fix and refact unit test
    No fubao no die

--- src/Moonglade.Core/Utils.cs
+++ src/Moonglade.Core/Utils.cs
@@ -57,19 +57,33 @@ namespace Moonglade.Core
             return fullPath;
         }
 
-        [Flags]
         public enum UrlScheme
         {
-            Http = 1,
-            Https = 2,
-            All = Http | Https
+            Http,
+            Https,
+            All
         }
 
         public static bool IsValidUrl(this string url, UrlScheme urlScheme = UrlScheme.All)
         {
-            bool isValidUrl = Uri.TryCreate(url, UriKind.Absolute, out var uriResult)
-                              && ((urlScheme.HasFlag(UrlScheme.Https) ? uriResult.Scheme == Uri.UriSchemeHttps : true)
-                              || (urlScheme.HasFlag(UrlScheme.Http) ? uriResult.Scheme == Uri.UriSchemeHttp : true));
+            bool isValidUrl = Uri.TryCreate(url, UriKind.Absolute, out var uriResult);
+            if (!isValidUrl)
+            {
+                return false;
+            }
+
+            switch (urlScheme)
+            {
+                case UrlScheme.All:
+                    isValidUrl &= uriResult.Scheme == Uri.UriSchemeHttps || uriResult.Scheme == Uri.UriSchemeHttp;
+                    break;
+                case UrlScheme.Https:
+                    isValidUrl &= uriResult.Scheme == Uri.UriSchemeHttps;
+                    break;
+                case UrlScheme.Http:
+                    isValidUrl &= uriResult.Scheme == Uri.UriSchemeHttp;
+                    break;
+            }
 
             return isValidUrl;
         }
