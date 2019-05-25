e9a3187892df1edbbfd3c8f04a973143e9ae8ffa
Edi Wang <Edi.Wang@outlook.com>
2019-05-25 15:45:42 +0800

    Minify CSS, fix #176

--- src/Moonglade.Core/Utils.cs
+++ src/Moonglade.Core/Utils.cs
@@ -15,6 +15,19 @@ namespace Moonglade.Core
         public static string AppVersion =>
             Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
 
+        public static string RemoveWhiteSpaceFromStylesheets(string body)
+        {
+            body = Regex.Replace(body, @"[a-zA-Z]+#", "#");
+            body = Regex.Replace(body, @"[\n\r]+\s*", string.Empty);
+            body = Regex.Replace(body, @"\s+", " ");
+            body = Regex.Replace(body, @"\s?([:,;{}])\s?", "$1");
+            body = body.Replace(";}", "}");
+            body = Regex.Replace(body, @"([\s:]0)(px|pt|%|em)", "$1");
+            // Remove comments from CSS
+            body = Regex.Replace(body, @"/\*[\d\D]*?\*/", string.Empty);
+            return body;
+        }
+
         public static string ResolveImageStoragePath(string contentRootPath, string path)
         {
             if (string.IsNullOrWhiteSpace(path))
