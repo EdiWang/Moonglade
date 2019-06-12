b5dcec94424811ae2ab31a0829b0a1f3b078f2da
Edi Wang <Edi.Wang@outlook.com>
2019-06-12 17:59:05 +0800

    Generate manifest.json
    #8

---
+++ src/Moonglade.Web/Controllers/AssetsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moonglade.Configuration.Abstraction;
+using Moonglade.Model.Settings;
+using Moonglade.Web.Models;
+
+namespace Moonglade.Web.Controllers
+{
+    public class AssetsController : MoongladeController
+    {
+        private readonly IBlogConfig _blogConfig;
+
+        public AssetsController(
+            ILogger<AssetsController> logger,
+            IOptions<AppSettings> settings,
+            IBlogConfig blogConfig) : base(logger, settings)
+        {
+            _blogConfig = blogConfig;
+        }
+
+        // Credits: https://github.com/Anduin2017/Blog
+        // [Route("/manifest.json")]
+        public IActionResult Manifest()
+        {
+            var model = new ManifestModel
+            {
+                ShortName = _blogConfig.GeneralSettings.SiteTitle,
+                Name = _blogConfig.GeneralSettings.SiteTitle,
+                StartUrl = "/",
+                Icons = new List<ManifestIcon>()
+                {
+                    // TODO
+                },
+                // BackgroundColor = "#",
+                // ThemeColor = "#",
+                Display = "standalone",
+                Orientation = "portrait"
+            };
+            return Json(model);
+        }
+    }
+}
---
+++ src/Moonglade.Web/Models/ManifestModels.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Moonglade.Web.Models
+{
+    // Credits: https://github.com/Anduin2017/Blog
+    public class ManifestModel
+    {
+        [JsonProperty("short_name")]
+        public string ShortName { get; set; }
+
+        public string Name { get; set; }
+
+        [JsonProperty("start_url")]
+        public string StartUrl { get; set; }
+
+        public List<ManifestIcon> Icons { get; set; }
+
+        [JsonProperty("background_color")]
+        public string BackgroundColor { get; set; }
+
+        [JsonProperty("theme_color")]
+        public string ThemeColor { get; set; }
+
+        public string Display { get; set; }
+        public string Orientation { get; set; }
+    }
+
+    public class ManifestIcon
+    {
+        public string Src { get; set; }
+        public string Sizes { get; set; }
+        public string Type { get; set; }
+    }
+}
