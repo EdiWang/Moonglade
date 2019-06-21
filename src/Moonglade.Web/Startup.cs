3235eb05ea820440f474820a4b8f07cae1e44baa
Edi Wang <Edi.Wang@outlook.com>
2019-06-21 16:37:50 +0800

    Add version endpoint

--- src/Moonglade.Web/Startup.cs
+++ src/Moonglade.Web/Startup.cs
@@ -239,6 +239,13 @@ namespace Moonglade.Web
                     }
                 }
 
+                app.MapWhen(context => context.Request.Path == "/version", builder =>
+                {
+                    builder.Run(async context =>
+                    {
+                        await context.Response.WriteAsync("Moonglade Version: " + Utils.AppVersion, Encoding.UTF8);
+                    });
+                });
                 app.UseMvc(routes =>
                 {
                     routes.MapRoute(
