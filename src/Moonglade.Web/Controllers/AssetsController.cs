c2b0c661c8660844ad57899d15a9b96337fcd7e0
Edi Wang <Edi.Wang@outlook.com>
2019-08-20 22:36:13 +0800

    Update slug year range limitation
    Nothing should exist before Microsoft was founded!

--- src/Moonglade.Web/Controllers/AssetsController.cs
+++ src/Moonglade.Web/Controllers/AssetsController.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moonglade.Configuration.Abstraction;
