ebfd7c8deaa68302aab9e688631d35b4a6792091
Edi Wang <Edi.Wang@outlook.com>
2019-08-20 12:55:02 +0800

    Refact JS code style

--- src/Moonglade.Web/wwwroot/js/app/app-js-bundle.js
+++ src/Moonglade.Web/wwwroot/js/app/app-js-bundle.js
@@ -73,12 +73,12 @@ $(function () {
 });
 var postSlug = {
     registerRatingButtons: function (pid) {
-        $(".btn-ratings").click(function () {
-            ajaxPostWithCSRFToken("/post/like", { postId: pid }, function (data) {
+        $('.btn-ratings').click(function () {
+            ajaxPostWithCSRFToken('/post/like', { postId: pid }, function (data) {
                 if (data.isSuccess) {
-                    var oldVal = parseInt($(".likehits-num").text(), 10);
-                    $(".likehits-num").html(++oldVal);
-                    $(".btn-ratings").attr("disabled", "disabled");
+                    var oldVal = parseInt($('.likehits-num').text(), 10);
+                    $('.likehits-num').html(++oldVal);
+                    $('.btn-ratings').attr('disabled', 'disabled');
                 } else {
                     window.toastr.warning(data.message);
                 }
@@ -87,27 +87,27 @@ var postSlug = {
     },
     resetCaptchaImage: function () {
         d = new Date();
-        $("#img-captcha").attr("src", "/get-captcha-image?" + d.getTime());
+        $('#img-captcha').attr('src', `/get-captcha-image?${d.getTime()}`);
     },
     resizeImages: function () {
-        $(".post-content img").removeAttr("height");
-        $(".post-content img").removeAttr("width");
-        $(".post-content img").addClass("img-fluid img-thumbnail");
+        $('.post-content img').removeAttr('height');
+        $('.post-content img').removeAttr('width');
+        $('.post-content img').addClass('img-fluid img-thumbnail');
     },
     renderCodeHighlighter: function () {
-        $("pre").each(function (i, pre) {
+        $('pre').each(function (i, pre) {
             // Find <pre> that doesn't have a <code> inside it.
-            if ($(pre).find("code")[0] === undefined) {
-                $(pre).wrapInner("<code></code>");
+            if ($(pre).find('code')[0] === undefined) {
+                $(pre).wrapInner('<code></code>');
             }
 
             // For code that can't be automatically detected, fall back to use XML
-            if ($(pre).hasClass("language-markup")) {
-                $(pre).children("code").addClass("lang-xml");
+            if ($(pre).hasClass('language-markup')) {
+                $(pre).children('code').addClass('lang-xml');
             }
         });
 
-        $("pre code").each(function (i, block) {
+        $('pre code').each(function (i, block) {
             hljs.highlightBlock(block);
         });
     }
