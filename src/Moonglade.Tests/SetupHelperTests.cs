de30608a6db291d41b63f915c59dca55fdeb1af8
Edi Wang <Edi.Wang@outlook.com>
2019-10-06 14:11:30 +0800

    Create SetupHelperTests.cs

---
+++ src/Moonglade.Tests/SetupHelperTests.cs
@@ -0,0 +1,21 @@
+﻿using Moonglade.Setup;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moonglade.Tests
+{
+    [TestFixture]
+    public class SetupHelperTests
+    {
+        [Test]
+        public void TestInstanceCreationInvalidConnectionString()
+        {
+            Assert.Throws(typeof(ArgumentNullException), () =>
+            {
+                var helper = new SetupHelper(null);
+            });
+        }
+    }
+}
