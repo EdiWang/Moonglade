ddbf335b69ac44ca80f1210b8205b31fe3eef698
Edi Wang <Edi.Wang@outlook.com>
2019-10-06 13:57:05 +0800

    Create MoongladeControllerTests.cs

---
+++ src/Moonglade.Tests/Web/MoongladeControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moonglade.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moonglade.Tests.Web
+{
+    [TestFixture]
+    public class MoongladeControllerTests
+    {
+        private Mock<ILogger<ControllerBase>> _loggerMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _loggerMock = new Mock<ILogger<ControllerBase>>();
+        }
+
+        [Test]
+        public void TestServerError()
+        {
+            var ctl = new MoongladeController(_loggerMock.Object);
+            var result = ctl.ServerError();
+            Assert.IsInstanceOf(typeof(StatusCodeResult), result);
+            if (result is StatusCodeResult rdResult)
+            {
+                Assert.That(rdResult.StatusCode, Is.EqualTo(500));
+            }
+        }
+    }
+}
