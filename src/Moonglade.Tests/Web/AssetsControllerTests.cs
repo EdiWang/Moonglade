7cc98a61517c75d6ec1469d506bc28d5a6fa033e
Edi Wang <Edi.Wang@outlook.com>
2019-10-06 15:46:20 +0800

    Create AssetsControllerTests.cs

---
+++ src/Moonglade.Tests/Web/AssetsControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moonglade.Configuration.Abstraction;
+using Moonglade.Model.Settings;
+using Moonglade.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moonglade.Tests.Web
+{
+    [TestFixture]
+    public class AssetsControllerTests
+    {
+        private Mock<ILogger<AssetsController>> _loggerMock;
+        private Mock<IOptions<AppSettings>> _appSettingsMock;
+        private Mock<IBlogConfig> _blogConfigMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _loggerMock = new Mock<ILogger<AssetsController>>();
+            _appSettingsMock = new Mock<IOptions<AppSettings>>();
+            _blogConfigMock = new Mock<IBlogConfig>();
+        }
+    }
+}
