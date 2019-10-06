389540100f867458f83753e53febd534da88a3ce
Edi Wang <Edi.Wang@outlook.com>
2019-10-06 14:20:05 +0800

    Update SetupHelperTests.cs

--- src/Moonglade.Tests/SetupHelperTests.cs
+++ src/Moonglade.Tests/SetupHelperTests.cs
@@ -16,6 +16,24 @@ namespace Moonglade.Tests
             {
                 var helper = new SetupHelper(null);
             });
+
+            Assert.Throws(typeof(ArgumentNullException), () =>
+            {
+                var helper = new SetupHelper(string.Empty);
+            });
+
+            Assert.Throws(typeof(ArgumentNullException), () =>
+            {
+                var helper = new SetupHelper(" ");
+            });
+        }
+
+        [Test]
+        public void TestInstanceCreationGoodConnectionString()
+        {
+            var connStr = "Server=(localdb)\\MSSQLLocalDB;Database=moonglade-dev;Trusted_Connection=True;";
+            var helper = new SetupHelper(connStr);
+            Assert.IsTrue(helper.DatabaseConnectionString == connStr);
         }
     }
 }
