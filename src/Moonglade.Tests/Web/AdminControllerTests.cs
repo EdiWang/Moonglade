27338f7bb735fb7a270fac675dd616b451947f9f
Edi Wang <Edi.Wang@outlook.com>
2019-10-06 11:26:38 +0800

    Update AdminControllerTests.cs

--- src/Moonglade.Tests/Web/AdminControllerTests.cs
+++ src/Moonglade.Tests/Web/AdminControllerTests.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moonglade.Web.Authentication;
@@ -31,6 +35,43 @@ namespace Moonglade.Tests.Web
             if (result is RedirectToActionResult rdResult)
             {
                 Assert.That(rdResult.ActionName, Is.EqualTo("Manage"));
+                Assert.That(rdResult.ControllerName, Is.EqualTo("Post"));
+            }
+        }
+
+        [Test]
+        public async Task TestSignOutAAD()
+        {
+            _authenticationSettingsMock.Setup(m => m.Value).Returns(new AuthenticationSettings()
+            {
+                Provider = AuthenticationProvider.AzureAD
+            });
+
+            var mockUrlHelper = new Mock<IUrlHelper>(MockBehavior.Strict);
+            mockUrlHelper.Setup(x => x.Action(It.IsAny<UrlActionContext>()))
+                .Returns("callbackUrl")
+                .Verifiable();
+
+            var ctx = new DefaultHttpContext();
+            var ctl = new AdminController(_loggerMock.Object, _authenticationSettingsMock.Object)
+            {
+                ControllerContext = new ControllerContext { HttpContext = ctx },
+                Url = mockUrlHelper.Object
+            };
+
+            var result = await ctl.SignOut();
+            Assert.IsInstanceOf(typeof(SignOutResult), result);
+        }
+
+        [Test]
+        public void TestSignedOut()
+        {
+            var ctl = new AdminController(_loggerMock.Object, _authenticationSettingsMock.Object);
+            var result = ctl.SignedOut();
+            Assert.IsInstanceOf(typeof(RedirectToActionResult), result);
+            if (result is RedirectToActionResult rdResult)
+            {
+                Assert.That(rdResult.ActionName, Is.EqualTo("Index"));
                 Assert.That(rdResult.ControllerName, Is.EqualTo("Post"));
             }
         }
