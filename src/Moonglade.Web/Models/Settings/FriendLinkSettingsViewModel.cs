a9a3a195d9aeeea6dd43d74f41aa20f82ffaebb5
Edi Wang <Edi.Wang@outlook.com>
2019-10-15 12:56:25 +0800

    Add friendlinks settings page

---
+++ src/Moonglade.Web/Models/Settings/FriendLinkSettingsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Moonglade.Web.Models.Settings
+{
+    public class FriendLinkSettingsViewModelWrap
+    {
+        public FriendLinkSettingsViewModel FriendLinkSettingsViewModel { get; set; }
+
+        public IReadOnlyList<Model.FriendLink> FriendLinks { get; set; }
+    }
+
+    public class FriendLinkSettingsViewModel
+    {
+        [Display(Name = "Show Friend Links Section")]
+        public bool ShowFriendLinksSection { get; set; }
+    }
+}
