a4397619675fa0bc7b3426caf8dd2b5b35e4b3f1
Edi Wang <Edi.Wang@outlook.com>
2019-10-28 15:19:28 +0800

    Use full HTML encode and UTF32 escape emoji #280

--- src/Moonglade.HtmlCodec/IHtmlCodec.cs
+++ src/Moonglade.HtmlCodec/IHtmlCodec.cs
@@ -4,6 +4,6 @@
     {
         string HtmlDecode(string encodedHtml);
 
-        string HtmlEncode(string rawHtml, bool attributeEncode = false);
+        string HtmlEncode(string rawHtml);
     }
 }
---
+++ src/Moonglade.HtmlCodec/MoongladeHtmlCodec.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Web;
+
+namespace Moonglade.HtmlCodec
+{
+    /// <summary>
+    /// Html Encode / Decode that handles emoji correctly
+    /// </summary>
+    public class MoongladeHtmlCodec : IHtmlCodec
+    {
+        public string HtmlDecode(string encodedHtml)
+        {
+            return HttpUtility.HtmlDecode(encodedHtml);
+        }
+
+        public string HtmlEncode(string rawHtml)
+        {
+            return HtmlEncoder.Default.Encode(rawHtml);
+        }
+    }
+}
