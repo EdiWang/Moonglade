f021cbafaffd575142450381e800b83686e44d51
Edi Wang <Edi.Wang@outlook.com>
2019-11-25 19:09:30 +0800

    Code clean up

--- src/Moonglade.Data/Spec/PostSpec.cs
+++ src/Moonglade.Data/Spec/PostSpec.cs
@@ -107,32 +107,4 @@ namespace Moonglade.Data.Spec
 
         }
     }
-
-    public sealed partial class PostInsightsSpec : BaseSpecification<PostEntity>
-    {
-        public PostInsightsSpec(PostInsightsType insightsType, int top) : base(p => !p.PostPublish.IsDeleted && p.PostPublish.IsPublished)
-        {
-            switch (insightsType)
-            {
-                case PostInsightsType.TopRead:
-                    ApplyOrderByDescending(p => p.PostExtension.Hits);
-                    break;
-                case PostInsightsType.TopCommented:
-                    ApplyOrderByDescending(p => p.Comment.Count);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(insightsType), insightsType, null);
-            }
-
-            UseCriteria(p => p.PostPublish.PubDateUtc >= DateTime.UtcNow.AddYears(-1));
-
-            ApplyPaging(0, top);
-        }
-    }
-
-    public enum PostInsightsType
-    {
-        TopRead = 0,
-        TopCommented = 1,
-    }
 }
