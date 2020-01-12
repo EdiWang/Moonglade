b3ffe809bd0fe3ac343dd5e4672280148155df52
Edi Wang <Edi.Wang@outlook.com>
2020-01-12 14:59:34 +0800

    integrate inline-attachment lib with SimpeMDE

--- src/Moonglade.Web/wwwroot/js/app/admin.js
+++ src/Moonglade.Web/wwwroot/js/app/admin.js
@@ -54,6 +54,26 @@ var postEditor = {
                 spellChecker: false,
                 status: false
             });
+
+            inlineAttachment.editors.codemirror4.attach(simplemde.codemirror, {
+                onFileUploadResponse: function (xhr) {
+                    var result = JSON.parse(xhr.responseText),
+                        filename = result[this.settings.jsonFieldName];
+
+                    if (result && filename) {
+                        var newValue;
+                        if (typeof this.settings.urlText === 'function') {
+                            newValue = this.settings.urlText.call(this, filename, result);
+                        } else {
+                            newValue = this.settings.urlText.replace(this.filenameTag, filename);
+                        }
+                        var text = this.editor.getValue().replace(this.lastValue, newValue);
+                        this.editor.setValue(text);
+                        this.settings.onFileUploaded.call(this, filename);
+                    }
+                    return false;
+                }
+            });
         }
     },
     initEvents: function () {
