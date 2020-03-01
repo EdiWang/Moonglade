40b02018dd9069d16baa9d54976d943cd2e6176d
Edi Wang <Edi.Wang@outlook.com>
2020-03-01 17:34:47 +0800

    MoongladeAudit

---
+++ src/Moonglade.Auditing/MoongladeAudit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Moonglade.Auditing
+{
+    public class MoongladeAudit
+    {
+        private readonly ILogger<MoongladeAudit> _logger;
+
+        private readonly IConfiguration _configuration;
+    }
+}
