66599a2c2ab11c1d96664fa8633d157c79492e2f
Edi Wang <Edi.Wang@outlook.com>
2020-03-26 16:12:50 +0800

    Fix UT

--- src/Moonglade.Tests/Core/DateTimeResolverTests.cs
+++ src/Moonglade.Tests/Core/DateTimeResolverTests.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Moonglade.Configuration;
-using Moonglade.Configuration.Abstraction;
 using Moonglade.Core;
-using Moq;
 using NUnit.Framework;
 
 namespace Moonglade.Tests.Core
@@ -12,24 +7,16 @@ namespace Moonglade.Tests.Core
     [TestFixture]
     public class DateTimeResolverTests
     {
-        private Mock<IBlogConfig> _blogConfigMock;
-
         [SetUp]
         public void Setup()
         {
-            _blogConfigMock = new Mock<IBlogConfig>();
         }
 
         [Test]
         public void TestGetDateTimeWithUserTZone()
         {
             var tSpan = "02:51:00";
-            _blogConfigMock.Setup(m => m.GeneralSettings).Returns(new GeneralSettings
-            {
-                TimeZoneUtcOffset = tSpan
-            });
-
-            var resolver = new DateTimeResolver(_blogConfigMock.Object);
+            var resolver = new DateTimeResolver(tSpan);
 
             var utc = new DateTime(2000, 1, 1, 0, 0, 0);
             var dt = resolver.GetDateTimeWithUserTZone(utc);
@@ -41,12 +28,7 @@ namespace Moonglade.Tests.Core
         public void TestGetUtcTimeFromUserTZone()
         {
             var tSpan = "10:55:00";
-            _blogConfigMock.Setup(m => m.GeneralSettings).Returns(new GeneralSettings
-            {
-                TimeZoneUtcOffset = tSpan
-            });
-
-            var resolver = new DateTimeResolver(_blogConfigMock.Object);
+            var resolver = new DateTimeResolver(tSpan);
 
             var dt = resolver.GetUtcTimeFromUserTZone(DateTime.Parse("2000/1/1 10:55:00"));
             var utc = new DateTime(2000, 1, 1, 0, 0, 0);
