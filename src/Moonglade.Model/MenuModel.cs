e0bf76f0bcb07229d339f9172e959f3a99964267
Edi Wang <Edi.Wang@outlook.com>
2020-04-09 19:21:57 +0800

    Create MenuModel.cs

---
+++ src/Moonglade.Model/MenuModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moonglade.Model
+{
+    public class MenuModel
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+
+        public string Icon { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public MenuModel()
+        {
+            Icon = "icon-file-text2";
+        }
+    }
+}
