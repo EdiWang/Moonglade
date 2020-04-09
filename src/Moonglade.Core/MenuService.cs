2d6b721e9803745659eb3480bf91be39f7955ffb
Edi Wang <Edi.Wang@outlook.com>
2020-04-09 19:33:28 +0800

    Create MenuService.cs

---
+++ src/Moonglade.Core/MenuService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moonglade.Data.Entities;
+using Moonglade.Data.Infrastructure;
+using Moonglade.Model.Settings;
+
+namespace Moonglade.Core
+{
+    public class MenuService : MoongladeService
+    {
+        private readonly IRepository<MenuEntity> _menuRepository;
+
+        public MenuService(
+            ILogger<MenuService> logger,
+            IOptions<AppSettings> settings, 
+            IRepository<MenuEntity> menuRepository) : base(logger, settings)
+        {
+            _menuRepository = menuRepository;
+        }
+    }
+}
