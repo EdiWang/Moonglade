de30008ffdec80b6a6de73acd1b7f98db4cc50fc
Edi Wang <Edi.Wang@outlook.com>
2020-04-11 15:39:26 +0800

    Merge Edi.Blog.Pingback.Mvc

---
+++ src/Moonglade.Web/Extensions/AddPingbackHeaderAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Edi.Blog.Pingback.Mvc
+{
+    public class AddPingbackHeaderAttribute : ResultFilterAttribute
+    {
+        private readonly string _pingbackEndpoint;
+
+        public AddPingbackHeaderAttribute(string pingbackEndpoint)
+        {
+            _pingbackEndpoint = pingbackEndpoint;
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            if (!context.HttpContext.Response.Headers.ContainsKey("x-pingback"))
+            {
+                context.HttpContext.Response.Headers.Add("x-pingback",
+                    new[]
+                    {
+                        $"{context.HttpContext.Request.Scheme}://{context.HttpContext.Request.Host}/{_pingbackEndpoint}"
+                    });
+            }
+
+            base.OnResultExecuting(context);
+        }
+    }
+}
