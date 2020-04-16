ce6676bfbe4e096c2c0845f98ed91b99a71af3bc
Edi Wang <Edi.Wang@outlook.com>
2020-04-16 20:05:08 +0800

    Create CreateEditMenuRequest.cs

---
+++ src/Moonglade.Web/Models/Settings/CreateEditMenuRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Moonglade.Web.Models.Settings
+{
+    public class CreateMenuRequest
+    {
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+
+        public string Icon { get; set; }
+
+        public int DisplayOrder { get; set; }
+    }
+
+    public class EditMenuRequest : CreateMenuRequest
+    {
+        public Guid Id { get; }
+
+        public EditMenuRequest(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
