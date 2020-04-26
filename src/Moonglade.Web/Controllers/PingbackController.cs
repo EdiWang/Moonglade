9bf4a3688daedadcd0f33d497044993d978b0664
Edi Wang <Edi.Wang@outlook.com>
2020-04-26 13:56:11 +0800

    Move ManagePingback.cshtml

--- src/Moonglade.Web/Controllers/PingbackController.cs
+++ src/Moonglade.Web/Controllers/PingbackController.cs
@@ -50,7 +50,7 @@ namespace Moonglade.Web.Controllers
         public async Task<IActionResult> Manage()
         {
             var response = await _pingbackService.GetReceivedPingbacksAsync();
-            return response.IsSuccess ? View(response.Item) : ServerError();
+            return response.IsSuccess ? View("~/Views/Admin/ManagePingback.cshtml", response.Item) : ServerError();
         }
 
         [Authorize]
