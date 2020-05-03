7369329f9a021c5e246d1bcaa947dc48e759883f
Edi Wang <Edi.Wang@outlook.com>
2020-05-03 11:24:01 +0800

    IExportable interface

---
+++ src/Moonglade.Core/IExportable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Moonglade.Core
+{
+    public interface IExportable
+    {
+        XDocument GetXmlDocument();
+    }
+}
