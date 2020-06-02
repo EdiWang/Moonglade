1fa003b4af60b23795b49b98f4275f24b02700be
Edi Wang <Edi.Wang@outlook.com>
2020-06-02 11:07:52 +0800

    MetaDescription EF entity

--- src/Moonglade.Data.Entities/CustomPageEntity.cs
+++ src/Moonglade.Data.Entities/CustomPageEntity.cs
@@ -7,6 +7,7 @@ namespace Moonglade.Data.Entities
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string RouteName { get; set; }
+        public string MetaDescription { get; set; }
         public string HtmlContent { get; set; }
         public string CssContent { get; set; }
         public bool HideSidebar { get; set; }
