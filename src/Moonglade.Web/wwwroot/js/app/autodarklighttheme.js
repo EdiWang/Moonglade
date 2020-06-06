187403354dda9e39b18a140fbd75be992038a075
Edi Wang <Edi.Wang@outlook.com>
2020-06-06 08:45:45 +0800

    Dark mode code refact #440

--- src/Moonglade.Web/wwwroot/js/app/autodarklighttheme.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-if (window.matchMedia && window.themeModeSwitcher && supportLightSwitch) {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        themeModeSwitcher.useDarkMode();
-    }
-    else {
-        themeModeSwitcher.useLightMode();
-    }
-}
