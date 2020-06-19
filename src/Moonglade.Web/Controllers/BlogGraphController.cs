afcfb160ab1c53e476cc5fbb2e1a1c8da0353b33
Edi Wang <Edi.Wang@outlook.com>
2020-06-19 21:15:02 +0800

    Tags API

--- src/Moonglade.Web/Controllers/BlogGraphController.cs
+++ src/Moonglade.Web/Controllers/BlogGraphController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moonglade.Core;
+using Moonglade.Model;
 using Moonglade.Web.Authentication;
 
 namespace Moonglade.Web.Controllers
@@ -16,15 +17,27 @@ namespace Moonglade.Web.Controllers
     public class BlogGraphController : ControllerBase
     {
         private readonly ILogger<BlogGraphController> _logger;
+        private readonly TagService _tagService;
 
-        public BlogGraphController(ILogger<BlogGraphController> logger)
+        public BlogGraphController(
+            ILogger<BlogGraphController> logger, 
+            TagService tagService)
         {
             _logger = logger;
+            _tagService = tagService;
         }
 
+        [HttpGet("version")]
         public string Version()
         {
             return Utils.AppVersion;
+        }
+
+        [HttpGet("tags")]
+        public async Task<IEnumerable<Tag>> Tags()
+        {
+            var response = await _tagService.GetAllAsync();
+            return response.Item;
         }
     }
 }
