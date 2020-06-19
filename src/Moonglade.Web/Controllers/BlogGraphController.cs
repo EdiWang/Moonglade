f99a816a48949d100b23897c5f265f52ed5ef1ad
Edi Wang <Edi.Wang@outlook.com>
2020-06-19 21:42:32 +0800

    categories api

--- src/Moonglade.Web/Controllers/BlogGraphController.cs
+++ src/Moonglade.Web/Controllers/BlogGraphController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moonglade.Core;
@@ -18,26 +19,50 @@ namespace Moonglade.Web.Controllers
     {
         private readonly ILogger<BlogGraphController> _logger;
         private readonly TagService _tagService;
+        private readonly CategoryService _categoryService;
 
         public BlogGraphController(
-            ILogger<BlogGraphController> logger, 
-            TagService tagService)
+            ILogger<BlogGraphController> logger,
+            TagService tagService,
+            CategoryService categoryService)
         {
             _logger = logger;
             _tagService = tagService;
+            _categoryService = categoryService;
         }
 
         [HttpGet("version")]
-        public string Version()
+        public ActionResult<string> Version()
         {
             return Utils.AppVersion;
         }
 
         [HttpGet("tags")]
-        public async Task<IEnumerable<Tag>> Tags()
+        [ProducesResponseType(typeof(IEnumerable<Tag>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Tags()
         {
             var response = await _tagService.GetAllAsync();
-            return response.Item;
+            if (response.IsSuccess)
+            {
+                return Ok(response.Item);
+            }
+
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+
+        [HttpGet("categories")]
+        [ProducesResponseType(typeof(IEnumerable<Category>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Categories()
+        {
+            var response = await _categoryService.GetAllAsync();
+            if (response.IsSuccess)
+            {
+                return Ok(response.Item);
+            }
+
+            return StatusCode(StatusCodes.Status500InternalServerError);
         }
     }
 }
