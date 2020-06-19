1d79408dfda790ba88654cdda0f8b61f9e848e5c
Edi Wang <Edi.Wang@outlook.com>
2020-06-19 21:10:04 +0800

    add version api

--- src/Moonglade.Web/Controllers/BlogGraphController.cs
+++ src/Moonglade.Web/Controllers/BlogGraphController.cs
@@ -5,12 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Moonglade.Core;
 using Moonglade.Web.Authentication;
 
 namespace Moonglade.Web.Controllers
 {
     [Authorize(AuthenticationSchemes = ApiKeyAuthenticationOptions.DefaultScheme)]
-    [Route("api/[controller]")]
+    [Route("api/graph")]
     [ApiController]
     public class BlogGraphController : ControllerBase
     {
@@ -19,6 +20,11 @@ namespace Moonglade.Web.Controllers
         public BlogGraphController(ILogger<BlogGraphController> logger)
         {
             _logger = logger;
+        }
+
+        public string Version()
+        {
+            return Utils.AppVersion;
         }
     }
 }
