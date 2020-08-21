4ab015bfaa16357c8088ca033ff279ae5a695e67
Edi Wang <Edi.Wang@outlook.com>
2020-08-21 15:22:31 +0800

    Update SyndicationFeedGeneratorTests.cs

--- src/Moonglade.Tests/SyndicationFeedGeneratorTests.cs
+++ src/Moonglade.Tests/SyndicationFeedGeneratorTests.cs
@@ -22,14 +22,39 @@ namespace Moonglade.Tests
                 HeadTitle = "996 ICU",
                 HeadDescription = "Work 996 and get into ICU",
                 Copyright = "(c) 2020 996.icu",
-                Generator = "Fubao Generator v9.9.6",
+                Generator = "Fubao Generator",
                 FeedItemCollection = itemCollection,
                 TrackBackUrl = "https://996.icu/trackback",
-                MaxContentLength = 0
+                MaxContentLength = 996,
+                GeneratorVersion = "9.9.6"
             };
 
-            var path = Path.Join(Path.GetTempPath(), $"Moonglade-UT-{Guid.NewGuid()}.xml");
+            var path = Path.Join(Path.GetTempPath(), $"Moonglade-UT-RSS-{Guid.NewGuid()}.xml");
             await rw.WriteRss20FileAsync(path);
+
+            Assert.IsTrue(File.Exists(path));
+        }
+
+        [Test]
+        public async Task TestAtom10EmptyCollection()
+        {
+            var itemCollection = new List<SimpleFeedItem>();
+
+            var rw = new SyndicationFeedGenerator
+            {
+                HostUrl = "https://996.icu",
+                HeadTitle = "996 ICU",
+                HeadDescription = "Work 996 and get into ICU",
+                Copyright = "(c) 2020 996.icu",
+                Generator = "Fubao Generator",
+                FeedItemCollection = itemCollection,
+                TrackBackUrl = "https://996.icu/trackback",
+                MaxContentLength = 996,
+                GeneratorVersion = "9.9.6"
+            };
+
+            var path = Path.Join(Path.GetTempPath(), $"Moonglade-UT-ATOM-{Guid.NewGuid()}.xml");
+            await rw.WriteAtom10FileAsync(path);
 
             Assert.IsTrue(File.Exists(path));
         }
