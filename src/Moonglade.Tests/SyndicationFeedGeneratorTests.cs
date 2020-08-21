7293dc582e2767daa9143432f6f0afbb6df3e0fd
Edi Wang <Edi.Wang@outlook.com>
2020-08-21 16:02:41 +0800

    Update SyndicationFeedGeneratorTests.cs

--- src/Moonglade.Tests/SyndicationFeedGeneratorTests.cs
+++ src/Moonglade.Tests/SyndicationFeedGeneratorTests.cs
@@ -12,7 +12,7 @@ namespace Moonglade.Tests
     public class SyndicationFeedGeneratorTests
     {
         [Test]
-        public async Task  TestRss20EmptyCollection()
+        public async Task TestRss20EmptyCollection()
         {
             var itemCollection = new List<SimpleFeedItem>();
 
@@ -24,6 +24,28 @@ namespace Moonglade.Tests
                 Copyright = "(c) 2020 996.icu",
                 Generator = "Fubao Generator",
                 FeedItemCollection = itemCollection,
+                TrackBackUrl = "https://996.icu/trackback",
+                MaxContentLength = 996,
+                GeneratorVersion = "9.9.6"
+            };
+
+            var path = Path.Join(Path.GetTempPath(), $"Moonglade-UT-RSS-{Guid.NewGuid()}.xml");
+            await rw.WriteRss20FileAsync(path);
+
+            Assert.IsTrue(File.Exists(path));
+        }
+
+        [Test]
+        public async Task TestRss20WithCollection()
+        {
+            var rw = new SyndicationFeedGenerator
+            {
+                HostUrl = "https://996.icu",
+                HeadTitle = "996 ICU",
+                HeadDescription = "Work 996 and get into ICU",
+                Copyright = "(c) 2020 996.icu",
+                Generator = "Fubao Generator",
+                FeedItemCollection = GetFeedItems(),
                 TrackBackUrl = "https://996.icu/trackback",
                 MaxContentLength = 996,
                 GeneratorVersion = "9.9.6"
@@ -57,6 +79,61 @@ namespace Moonglade.Tests
             await rw.WriteAtom10FileAsync(path);
 
             Assert.IsTrue(File.Exists(path));
+        }
+
+        [Test]
+        public async Task TestAtom10WithCollection()
+        {
+            var itemCollection = new List<SimpleFeedItem>();
+
+            var rw = new SyndicationFeedGenerator
+            {
+                HostUrl = "https://996.icu",
+                HeadTitle = "996 ICU",
+                HeadDescription = "Work 996 and get into ICU",
+                Copyright = "(c) 2020 996.icu",
+                Generator = "Fubao Generator",
+                FeedItemCollection = GetFeedItems(),
+                TrackBackUrl = "https://996.icu/trackback",
+                MaxContentLength = 996,
+                GeneratorVersion = "9.9.6"
+            };
+
+            var path = Path.Join(Path.GetTempPath(), $"Moonglade-UT-ATOM-{Guid.NewGuid()}.xml");
+            await rw.WriteAtom10FileAsync(path);
+
+            Assert.IsTrue(File.Exists(path));
+        }
+
+        private IList<SimpleFeedItem> GetFeedItems()
+        {
+            var itemCollection = new List<SimpleFeedItem>
+            {
+                new SimpleFeedItem
+                {
+                    Author = "J Ma",
+                    Title = "996 is Fubao",
+                    AuthorEmail = "admin@996.icu",
+                    Categories = new List<string>{ "Hard work" },
+                    Description = "You young people need fen dou",
+                    Id = "996",
+                    Link = "https://996.icu/fubao",
+                    PubDateUtc = DateTime.Now
+                },
+                new SimpleFeedItem
+                {
+                    Author = "G Ni",
+                    Title = "Cheating funds from zero to hero",
+                    AuthorEmail = "copy-paste@from.github",
+                    Categories = new List<string> { "Independent Development" },
+                    Description = "Nation's Proud",
+                    Id = "251",
+                    Link = "https://404.com/no-such-thing",
+                    PubDateUtc = DateTime.Now
+                }
+            };
+
+            return itemCollection;
         }
     }
 }
