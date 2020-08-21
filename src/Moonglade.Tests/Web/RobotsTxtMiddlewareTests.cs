7252218c0922c272504a90a056f694f9be079477
Edi Wang <Edi.Wang@outlook.com>
2020-08-21 14:09:09 +0800

    Update RobotsTxtMiddlewareTests.cs

--- src/Moonglade.Tests/Web/RobotsTxtMiddlewareTests.cs
+++ src/Moonglade.Tests/Web/RobotsTxtMiddlewareTests.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Primitives;
+using Moonglade.Configuration;
+using Moonglade.Configuration.Abstraction;
 using Moonglade.Web.Middleware;
 using Moq;
 using NUnit.Framework;
@@ -26,6 +29,52 @@ namespace Moonglade.Tests.Web
             var middleware = new RobotsTxtMiddleware(RequestDelegate);
 
             await middleware.Invoke(httpContextMock.Object, null);
+
+            Assert.Pass();
+        }
+
+        [Test]
+        public async Task TestRobotsTxtRequestPathWithContent()
+        {
+            var blogConfigMock = new Mock<IBlogConfig>();
+            blogConfigMock.Setup(c => c.AdvancedSettings).Returns(new AdvancedSettings
+            {
+                RobotsTxtContent = "996"
+            });
+
+            static Task RequestDelegate(HttpContext context) => Task.CompletedTask;
+            var middleware = new RobotsTxtMiddleware(RequestDelegate);
+
+            var ctx = new DefaultHttpContext();
+            ctx.Response.Body = new MemoryStream();
+            ctx.Request.Path = "/robots.txt";
+
+            await middleware.Invoke(ctx, blogConfigMock.Object);
+
+            Assert.AreEqual("text/plain", ctx.Response.ContentType);
+
+            Assert.Pass();
+        }
+
+        [Test]
+        public async Task TestRobotsTxtRequestPathNoContent()
+        {
+            var blogConfigMock = new Mock<IBlogConfig>();
+            blogConfigMock.Setup(c => c.AdvancedSettings).Returns(new AdvancedSettings
+            {
+                RobotsTxtContent = string.Empty
+            });
+
+            static Task RequestDelegate(HttpContext context) => Task.CompletedTask;
+            var middleware = new RobotsTxtMiddleware(RequestDelegate);
+
+            var ctx = new DefaultHttpContext();
+            ctx.Response.Body = new MemoryStream();
+            ctx.Request.Path = "/robots.txt";
+
+            await middleware.Invoke(ctx, blogConfigMock.Object);
+
+            Assert.AreEqual(StatusCodes.Status404NotFound, ctx.Response.StatusCode);
 
             Assert.Pass();
         }
