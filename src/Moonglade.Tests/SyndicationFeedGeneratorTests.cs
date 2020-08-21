9d2b94657f71344179f513d9cc3d01454d5d251d
Edi Wang <Edi.Wang@outlook.com>
2020-08-21 15:17:11 +0800

    Create SyndicationFeedGeneratorTests.cs

---
+++ src/Moonglade.Tests/SyndicationFeedGeneratorTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Moonglade.Syndication;
+using NUnit.Framework;
+
+namespace Moonglade.Tests
+{
+    [TestFixture]
+    public class SyndicationFeedGeneratorTests
+    {
+        [Test]
+        public async Task  TestRss20EmptyCollection()
+        {
+            var itemCollection = new List<SimpleFeedItem>();
+
+            var rw = new SyndicationFeedGenerator
+            {
+                HostUrl = "https://996.icu",
+                HeadTitle = "996 ICU",
+                HeadDescription = "Work 996 and get into ICU",
+                Copyright = "(c) 2020 996.icu",
+                Generator = "Fubao Generator v9.9.6",
+                FeedItemCollection = itemCollection,
+                TrackBackUrl = "https://996.icu/trackback",
+                MaxContentLength = 0
+            };
+
+            var path = Path.Join(Path.GetTempPath(), $"Moonglade-UT-{Guid.NewGuid()}.xml");
+            await rw.WriteRss20FileAsync(path);
+
+            Assert.IsTrue(File.Exists(path));
+        }
+    }
+}
