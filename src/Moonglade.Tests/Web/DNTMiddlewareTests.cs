ab60f34333e93ec6cec8773a74a75c76c9a0b2e2
Edi Wang <Edi.Wang@outlook.com>
2020-08-21 13:45:39 +0800

    Create DNTMiddlewareTests.cs

---
+++ src/Moonglade.Tests/Web/DNTMiddlewareTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using Moonglade.Web.Middleware;
+using Moq;
+using NUnit.Framework;
+
+namespace Moonglade.Tests.Web
+{
+    [TestFixture]
+    public class DNTMiddlewareTests
+    {
+        [Test]
+        public async Task TestDNTHeader()
+        {
+            var headersArray = new Dictionary<string, StringValues> { { "DNT", "1" } };
+
+            var reqMock = new Mock<HttpRequest>();
+            reqMock.SetupGet(r => r.Headers).Returns(new HeaderDictionary(headersArray));
+
+            var httpContextMock = new Mock<HttpContext>();
+            httpContextMock.Setup(c => c.Request).Returns(reqMock.Object);
+            httpContextMock.Setup(c => c.Items).Returns(new Dictionary<object, object>());
+
+            static Task RequestDelegate(HttpContext context) => Task.CompletedTask;
+            var middleware = new DNTMiddleware(RequestDelegate);
+
+            await middleware.Invoke(httpContextMock.Object);
+
+            Assert.AreEqual(true, httpContextMock.Object.Items["DNT"]);
+        }
+    }
+}
