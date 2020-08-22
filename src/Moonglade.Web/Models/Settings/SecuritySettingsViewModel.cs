30e6856f287bcdb46aa7446c0178e34e12db965d
Edi Wang <Edi.Wang@outlook.com>
2020-08-22 10:33:17 +0800

    Create SecuritySettingsViewModel.cs

---
+++ src/Moonglade.Web/Models/Settings/SecuritySettingsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Moonglade.Web.Models.Settings
+{
+    public class SecuritySettingsViewModel
+    {
+        public bool WarnExternalLink { get; set; }
+        public bool AllowScriptsInCustomPage { get; set; }
+        public bool ShowAdminLoginButton { get; set; }
+        public bool EnablePostRawEndpoint { get; set; }
+    }
+}
