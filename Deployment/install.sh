be65d6b2c3b2952e26fb88636ea8cf3abc483d9c
Anduin Xue <anduin@aiursoft.com>
2020-09-17 16:03:50 +0800

    Update install.sh

--- Deployment/install.sh
+++ Deployment/install.sh
@@ -84,7 +84,7 @@ register_service()
     KillSignal=SIGINT
     Environment=\"ASPNETCORE_ENVIRONMENT=Production\"
     Environment=\"DOTNET_PRINT_TELEMETRY_MESSAGE=false\"
-    Environment=\"DOTNET_CLI_TELEMETRY_OPTOUT=false\"
+    Environment=\"DOTNET_CLI_TELEMETRY_OPTOUT=1\"
     Environment=\"ASPNETCORE_FORWARDEDHEADERS_ENABLED=true\"
 
     [Install]
