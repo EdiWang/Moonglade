fd7810fda3bfddfb4d07eb8a6ea91c112afce65c
Edi Wang <Edi.Wang@outlook.com>
2020-09-18 13:12:44 +0800

    Create CacheDivisionKeys.cs

---
+++ src/Moonglade.Model/CacheDivisionKeys.cs
@@ -0,0 +1,10 @@
+﻿namespace Moonglade.Model
+{
+    public static class CacheDivisionKeys
+    {
+        public const string General = nameof(General);
+        public const string Post = nameof(Post);
+        public const string Page = nameof(Page);
+        public const string PostCountCategory = nameof(PostCountCategory);
+    }
+}
