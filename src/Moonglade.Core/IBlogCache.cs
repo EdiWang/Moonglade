a2be35f53cd646d843a535be730d21c4da90b18d
Edi Wang <Edi.Wang@outlook.com>
2020-09-18 12:52:54 +0800

    Create IBlogCache.cs

---
+++ src/Moonglade.Core/IBlogCache.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Moonglade.Core
+{
+    public interface IBlogCache
+    {
+        Dictionary<string, IList<string>> CacheDivision { get; }
+        TItem GetOrCreate<TItem>(string divisionKey, string key, Func<ICacheEntry, TItem> factory);
+        Task<TItem> GetOrCreateAsync<TItem>(string divisionKey, string key, Func<ICacheEntry, Task<TItem>> factory);
+        void AddToDivision(string divisionKey, IEnumerable<string> cacheKeys = null);
+        string AddToDivision(string divisionKey, string cacheKey);
+        void RemoveAllCache();
+        void RemoveCache(string divisionKey);
+        void Remove(string key);
+    }
+}
