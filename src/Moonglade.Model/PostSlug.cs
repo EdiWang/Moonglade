cd12fed623c3cb87e48da3e25eff26d7c3126e11
Edi Wang <Edi.Wang@outlook.com>
2020-09-28 15:16:29 +0800

    refact code

--- src/Moonglade.Model/PostSlug.cs
+++ src/Moonglade.Model/PostSlug.cs
@@ -2,21 +2,16 @@
 
 namespace Moonglade.Model
 {
-    public class PostSlug
+    public class PostSlug : Post
     {
-        public string Title { get; set; }
         public string Abstract { get; set; }
-        public DateTime PubDateUtc { get; set; }
         public DateTime? LastModifyOnUtc { get; set; }
         public string Content { get; set; }
         public int Hits { get; set; }
         public int Likes { get; set; }
         public Guid PostId { get; set; }
-        public bool CommentEnabled { get; set; }
         public int CommentCount { get; set; }
         public bool IsExposedToSiteMap { get; set; }
         public string LangCode { get; set; }
-        public Category[] Categories { get; set; }
-        public Tag[] Tags { get; set; }
     }
 }
