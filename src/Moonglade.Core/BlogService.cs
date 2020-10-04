359f2a756ea17ccf2ea51027d50242a4aa58842a
Edi Wang <Edi.Wang@outlook.com>
2020-10-04 09:16:10 +0800

    remove response wrapper

--- src/Moonglade.Core/BlogService.cs
+++ src/Moonglade.Core/BlogService.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
-using Edi.Practice.RequestResponseModel;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Moonglade.Model;
 using Moonglade.Model.Settings;
 
 namespace Moonglade.Core
@@ -20,59 +15,6 @@ namespace Moonglade.Core
         {
             if (null != settings) AppSettings = settings.Value;
             if (null != logger) Logger = logger;
-        }
-
-        public Response TryExecute(Func<Response> func, [CallerMemberName] string callerMemberName = "", object keyParameter = null)
-        {
-            try
-            {
-                return func();
-            }
-            catch (Exception e)
-            {
-                Logger.LogError(e, $"Error executing {callerMemberName}({keyParameter})");
-                return new FailedResponse((int)FaultCode.GeneralException, e.Message);
-            }
-        }
-
-        public Response<T> TryExecute<T>(Func<Response<T>> func, [CallerMemberName] string callerMemberName = "", object keyParameter = null)
-        {
-            try
-            {
-                return func();
-            }
-            catch (Exception e)
-            {
-                Logger.LogError(e, $"Error executing {callerMemberName}({keyParameter})");
-                return new FailedResponse<T>((int)FaultCode.GeneralException, e.Message);
-            }
-        }
-
-        public async Task<Response> TryExecuteAsync(Func<Task<Response>> func, [CallerMemberName] string callerMemberName = "", object keyParameter = null)
-        {
-            try
-            {
-                return await func();
-            }
-            catch (Exception e)
-            {
-                Logger.LogError(e, $"Error executing {callerMemberName}({keyParameter})");
-                return new FailedResponse((int)FaultCode.GeneralException, e.Message);
-            }
-        }
-
-        public async Task<Response<T>> TryExecuteAsync<T>(Func<Task<Response<T>>> func,
-            [CallerMemberName] string callerMemberName = "", object keyParameter = null)
-        {
-            try
-            {
-                return await func();
-            }
-            catch (Exception e)
-            {
-                Logger.LogError(e, $"Error executing {callerMemberName}({keyParameter})");
-                return new FailedResponse<T>((int)FaultCode.GeneralException, e.Message);
-            }
         }
     }
 }
