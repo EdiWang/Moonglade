3112e1d4966f1cedb7c7ccdcc424e962d4124467
Edi Wang <Edi.Wang@outlook.com>
2020-10-11 09:53:08 +0800

    Create SiteMapSettings.cs

---
+++ src/Moonglade.Model/Settings/SiteMapSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Moonglade.Model.Settings
+{
+    public class SiteMapSettings
+    {
+        public string UrlSetNamespace { get; set; }
+
+        public Dictionary<string, string> ChangeFreq { get; set; }
+    }
+}
