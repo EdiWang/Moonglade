71461a38645bc3db975f10e39134a974ff06b9c3
Edi Wang <Edi.Wang@outlook.com>
2020-10-15 11:08:27 +0800

    prefer canonicalUrl for sitemap

--- src/Moonglade.Web/Controllers/SearchController.cs
+++ src/Moonglade.Web/Controllers/SearchController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Moonglade.Configuration.Abstraction;
 using Moonglade.Core;
 using Moonglade.Model;
 using Moonglade.Model.Settings;
@@ -48,14 +49,22 @@ namespace Moonglade.Web.Controllers
         }
 
         [Route("sitemap.xml")]
-        public async Task<IActionResult> SiteMap()
+        public async Task<IActionResult> SiteMap([FromServices] IBlogConfig blogConfig)
         {
             var siteMapDataFile = Path.Join($"{SiteDataDirectory}", $"{Constants.SiteMapFileName}");
             if (!System.IO.File.Exists(siteMapDataFile))
             {
                 Logger.LogInformation($"SiteMap file not found, writing new file on {siteMapDataFile}");
 
-                await _searchService.WriteSiteMapFileAsync(SiteRootUrl, SiteDataDirectory);
+                var url = SiteRootUrl;
+                var canonicalUrl = blogConfig.GeneralSettings.CanonicalPrefix;
+                if (!string.IsNullOrWhiteSpace(canonicalUrl))
+                {
+                    url = canonicalUrl;
+                }
+
+                await _searchService.WriteSiteMapFileAsync(url, SiteDataDirectory);
+
                 if (!System.IO.File.Exists(siteMapDataFile))
                 {
                     Logger.LogError("SiteMap file still not found, what the heck?!");
