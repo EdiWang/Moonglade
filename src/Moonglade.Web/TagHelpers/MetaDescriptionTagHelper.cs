2239f40cf2ddce359c4e3fa0807f9f4f5b3b18f5
Edi Wang <Edi.Wang@outlook.com>
2020-10-17 14:27:04 +0800

    Create MetaDescriptionTagHelper.cs

---
+++ src/Moonglade.Web/TagHelpers/MetaDescriptionTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Moonglade.Web.TagHelpers
+{
+    [HtmlTargetElement("metadesc", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class MetaDescriptionTagHelper : TagHelper
+    {
+        public string Description { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "meta";
+            output.Attributes.SetAttribute("description", Description.Trim());
+        }
+    }
+}
