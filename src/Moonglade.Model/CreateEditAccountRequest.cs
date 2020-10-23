94adb0f4c6ee3617c4d5773f16839b18b6fe7c60
Edi Wang <Edi.Wang@outlook.com>
2020-10-23 15:25:11 +0800

    Create CreateEditAccountRequest.cs

---
+++ src/Moonglade.Model/CreateEditAccountRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moonglade.Model
+{
+    public class CreateAccountRequest
+    {
+        public string Username { get; set; }
+
+        public string ClearPassword { get; set; }
+    }
+
+    public class EditAccountRequest : CreateAccountRequest
+    {
+        public Guid Id { get; }
+
+        public EditAccountRequest(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
