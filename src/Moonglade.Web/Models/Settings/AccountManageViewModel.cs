4301c1a1a26a56943b180d7c4b8523240fe8ddcb
Edi Wang <Edi.Wang@outlook.com>
2020-10-23 20:23:32 +0800

    Create AccountManageViewModel.cs

---
+++ src/Moonglade.Web/Models/Settings/AccountManageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moonglade.Model;
+
+namespace Moonglade.Web.Models.Settings
+{
+    public class AccountManageViewModel
+    {
+        public AccountEditViewModel AccountEditViewModel { get; set; }
+
+        public IReadOnlyList<Account> Accounts { get; set; }
+
+        public AccountManageViewModel()
+        {
+            Accounts = new List<Account>();
+        }
+    }
+
+    public class AccountEditViewModel
+    {
+        [HiddenInput]
+        public Guid Id { get; set; }
+
+        [Display(Name = "New Password")]
+        [MaxLength(32)]
+        public string NewPassword { get; set; }
+    }
+}
