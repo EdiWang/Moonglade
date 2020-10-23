5b25472a82c5dd4a931f7239aacbce09a6622f86
Edi Wang <Edi.Wang@outlook.com>
2020-10-23 15:01:50 +0800

    Create LocalAccountService.cs

---
+++ src/Moonglade.Core/LocalAccountService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Moonglade.Auditing;
+using Moonglade.Data.Entities;
+using Moonglade.Data.Infrastructure;
+
+namespace Moonglade.Core
+{
+    public class LocalAccountService : BlogService
+    {
+        private readonly IRepository<LocalAccountEntity> _accountRepository;
+        private readonly IBlogAudit _blogAudit;
+
+        public LocalAccountService(
+            ILogger<LocalAccountService> logger,
+            IRepository<LocalAccountEntity> accountRepository, 
+            IBlogAudit blogAudit) : base(logger)
+        {
+            _accountRepository = accountRepository;
+            _blogAudit = blogAudit;
+        }
+    }
+}
