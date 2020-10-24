776e54925cff17d1cc68eaed6e28a3a607c8efd1
Edi Wang <Edi.Wang@outlook.com>
2020-10-24 18:30:57 +0800

    reset password

--- src/Moonglade.Web/Models/Settings/AccountManageViewModel.cs
+++ src/Moonglade.Web/Models/Settings/AccountManageViewModel.cs
@@ -18,13 +18,15 @@ namespace Moonglade.Web.Models.Settings
 
     public class AccountEditViewModel
     {
+        [Required]
         [Display(Name = "Username")]
         [MinLength(2), MaxLength(32)]
         [RegularExpression("[a-z0-9]+")]
         public string Username { get; set; }
 
+        [Required]
         [Display(Name = "Password")]
-        [MaxLength(32)]
+        [MinLength(8), MaxLength(32)]
         [DataType(DataType.Password)]
         [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$", ErrorMessage = "Password must be minimum eight characters, at least one letter and one number")]
         public string Password { get; set; }
