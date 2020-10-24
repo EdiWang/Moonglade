2d22a6740562620210e1a5f069bb6d94b5716f0f
Edi Wang <Edi.Wang@outlook.com>
2020-10-24 17:16:02 +0800

    fix error message

--- src/Moonglade.Web/wwwroot/js/app/csrf.js
+++ src/Moonglade.Web/wwwroot/js/app/csrf.js
@@ -1,22 +1,37 @@
-﻿var csrfFieldName = "CSRF-TOKEN-MOONGLADE-FORM";
+﻿var csrfFieldName = 'CSRF-TOKEN-MOONGLADE-FORM';
 function makeCSRFExtendedData(data) {
     var options = {};
-    options[csrfFieldName] = $("input[name=" + csrfFieldName + "]").val();
+    options[csrfFieldName] = $(`input[name=${csrfFieldName}]`).val();
     var extData = $.extend(data, options);
     return extData;
 }
 
 function ajaxPostWithCSRFToken(url, pData, funcSuccess) {
     var options = {
-        type: "POST",
+        type: 'POST',
         url: url,
         headers: {},
         data: makeCSRFExtendedData(pData),
         success: function (data) {
             funcSuccess(data);
         },
-        dataType: "json"
+        statusCode: {
+            404: function (responseObject, textStatus, jqXHR) {
+                toastr.error('Endpoint not found.');
+            },
+            409: function (responseObject, textStatus, jqXHR) {
+                var json = responseObject.responseJSON;
+                var errorMessage = 'Error(s):\n\r';
+
+                Object.keys(json).forEach(function (k) {
+                    errorMessage += (k + ': ' + json[k]) + '\n\r';
+                });
+
+                toastr.error(errorMessage);
+            }
+        },
+        dataType: 'json'
     };
-    options.headers[csrfFieldName] = $("input[name=" + csrfFieldName + "]").val();
+    options.headers[csrfFieldName] = $(`input[name=${csrfFieldName}]`).val();
     $.ajax(options);
 }
