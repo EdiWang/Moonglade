02cbe96e37c157cc7e2dce2162443a739328e7ae
Edi Wang <Edi.Wang@outlook.com>
2020-10-31 10:27:24 +0800

    Update StatisticsControllerTests.cs

--- src/Moonglade.Tests/Web/StatisticsControllerTests.cs
+++ src/Moonglade.Tests/Web/StatisticsControllerTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moonglade.Core;
 using Moonglade.Web.Controllers;
@@ -35,6 +36,38 @@ namespace Moonglade.Tests.Web
             var ctl = new StatisticsController(_statisticsMock.Object);
             var result = await ctl.Like(Guid.Empty);
             Assert.IsInstanceOf(typeof(BadRequestObjectResult), result);
+        }
+
+        [Test]
+        public async Task TestHitDNTEnabled()
+        {
+            var httpContextMock = new Mock<HttpContext>();
+            httpContextMock.Setup(c => c.Items).Returns(
+                new Dictionary<object, object> { { "DNT", true } });
+
+            var ctl = new StatisticsController(_statisticsMock.Object)
+            {
+                ControllerContext = { HttpContext = httpContextMock.Object }
+            };
+
+            var result = await ctl.Hit(Guid.NewGuid());
+            Assert.IsInstanceOf(typeof(OkResult), result);
+        }
+
+        [Test]
+        public async Task TestLikeDNTEnabled()
+        {
+            var httpContextMock = new Mock<HttpContext>();
+            httpContextMock.Setup(c => c.Items).Returns(
+                new Dictionary<object, object> { { "DNT", true } });
+
+            var ctl = new StatisticsController(_statisticsMock.Object)
+            {
+                ControllerContext = { HttpContext = httpContextMock.Object }
+            };
+
+            var result = await ctl.Like(Guid.NewGuid());
+            Assert.IsInstanceOf(typeof(OkResult), result);
         }
     }
 }
