80ead8ebd8d03be80b1300d502a1c4320e547765
Edi Wang <Edi.Wang@outlook.com>
2020-12-03 14:37:18 +0800

    Update AzureQuickDeploy.ps1

--- Deployment/AzureQuickDeploy.ps1
+++ Deployment/AzureQuickDeploy.ps1
@@ -62,6 +62,7 @@ else {
 }
 
 # Confirmation
+Clear-Host
 Write-Host ""
 Write-Host "Your Moonglade will be deployed to [$rsgName] in [$regionName] under Azure subscription [$subscriptionName]. Please confirm before continue." -ForegroundColor Green
 if ($useLinuxPlanWithDocker) {
@@ -74,7 +75,7 @@ if ($createCDN) {
 Read-Host -Prompt "Press [ENTER] to continue, [CTRL + C] to cancel"
 
 # Select Subscription
-az account set --subscription $subscriptionName
+$echo = az account set --subscription $subscriptionName
 Write-Host "Selected Azure Subscription: " $subscriptionName -ForegroundColor Cyan
 
 # Resource Group
@@ -82,7 +83,7 @@ Write-Host "Preparing Resource Group" -ForegroundColor Green
 $rsgExists = az group exists -n $rsgName
 if ($rsgExists -eq 'false') {
     Write-Host "Creating Resource Group"
-    az group create -l $regionName -n $rsgName
+    $echo = az group create -l $regionName -n $rsgName
 }
 
 # App Service Plan
@@ -93,10 +94,10 @@ $planExists = $planCheck.Length -gt 0
 if (!$planExists) {
     Write-Host "Creating App Service Plan"
     if ($useLinuxPlanWithDocker) {
-        az appservice plan create -n $aspName -g $rsgName --is-linux --sku S1 --location $regionName
+        $echo = az appservice plan create -n $aspName -g $rsgName --is-linux --sku S1 --location $regionName
     }
     else {
-        az appservice plan create -n $aspName -g $rsgName --sku S1 --location $regionName
+        $echo = az appservice plan create -n $aspName -g $rsgName --sku S1 --location $regionName
     }
 }
 
@@ -109,13 +110,14 @@ if (!$appExists) {
     Write-Host "Creating Web App"
     if ($useLinuxPlanWithDocker) {
         Write-Host "Using Linux Plan with Docker image from 'ediwang/moonglade', this deployment will be ready to run."
-        az webapp create -g $rsgName -p $aspName -n $webAppName --deployment-container-image-name ediwang/moonglade
+        $echo = az webapp create -g $rsgName -p $aspName -n $webAppName --deployment-container-image-name ediwang/moonglade
     }
     else {
         Write-Host "Using Windows Plan with deployment method as not set, you need to build and deploy the code yourself."
-        az webapp create -g $rsgName -p $aspName -n $webAppName
+        $echo = az webapp create -g $rsgName -p $aspName -n $webAppName
+        $echo = az webapp config set -g $rsgName -n $webAppName --net-framework-version v5.0
     }
-    az webapp config set -g $rsgName -n $webAppName --always-on true --use-32bit-worker-process false --http20-enabled true
+    $echo = az webapp config set -g $rsgName -n $webAppName --always-on true --use-32bit-worker-process false --http20-enabled true
 }
 
 $createdApp = az webapp list --query "[?name=='$webAppName']" | ConvertFrom-Json
@@ -132,14 +134,14 @@ $storageAccountCheck = az storage account list --query "[?name=='$storageAccount
 $storageAccountExists = $storageAccountCheck.Length -gt 0
 if (!$storageAccountExists) {
     Write-Host "Creating Storage Account"
-    az storage account create --name $storageAccountName --resource-group $rsgName --location $regionName --sku Standard_LRS --kind StorageV2
+    $echo = az storage account create --name $storageAccountName --resource-group $rsgName --location $regionName --sku Standard_LRS --kind StorageV2
 }
 
 $storageConn = az storage account show-connection-string -g $rsgName -n $storageAccountName | ConvertFrom-Json
 $storageContainerExists = az storage container exists --name $storageContainerName --connection-string $storageConn.connectionString | ConvertFrom-Json
 if (!$storageContainerExists.exists) {
     Write-Host "Creating storage container"
-    az storage container create --name $storageContainerName --connection-string $storageConn.connectionString --public-access container
+    $echo = az storage container create --name $storageContainerName --connection-string $storageConn.connectionString --public-access container
 }
 
 if ($createCDN) {
@@ -150,11 +152,11 @@ if ($createCDN) {
     $cdnProfileExists = $cdnProfileCheck.Length -gt 0
     if (!$cdnProfileExists) {
         Write-Host "Creating CDN Profile"
-        az cdn profile create --name $cdnProfileName --resource-group $rsgName --location $regionName --sku Standard_Microsoft
+        $echo = az cdn profile create --name $cdnProfileName --resource-group $rsgName --location $regionName --sku Standard_Microsoft
 
         Write-Host "Creating CDN Endpoint"
         $storageOrigion = "$storageAccountName.blob.core.windows.net"
-        az cdn endpoint create -g $rsgName -n $cdnEndpointName --profile-name $cdnProfileName --origin $storageOrigion --origin-host-header $storageOrigion --enable-compression
+        $echo = az cdn endpoint create -g $rsgName -n $cdnEndpointName --profile-name $cdnProfileName --origin $storageOrigion --origin-host-header $storageOrigion --enable-compression
     }
 }
 
@@ -165,18 +167,18 @@ $sqlServerCheck = az sql server list --query "[?name=='$sqlServerName']" | Conve
 $sqlServerExists = $sqlServerCheck.Length -gt 0
 if (!$sqlServerExists) {
     Write-Host "Creating SQL Server"
-    az sql server create --name $sqlServerName --resource-group $rsgName --location $regionName --admin-user $sqlServerUsername --admin-password $sqlServerPassword
+    $echo = az sql server create --name $sqlServerName --resource-group $rsgName --location $regionName --admin-user $sqlServerUsername --admin-password $sqlServerPassword
 
     Write-Host "Setting Firewall to Allow Azure Connection"
     # When both starting IP and end IP are set to 0.0.0.0, the firewall is only opened for other Azure resources.
-    az sql server firewall-rule create --resource-group $rsgName --server $sqlServerName --name AllowAllIps --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
+    $echo = az sql server firewall-rule create --resource-group $rsgName --server $sqlServerName --name AllowAllIps --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
 }
 
 $sqlDbCheck = az sql db list --resource-group $rsgName --server $sqlServerName --query "[?name=='$sqlDatabaseName']" | ConvertFrom-Json
 $sqlDbExists = $sqlDbCheck.Length -gt 0
 if (!$sqlDbExists) {
     Write-Host "Creating SQL Database"
-    az sql db create --resource-group $rsgName --server $sqlServerName --name $sqlDatabaseName --service-objective S0 --backup-storage-redundancy Local
+    $echo = az sql db create --resource-group $rsgName --server $sqlServerName --name $sqlDatabaseName --service-objective S0 --backup-storage-redundancy Local
     Write-Host "SQL Server Password: $sqlServerPassword" -ForegroundColor Yellow
 }
 
@@ -187,29 +189,30 @@ Write-Host "Updating Configuration" -ForegroundColor Green
 Write-Host "Setting SQL Database Connection String"
 $sqlConnStrTemplate = az sql db show-connection-string -s $sqlServerName -n $sqlDatabaseName -c ado.net --auth-type SqlPassword
 $sqlConnStr = $sqlConnStrTemplate.Replace("<username>", $sqlServerUsername).Replace("<password>", $sqlServerPassword)
-az webapp config connection-string set -g $rsgName -n $webAppName -t SQLAzure --settings MoongladeDatabase=$sqlConnStr
+$echo = az webapp config connection-string set -g $rsgName -n $webAppName -t SQLAzure --settings MoongladeDatabase=$sqlConnStr
 
 Write-Host "Adding Blob Storage Connection String"
 $scon = $storageConn.connectionString
 if ($useLinuxPlanWithDocker) {
-    az webapp config appsettings set -g $rsgName -n $webAppName --settings ImageStorage__Provider=azurestorage
-    az webapp config appsettings set -g $rsgName -n $webAppName --settings ImageStorage__AzureStorageSettings__ConnectionString=$scon
-    az webapp config appsettings set -g $rsgName -n $webAppName --settings ImageStorage__AzureStorageSettings__ContainerName=$storageContainerName
+    $echo = az webapp config appsettings set -g $rsgName -n $webAppName --settings ImageStorage__Provider=azurestorage
+    $echo = az webapp config appsettings set -g $rsgName -n $webAppName --settings ImageStorage__AzureStorageSettings__ConnectionString=$scon
+    $echo = az webapp config appsettings set -g $rsgName -n $webAppName --settings ImageStorage__AzureStorageSettings__ContainerName=$storageContainerName
 }
 else {
-    az webapp config appsettings set -g $rsgName -n $webAppName --settings ImageStorage:Provider=azurestorage
-    az webapp config appsettings set -g $rsgName -n $webAppName --settings ImageStorage:AzureStorageSettings:ConnectionString=$scon
-    az webapp config appsettings set -g $rsgName -n $webAppName --settings ImageStorage:AzureStorageSettings:ContainerName=$storageContainerName
+    $echo = az webapp config appsettings set -g $rsgName -n $webAppName --settings ImageStorage:Provider=azurestorage
+    $echo = az webapp config appsettings set -g $rsgName -n $webAppName --settings ImageStorage:AzureStorageSettings:ConnectionString=$scon
+    $echo = az webapp config appsettings set -g $rsgName -n $webAppName --settings ImageStorage:AzureStorageSettings:ContainerName=$storageContainerName
 }
 
 if ($createCDN) {
+    Write-Host "Configuring CDN endpoint for Image Storage"
     if ($useLinuxPlanWithDocker){
-        az webapp config appsettings set -g $rsgName -n $webAppName --settings ImageStorage__CDNSettings__EnableCDNRedirect=true
-        az webapp config appsettings set -g $rsgName -n $webAppName --settings ImageStorage__CDNSettings__CDNEndpoint="https://$cdnEndpointName.azureedge.net/$storageContainerName"
+        $echo = az webapp config appsettings set -g $rsgName -n $webAppName --settings ImageStorage__CDNSettings__EnableCDNRedirect=true
+        $echo = az webapp config appsettings set -g $rsgName -n $webAppName --settings ImageStorage__CDNSettings__CDNEndpoint="https://$cdnEndpointName.azureedge.net/$storageContainerName"
     }
     else{
-        az webapp config appsettings set -g $rsgName -n $webAppName --settings ImageStorage:CDNSettings:EnableCDNRedirect=true
-        az webapp config appsettings set -g $rsgName -n $webAppName --settings ImageStorage:CDNSettings:CDNEndpoint="https://$cdnEndpointName.azureedge.net/$storageContainerName"
+        $echo = az webapp config appsettings set -g $rsgName -n $webAppName --settings ImageStorage:CDNSettings:EnableCDNRedirect=true
+        $echo = az webapp config appsettings set -g $rsgName -n $webAppName --settings ImageStorage:CDNSettings:CDNEndpoint="https://$cdnEndpointName.azureedge.net/$storageContainerName"
     }
     
     Write-Host "It can take up to 10 minutes for endpoint '$cdnEndpointName.azureedge.net' settings to propagate." -ForegroundColor Yellow
