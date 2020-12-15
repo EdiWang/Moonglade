8b54ea517c331e5b8f8c604f676d2469ca9f842b
Edi Wang <Edi.Wang@outlook.com>
2020-12-15 09:34:44 +0800

    Create OpenSearchTagHelper.cs

---
+++ src/Moonglade.Web/TagHelpers/OpenSearchTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Moonglade.Web.TagHelpers
+{
+    [HtmlTargetElement("opensearch", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class OpenSearchTagHelper : TagHelper
+    {
+        public string Title { get; set; }
+        public string Href { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "link";
+            output.Attributes.SetAttribute("type", "application/opensearchdescription+xml");
+            output.Attributes.SetAttribute("rel", "search");
+            output.Attributes.SetAttribute("title", Title.Trim());
+            output.Attributes.SetAttribute("href", Href.Trim());
+        }
+    }
+}
