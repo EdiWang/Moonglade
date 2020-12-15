6448c47297ba521d3aea7c362720cdbe002cba83
Edi Wang <Edi.Wang@outlook.com>
2020-12-15 09:52:57 +0800

    PubDateTagHelper

---
+++ src/Moonglade.Web/TagHelpers/PubDateTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Moonglade.DateTimeOps;
+
+namespace Moonglade.Web.TagHelpers
+{
+    [HtmlTargetElement("pubdate", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class PubDateTagHelper : TagHelper
+    {
+        public DateTime? PubDateUtc { get; set; }
+
+        public IDateTimeResolver DateTimeResolver { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "time";
+            output.Attributes.SetAttribute("title", $"GMT {PubDateUtc}");
+            output.Attributes.SetAttribute("datetime", PubDateUtc.GetValueOrDefault().ToString("u"));
+            output.Content.SetContent(DateTimeResolver.ToTimeZone(PubDateUtc.GetValueOrDefault()).ToLongDateString());
+        }
+    }
+}
