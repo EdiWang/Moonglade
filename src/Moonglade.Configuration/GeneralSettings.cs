7d687f20624dd5000a8b0cd3b5d3eac4f06106a9
Edi Wang <Edi.Wang@outlook.com>
2020-12-19 19:03:21 +0800

    Add SideBarOption settings key

--- src/Moonglade.Configuration/GeneralSettings.cs
+++ src/Moonglade.Configuration/GeneralSettings.cs
@@ -18,6 +18,8 @@ namespace Moonglade.Configuration
 
         public string SideBarCustomizedHtmlPitch { get; set; }
 
+        public SideBarOption SideBarOption { get; set; }
+
         public string FooterCustomizedHtmlPitch { get; set; }
 
         // Use string instead of TimeSpan as workaround for System.Text.Json issue
@@ -46,5 +48,12 @@ namespace Moonglade.Configuration
             SiteIconBase64 = string.Empty;
             AvatarBase64 = string.Empty;
         }
+    }
+
+    public enum SideBarOption
+    {
+        Right = 0,
+        Left = 1,
+        Disabled = 2
     }
 }
