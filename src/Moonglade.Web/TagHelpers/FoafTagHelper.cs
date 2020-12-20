8a6b6f053cf6f162ae88a09ee896b55441b21e37
Edi Wang <Edi.Wang@outlook.com>
2020-12-20 10:18:58 +0800

    fix link

--- src/Moonglade.Web/TagHelpers/FoafTagHelper.cs
+++ src/Moonglade.Web/TagHelpers/FoafTagHelper.cs
@@ -7,7 +7,7 @@ namespace Moonglade.Web.TagHelpers
     [HtmlTargetElement("foaf", TagStructure = TagStructure.NormalOrSelfClosing)]
     public class FoafTagHelper : TagHelper
     {
-        public string FoafLink { get; set; }
+        public string Href { get; set; }
         
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
@@ -15,7 +15,7 @@ namespace Moonglade.Web.TagHelpers
             output.Attributes.SetAttribute("type", new HtmlString(FoafWriter.ContentType));
             output.Attributes.SetAttribute("rel", "meta");
             output.Attributes.SetAttribute("title", "FOAF");
-            output.Attributes.SetAttribute("href", FoafLink);
+            output.Attributes.SetAttribute("href", Href);
         }
     }
 }
