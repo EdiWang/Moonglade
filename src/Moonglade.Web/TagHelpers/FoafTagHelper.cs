856da5e39b1dca2b51c753451624aaacad230579
Edi Wang <Edi.Wang@outlook.com>
2020-12-20 10:05:18 +0800

    fix taghelper encoding

--- src/Moonglade.Web/TagHelpers/FoafTagHelper.cs
+++ src/Moonglade.Web/TagHelpers/FoafTagHelper.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 using Moonglade.Foaf;
 
 namespace Moonglade.Web.TagHelpers
@@ -11,7 +12,7 @@ namespace Moonglade.Web.TagHelpers
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "link";
-            output.Attributes.SetAttribute("type", FoafWriter.ContentType);
+            output.Attributes.SetAttribute("type", new HtmlString(FoafWriter.ContentType));
             output.Attributes.SetAttribute("rel", "meta");
             output.Attributes.SetAttribute("title", "FOAF");
             output.Attributes.SetAttribute("href", FoafLink);
