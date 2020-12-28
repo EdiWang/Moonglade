80be52a9b782fb9ce771087708cccf8dfbc0f0e0
Edi Wang <Edi.Wang@outlook.com>
2020-12-28 12:58:07 +0800

    ICommentModerator interface

---
+++ src/Moonglade.Core/CommentModerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moonglade.Core
+{
+    public interface ICommentModerator
+    {
+
+    }
+
+    public class LocalWordFilterModerator : ICommentModerator
+    {
+
+    }
+}
