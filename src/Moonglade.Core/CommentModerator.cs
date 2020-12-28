bb3542f7720e27118fac1a966d7499fb7401784d
Edi Wang <Edi.Wang@outlook.com>
2020-12-28 13:05:02 +0800

    Update CommentModerator.cs

--- src/Moonglade.Core/CommentModerator.cs
+++ src/Moonglade.Core/CommentModerator.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Edi.WordFilter;
 
 namespace Moonglade.Core
 {
     public interface ICommentModerator
     {
+        public string ModerateContent(string input);
 
+        public bool HasBadWord(params string[] input);
     }
 
     public class LocalWordFilterModerator : ICommentModerator
     {
+        private readonly IMaskWordFilter _filter;
 
+        public LocalWordFilterModerator(string badWords)
+        {
+            _filter = new MaskWordFilter(new StringWordSource(badWords));
+        }
+
+        public string ModerateContent(string input)
+        {
+            return _filter.FilterContent(input);
+        }
+
+        public bool HasBadWord(params string[] input)
+        {
+            return input.Any(s => _filter.ContainsAnyWord(s));
+        }
     }
 }
