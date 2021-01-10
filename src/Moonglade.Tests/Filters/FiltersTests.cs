5528a6319218c552da7f8cbae9febaef5c02807b
Edi Wang <Edi.Wang@outlook.com>
2021-01-10 10:12:08 +0800

    ClearPagingCountCache UT

--- src/Moonglade.Tests/Filters/FiltersTests.cs
+++ src/Moonglade.Tests/Filters/FiltersTests.cs
@@ -1,10 +1,14 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
+using MemoryCache.Testing.Moq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Caching.Memory;
+using Moonglade.Caching;
 using Moonglade.Utils;
 using Moonglade.Web.Filters;
 using NUnit.Framework;
@@ -28,6 +32,33 @@ namespace Moonglade.Tests.Filters
             Assert.AreEqual(header, Helper.AppVersion);
         }
 
+        [Test]
+        public void ClearPagingCountCache_Success()
+        {
+            var ctx = CreateActionExecutingContext(null);
+            var ctx2 = CreateActionExecutedContext(ctx);
+
+            var mockedCache = Create.MockedMemoryCache();
+            var blogCache = new BlogMemoryCache(mockedCache);
+            blogCache.GetOrCreate(CacheDivision.General, "postcount", _ => 996);
+            blogCache.GetOrCreate(CacheDivision.General, "ali", _ => "fubao");
+            blogCache.GetOrCreate(CacheDivision.PostCountCategory, "pdd", _ => 007);
+            blogCache.GetOrCreate(CacheDivision.PostCountTag, "hw", _ => 251);
+
+            var att = new ClearPagingCountCache(blogCache);
+            att.OnActionExecuted(ctx2);
+
+            var postcount = mockedCache.Get<int>("General-postcount");
+            var ali = mockedCache.Get<string>("General-ali");
+            var pdd = mockedCache.Get<int>("PostCountCategory-pdd");
+            var hw = mockedCache.Get<int>("PostCountTag-hw");
+
+            Assert.AreEqual(0, postcount);
+            Assert.AreEqual("fubao", ali);
+            Assert.AreEqual(0, pdd);
+            Assert.AreEqual(0, hw);
+        }
+
         #region Helper Methods
 
         // https://github.com/dotnet/aspnetcore/blob/master/src/Mvc/shared/Mvc.Core.TestCommon/CommonFilterTest.cs
@@ -40,6 +71,24 @@ namespace Moonglade.Tests.Filters
                 new NoOpResult(),
                 controller: new object());
         }
+
+        private static ActionExecutingContext CreateActionExecutingContext(IFilterMetadata filter)
+        {
+            return new ActionExecutingContext(
+                CreateActionContext(),
+                new IFilterMetadata[] { filter, },
+                new Dictionary<string, object>(),
+                controller: new object());
+        }
+
+        private static ActionExecutedContext CreateActionExecutedContext(ActionExecutingContext context)
+        {
+            return new ActionExecutedContext(context, context.Filters, context.Controller)
+            {
+                Result = context.Result,
+            };
+        }
+
         private static ActionContext CreateActionContext()
         {
             return new ActionContext(new DefaultHttpContext(), new RouteData(), new ActionDescriptor());
