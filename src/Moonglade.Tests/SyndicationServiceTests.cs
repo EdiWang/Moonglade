3f9dc4decbdfaacaef147bb8fbef79ebab070114
Edi Wang <Edi.Wang@outlook.com>
2021-01-11 10:25:20 +0800

    GetRssStreamDataAsync_ExistingCategory

--- src/Moonglade.Tests/SyndicationServiceTests.cs
+++ src/Moonglade.Tests/SyndicationServiceTests.cs
@@ -108,6 +108,7 @@ namespace Moonglade.Tests
             Assert.AreEqual("996 is fubao", titles[1]);
             Assert.AreEqual("Fuck 996", titles[0]);
         }
+
         [Test]
         public async Task GetRssStreamDataAsync_NonExistingCategory()
         {
@@ -137,6 +138,72 @@ namespace Moonglade.Tests
 
             var result = await service.GetRssDataAsync("fuckpdd");
             Assert.IsNull(result);
+        }
+
+        [Test]
+        public async Task GetRssStreamDataAsync_ExistingCategory()
+        {
+            _mockHttpContextAccessor.Setup(p => p.HttpContext).Returns(new DefaultHttpContext
+            {
+                Request =
+                {
+                    Scheme = "https",
+                    Host = new("pdd.icu", 11116),
+                    Path = "/fuck-pdd"
+                }
+            });
+
+            _mockBlogConfig.Setup(bc => bc.FeedSettings).Returns(new FeedSettings
+            {
+                RssTitle = "Fuck PDD",
+                AuthorName = "Dead Workers",
+                RssCopyright = "(C) 2021 Gank PDD",
+                RssDescription = "Die in pain",
+                RssItemCount = 20
+            });
+
+            var fakeCat = new CategoryEntity
+            {
+                DisplayName = "PDD is shit",
+                Id = Guid.NewGuid(),
+                Note = "No PDD no death",
+                RouteName = "fuck-pdd"
+            };
+
+            _mockRepositoryCategoryEntity.Setup(p =>
+                p.GetAsync(It.IsAny<Expression<Func<CategoryEntity, bool>>>())).Returns(Task.FromResult(fakeCat));
+            
+            var fakeFeeds = new List<FeedEntry>
+            {
+                new()
+                {
+                    Author = "Jack Ma",
+                    AuthorEmail = "996@ali.com",
+                    Categories = new[] {"fubao", "cusi"},
+                    Description = "Work 996 and get into ICU",
+                    Id = "FUBAO996",
+                    Link = "https://996.icu",
+                    PubDateUtc = new(996, 9, 9),
+                    Title = "996 is fubao"
+                }
+            };
+
+            _mockRepositoryPostEntity.Setup(p =>
+                    p.SelectAsync(It.IsAny<ISpecification<PostEntity>>(),
+                        It.IsAny<Expression<Func<PostEntity, FeedEntry>>>(), true))
+                .Returns(Task.FromResult((IReadOnlyList<FeedEntry>)fakeFeeds));
+
+            var service = CreateService();
+
+            var result = await service.GetRssDataAsync("fuckpdd");
+            Assert.IsNotNull(result);
+
+            var xdoc = XDocument.Parse(result);
+            var titles = xdoc.Descendants("title").Select(x => x.Value).ToList();
+
+            Assert.AreEqual(2, titles.Count);
+            Assert.AreEqual("996 is fubao", titles[1]);
+            Assert.AreEqual("Fuck PDD", titles[0]);
         }
     }
 }
