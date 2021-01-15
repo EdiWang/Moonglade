824569b89dc80afc1f10a8fcbd2bd7a8f1c92e67
Edi Wang <Edi.Wang@outlook.com>
2021-01-15 10:39:29 +0800

    Update HomeControllerTests.cs

--- src/Moonglade.Tests/Controllers/HomeControllerTests.cs
+++ src/Moonglade.Tests/Controllers/HomeControllerTests.cs
@@ -6,11 +6,16 @@ using Moonglade.Web.Controllers;
 using Moq;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moonglade.Model;
 
 namespace Moonglade.Tests.Controllers
 {
     [TestFixture]
+    [ExcludeFromCodeCoverage]
     public class HomeControllerTests
     {
         private MockRepository _mockRepository;
@@ -38,6 +43,27 @@ namespace Moonglade.Tests.Controllers
                 _mockBlogCache.Object,
                 _mockBlogConfig.Object,
                 _mockLogger.Object);
+        }
+
+        [Test]
+        public async Task Tags_Index()
+        {
+            var fakeTags = new List<DegreeTag>
+            {
+                new() { Degree = 251, DisplayName = "Huawei", Id = 35, NormalizedName = "aiguo" },
+                new() { Degree = 996, DisplayName = "Ali", Id = 35, NormalizedName = "fubao" }
+            };
+
+            var mockTagService = new Mock<ITagService>();
+            mockTagService.Setup(p => p.GetTagCountListAsync())
+                .Returns(Task.FromResult((IReadOnlyList<DegreeTag>)fakeTags));
+
+            var ctl = CreateHomeController();
+            var result = await ctl.Tags(mockTagService.Object);
+
+            Assert.IsInstanceOf<ViewResult>(result);
+
+            Assert.AreEqual(fakeTags, ((ViewResult)result).Model);
         }
     }
 }
