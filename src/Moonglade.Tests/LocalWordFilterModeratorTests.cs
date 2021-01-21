34ede2b340c56e1f14da38313dd6e3517646363e
Edi Wang <Edi.Wang@outlook.com>
2021-01-21 13:25:16 +0800

    Create LocalWordFilterModeratorTests.cs

---
+++ src/Moonglade.Tests/LocalWordFilterModeratorTests.cs
@@ -0,0 +1,55 @@
+using Moonglade.Comments;
+using Moonglade.Configuration.Abstraction;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Moonglade.Configuration;
+
+namespace Moonglade.Tests
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class LocalWordFilterModeratorTests
+    {
+        private MockRepository _mockRepository;
+        private Mock<IBlogConfig> _mockBlogConfig;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockRepository = new(MockBehavior.Strict);
+            _mockBlogConfig = _mockRepository.Create<IBlogConfig>();
+        }
+
+        private LocalWordFilterModerator CreateLocalWordFilterModerator()
+        {
+            _mockBlogConfig.Setup(p => p.ContentSettings).Returns(new ContentSettings
+            {
+                DisharmonyWords = "fuck|shit"
+            });
+
+            return new(
+                _mockBlogConfig.Object);
+        }
+
+        [Test]
+        public async Task ModerateContent_ExpectedBehavior()
+        {
+            var localWordFilterModerator = CreateLocalWordFilterModerator();
+            string input = "Fuck Jack Ma";
+            var result = await localWordFilterModerator.ModerateContent(input);
+
+            Assert.AreEqual("**** Jack Ma", result);
+        }
+
+        [Test]
+        public async Task HasBadWord_ExpectedBehavior()
+        {
+            var localWordFilterModerator = CreateLocalWordFilterModerator();
+            var result = await localWordFilterModerator.HasBadWord("Fuck 996", "Fuck some shit");
+            Assert.IsTrue(result);
+        }
+    }
+}
