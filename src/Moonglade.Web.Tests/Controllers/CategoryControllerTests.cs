683127cb391fc15a5f12b2f737e16edd2904bef9
Edi Wang <Edi.Wang@outlook.com>
2021-02-20 14:16:24 +0800

    Create CategoryControllerTests.cs

---
+++ src/Moonglade.Web.Tests/Controllers/CategoryControllerTests.cs
@@ -0,0 +1,78 @@
+using Moonglade.Core;
+using Moonglade.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moonglade.Web.Models;
+
+namespace Moonglade.Web.Tests.Controllers
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class CategoryControllerTests
+    {
+        private MockRepository _mockRepository;
+        private Mock<ICategoryService> _mockCategoryService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockRepository = new(MockBehavior.Default);
+            _mockCategoryService = _mockRepository.Create<ICategoryService>();
+        }
+
+        private CategoryController CreateCategoryController()
+        {
+            return new(_mockCategoryService.Object);
+        }
+
+        [Test]
+        public async Task Create_BadModelState()
+        {
+            var categoryController = CreateCategoryController();
+            var model = new CategoryEditModel
+            {
+                DisplayName = "996",
+                Note = string.Empty
+            };
+            categoryController.ModelState.AddModelError("Note", "Note is required");
+
+            var result = await categoryController.Create(model);
+            Assert.IsInstanceOf<BadRequestObjectResult>(result);
+        }
+
+        [Test]
+        public async Task Create_ValidModel()
+        {
+            var categoryController = CreateCategoryController();
+            var model = new CategoryEditModel
+            {
+                DisplayName = "996",
+                RouteName = "996",
+                Note = "fubao"
+            };
+
+            var result = await categoryController.Create(model);
+            Assert.IsInstanceOf<OkObjectResult>(result);
+        }
+
+        [Test]
+        public async Task Delete_EmptyId()
+        {
+            var categoryController = CreateCategoryController();
+            var result = await categoryController.Delete(Guid.Empty);
+            Assert.IsInstanceOf<BadRequestObjectResult>(result);
+        }
+
+        [Test]
+        public async Task Delete_ValidId()
+        {
+            var categoryController = CreateCategoryController();
+            var result = await categoryController.Delete(Guid.NewGuid());
+            Assert.IsInstanceOf<OkResult>(result);
+        }
+    }
+}
