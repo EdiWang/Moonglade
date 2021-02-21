87118ff8f61cee778410bdf4f73b117322f0715a
Edi Wang <Edi.Wang@outlook.com>
2021-02-21 17:46:46 +0800

    Create RssListViewComponentTests.cs

---
+++ src/Moonglade.Web.Tests/ViewComponents/RssListViewComponentTests.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Logging;
+using Moonglade.Core;
+using Moonglade.Web.ViewComponents;
+using Moq;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace Moonglade.Web.Tests.ViewComponents
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class RssListViewComponentTests
+    {
+        private MockRepository _mockRepository;
+
+        private Mock<ILogger<RssListViewComponent>> _mockLogger;
+        private Mock<ICategoryService> _mockCategoryService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockRepository = new(MockBehavior.Default);
+
+            _mockLogger = _mockRepository.Create<ILogger<RssListViewComponent>>();
+            _mockCategoryService = _mockRepository.Create<ICategoryService>();
+        }
+
+        private RssListViewComponent CreateComponent()
+        {
+            return new(
+                _mockLogger.Object,
+                _mockCategoryService.Object);
+        }
+
+        [Test]
+        public async Task InvokeAsync_Exception()
+        {
+            _mockCategoryService.Setup(p => p.GetAll()).Throws(new("996"));
+
+            var component = CreateComponent();
+            var result = await component.InvokeAsync();
+
+            Assert.IsInstanceOf<ViewViewComponentResult>(result);
+
+            var message = ((ViewViewComponentResult)result).ViewData["ComponentErrorMessage"];
+            Assert.AreEqual("996", message);
+        }
+    }
+}
