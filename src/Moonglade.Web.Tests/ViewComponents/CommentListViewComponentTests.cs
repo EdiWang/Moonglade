0ae84b41f2d9854d0c6b816dcc37eba46a3ecd66
Edi Wang <Edi.Wang@outlook.com>
2021-02-21 17:43:16 +0800

    Update CommentListViewComponentTests.cs

--- src/Moonglade.Web.Tests/ViewComponents/CommentListViewComponentTests.cs
+++ src/Moonglade.Web.Tests/ViewComponents/CommentListViewComponentTests.cs
@@ -4,6 +4,7 @@ using Moonglade.Web.ViewComponents;
 using Moq;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.ViewComponents;
@@ -41,12 +42,36 @@ namespace Moonglade.Web.Tests.ViewComponents
             _mockCommentService.Setup(p => p.GetApprovedCommentsAsync(It.IsAny<Guid>())).Throws(new("996"));
 
             var component = CreateComponent();
-            var result = await component.InvokeAsync(Guid.NewGuid());
+            var result = await component.InvokeAsync(Guid.Parse("5ef8cb0d-963d-47e9-802c-48e40c7f4ef5"));
 
             Assert.IsInstanceOf<ViewViewComponentResult>(result);
 
             var message = ((ViewViewComponentResult)result).ViewData["ComponentErrorMessage"];
             Assert.AreEqual("996", message);
+        }
+
+        [Test]
+        public async Task InvokeAsync_EmptyId()
+        {
+            var component = CreateComponent();
+            var result = await component.InvokeAsync(Guid.Empty);
+
+            Assert.IsInstanceOf<ViewViewComponentResult>(result);
+            Assert.AreEqual("Error", ((ViewViewComponentResult)result).ViewName);
+        }
+
+        [Test]
+        public async Task InvokeAsync_View()
+        {
+            IReadOnlyList<Comment> comments = new List<Comment>();
+
+            _mockCommentService.Setup(p => p.GetApprovedCommentsAsync(It.IsAny<Guid>())).Returns(Task.FromResult(comments));
+
+            var component = CreateComponent();
+            var result = await component.InvokeAsync(Guid.Parse("5ef8cb0d-963d-47e9-802c-48e40c7f4ef5"));
+
+            Assert.IsInstanceOf<ViewViewComponentResult>(result);
+            Assert.AreEqual(null, ((ViewViewComponentResult)result).ViewName);
         }
     }
 }
