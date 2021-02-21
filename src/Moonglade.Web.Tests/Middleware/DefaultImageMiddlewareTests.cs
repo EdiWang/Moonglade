a731063ce09f798684b654c11fb8629adb5a173d
Edi Wang <Edi.Wang@outlook.com>
2021-02-21 08:36:47 +0800

    Create DefaultImageMiddlewareTests.cs

---
+++ src/Moonglade.Web.Tests/Middleware/DefaultImageMiddlewareTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Moonglade.Web.Middleware;
+using NUnit.Framework;
+
+namespace Moonglade.Web.Tests.Middleware
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class DefaultImageMiddlewareTests
+    {
+        [Test]
+        public void UseDefaultImageMiddlewareExtensions()
+        {
+            var serviceCollection = new ServiceCollection();
+            var applicationBuilder = new ApplicationBuilder(serviceCollection.BuildServiceProvider());
+
+            applicationBuilder.UseDefaultImage(options => { });
+
+            var app = applicationBuilder.Build();
+
+            var type = app.Target.GetType();
+            Assert.AreEqual(nameof(UseMiddlewareExtensions), type.DeclaringType.Name);
+        }
+    }
+}
