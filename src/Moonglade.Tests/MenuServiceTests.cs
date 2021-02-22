d063e3b60495f15db12eb00907246c523203d904
Edi Wang <Edi.Wang@outlook.com>
2021-02-22 15:55:01 +0800

    Update MenuServiceTests.cs

--- src/Moonglade.Tests/MenuServiceTests.cs
+++ src/Moonglade.Tests/MenuServiceTests.cs
@@ -1,6 +1,13 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using Moonglade.Utils;
+using Microsoft.Extensions.Logging;
+using Moonglade.Auditing;
+using Moonglade.Data.Entities;
+using Moonglade.Data.Infrastructure;
+using Moonglade.Menus;
+using Moq;
 using NUnit.Framework;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
 
 namespace Moonglade.Tests
 {
@@ -8,41 +15,28 @@ namespace Moonglade.Tests
     [ExcludeFromCodeCoverage]
     public class MenuServiceTests
     {
-        // Valid Urls
-        [TestCase("http://996.icu", ExpectedResult = "http://996.icu")]
-        [TestCase("https://996.icu", ExpectedResult = "https://996.icu")]
-        [TestCase("https://996.icu/", ExpectedResult = "https://996.icu/")]
-        [TestCase("https://996.icu/work996", ExpectedResult = "https://996.icu/work996")]
-        [TestCase("https://996.icu/#", ExpectedResult = "https://996.icu/#")]
-        [TestCase("https://996.icu/#icu", ExpectedResult = "https://996.icu/#icu")]
-        [TestCase("https://996.icu/996#icu", ExpectedResult = "https://996.icu/996#icu")]
-        [TestCase("https://996.icu/why?reason=fubao", ExpectedResult = "https://996.icu/why?reason=fubao")]
-        [TestCase("https://13.107.21.200", ExpectedResult = "https://13.107.21.200")]
-        [TestCase("/", ExpectedResult = "/")]
-        [TestCase("/996/icu", ExpectedResult = "/996/icu")]
-        [TestCase("/996/icu?reason=fubao", ExpectedResult = "/996/icu?reason=fubao")]
-        [TestCase("https://internalsystem/996/icu", ExpectedResult = "https://internalsystem/996/icu")]
-        // Invalid Urls
-        [TestCase("https://localhost", ExpectedResult = "#")]
-        [TestCase("https://localhost:996", ExpectedResult = "#")]
-        [TestCase("http://localhost/icu", ExpectedResult = "#")]
-        [TestCase("https://192.168.0.1", ExpectedResult = "#")]
-        [TestCase("https://127.0.0.1", ExpectedResult = "#")]
-        [TestCase("https://10.0.0.1", ExpectedResult = "#")]
-        [TestCase("192.168.0.1", ExpectedResult = "#")]
-        [TestCase("../", ExpectedResult = "#")]
-        [TestCase("./", ExpectedResult = "#")]
-        [TestCase("//", ExpectedResult = "#")]
-        [TestCase(@"/\", ExpectedResult = "#")]
-        [TestCase("", ExpectedResult = "#")]
-        [TestCase(" ", ExpectedResult = "#")]
-        [TestCase(null, ExpectedResult = "#")]
-        [TestCase("javascript:;", ExpectedResult = "#")]
-        [TestCase("javascript:while(true){alert('fuck')};", ExpectedResult = "#")]
-        [TestCase("blob:https://996.icu/fubao", ExpectedResult = "#")]
-        public string SterilizeMenuLink(string rawUrl)
+        private MockRepository _mockRepository;
+
+        private Mock<ILogger<MenuService>> _mockLogger;
+        private Mock<IRepository<MenuEntity>> _mockMenuRepository;
+        private Mock<IBlogAudit> _mockBlogAudit;
+
+        [SetUp]
+        public void SetUp()
         {
-            return Helper.SterilizeLink(rawUrl);
+            _mockRepository = new(MockBehavior.Strict);
+
+            _mockLogger = _mockRepository.Create<ILogger<MenuService>>();
+            _mockMenuRepository = _mockRepository.Create<IRepository<MenuEntity>>();
+            _mockBlogAudit = _mockRepository.Create<IBlogAudit>();
+        }
+
+        private MenuService CreateService()
+        {
+            return new(
+                _mockLogger.Object,
+                _mockMenuRepository.Object,
+                _mockBlogAudit.Object);
         }
     }
 }
