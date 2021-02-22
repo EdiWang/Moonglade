7525267d90667334df38ca3f2463593179b83e99
Edi Wang <Edi.Wang@outlook.com>
2021-02-22 12:01:25 +0800

    Update FriendLinkViewComponentTests.cs

--- src/Moonglade.Web.Tests/ViewComponents/FriendLinkViewComponentTests.cs
+++ src/Moonglade.Web.Tests/ViewComponents/FriendLinkViewComponentTests.cs
@@ -5,6 +5,7 @@ using Moonglade.Web.ViewComponents;
 using Moq;
 using NUnit.Framework;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 
 namespace Moonglade.Web.Tests.ViewComponents
 {
@@ -31,6 +32,20 @@ namespace Moonglade.Web.Tests.ViewComponents
             return new(
                 _mockLogger.Object,
                 _mockFriendLinkService.Object);
+        }
+
+        [Test]
+        public async Task InvokeAsync_Exception()
+        {
+            _mockFriendLinkService.Setup(p => p.GetAllAsync()).Throws(new("996"));
+
+            var component = CreateComponent();
+            var result = await component.InvokeAsync();
+
+            Assert.IsInstanceOf<ViewViewComponentResult>(result);
+
+            var message = ((ViewViewComponentResult)result).ViewData["ComponentErrorMessage"];
+            Assert.AreEqual("996", message);
         }
     }
 }
