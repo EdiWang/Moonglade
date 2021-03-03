a1d7c37bce6b53e4af8166f20e5fa0396ad3b3a7
Edi Wang <Edi.Wang@outlook.com>
2021-03-03 12:36:39 +0800

    rewrite gravatar html helper to tag helper

---
+++ src/Moonglade.Web/TagHelpers/GravatarImageHelper.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Moonglade.Utils;
+
+namespace Moonglade.Web.TagHelpers
+{
+    [HtmlTargetElement("gravatar", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class GravatarImageHelper : TagHelper
+    {
+        public string Email { get; set; }
+
+        public int Size { get; set; } = 58;
+
+        public string DefaultImageUrl { get; set; } = string.Empty;
+
+        public bool PreferHttps { get; set; } = true;
+
+        public bool ForceDefaultImage { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var email = string.IsNullOrEmpty(Email) ? string.Empty : Email.Trim().ToLower();
+            var emailHash = Helper.GetMd5Hash(email);
+
+            var src = string.Format("{0}://{1}.gravatar.com/avatar/{2}?s={3}{4}{5}{6}",
+                PreferHttps ? "https" : "http",
+                PreferHttps ? "secure" : "www",
+                emailHash,
+                Size.ToString(),
+                "&d=" + (!string.IsNullOrEmpty(DefaultImageUrl)
+                    ? UrlEncoder.Default.Encode(DefaultImageUrl)
+                    : string.Empty),
+                ForceDefaultImage ? "&f=y" : string.Empty,
+                "&r=g");
+
+            output.TagName = "img";
+            output.Attributes.SetAttribute("src", src);
+            output.Attributes.SetAttribute("alt", "Gravatar image");
+        }
+    }
+}
