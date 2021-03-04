ad82054fedb6a144293d36927248aa48d3a8f34f
Edi Wang <Edi.Wang@outlook.com>
2021-03-04 14:00:48 +0800

    Create RSDMiddlewareTests.cs

---
+++ src/Moonglade.Web.Tests/Middleware/RSDMiddlewareTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Moonglade.Web.Middleware;
+using Moq;
+using NUnit.Framework;
+
+namespace Moonglade.Web.Tests.Middleware
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class RSDMiddlewareTests
+    {
+        [Test]
+        public async Task NonRSDRequestPath()
+        {
+            var reqMock = new Mock<HttpRequest>();
+            reqMock.SetupGet(r => r.Path).Returns("/996");
+
+            var httpContextMock = new Mock<HttpContext>();
+            httpContextMock.Setup(c => c.Request).Returns(reqMock.Object);
+
+            static Task RequestDelegate(HttpContext context) => Task.CompletedTask;
+            var middleware = new RSDMiddleware(RequestDelegate);
+
+            await middleware.Invoke(httpContextMock.Object, null);
+
+            Assert.Pass();
+        }
+    }
+}
