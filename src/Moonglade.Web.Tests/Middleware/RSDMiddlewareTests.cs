f093b2465e5b638d9e61f3ea3ce1b9fa6498708a
Edi Wang <Edi.Wang@outlook.com>
2021-03-04 14:06:32 +0800

    Update RSDMiddlewareTests.cs

--- src/Moonglade.Web.Tests/Middleware/RSDMiddlewareTests.cs
+++ src/Moonglade.Web.Tests/Middleware/RSDMiddlewareTests.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Moonglade.Configuration;
+using Moonglade.Configuration.Abstraction;
 using Moonglade.Web.Middleware;
 using Moq;
 using NUnit.Framework;
@@ -16,7 +19,7 @@ namespace Moonglade.Web.Tests.Middleware
     public class RSDMiddlewareTests
     {
         [Test]
-        public async Task NonRSDRequestPath()
+        public async Task Invoke_NonRSDRequestPath()
         {
             var reqMock = new Mock<HttpRequest>();
             reqMock.SetupGet(r => r.Path).Returns("/996");
@@ -29,6 +32,28 @@ namespace Moonglade.Web.Tests.Middleware
 
             await middleware.Invoke(httpContextMock.Object, null);
 
+            Assert.Pass();
+        }
+
+        [Test]
+        public async Task Invoke_RSDRequestPath()
+        {
+            Mock<IBlogConfig> blogConfigMock = new();
+            blogConfigMock.Setup(p => p.GeneralSettings).Returns(new GeneralSettings
+            {
+                CanonicalPrefix = "https://996.icu"
+            });
+
+            static Task RequestDelegate(HttpContext context) => Task.CompletedTask;
+            var middleware = new RSDMiddleware(RequestDelegate);
+
+            var ctx = new DefaultHttpContext();
+            ctx.Response.Body = new MemoryStream();
+            ctx.Request.Path = "/rsd";
+
+            await middleware.Invoke(ctx, blogConfigMock.Object);
+
+            Assert.AreEqual("text/xml", ctx.Response.ContentType);
             Assert.Pass();
         }
     }
