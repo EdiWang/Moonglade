b4d0d0c8285991b42f557aec36fad72b7918dcc3
Edi Wang <Edi.Wang@outlook.com>
2021-03-05 12:56:53 +0800

    Update SiteMapMiddlewareTests.cs

--- src/Moonglade.Web.Tests/Middleware/SiteMapMiddlewareTests.cs
+++ src/Moonglade.Web.Tests/Middleware/SiteMapMiddlewareTests.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Memory;
+using Moonglade.Caching;
+using Moonglade.Configuration;
+using Moonglade.Configuration.Abstraction;
+using Moonglade.Web.BlogProtocols;
 using Moonglade.Web.Middleware;
 using Moq;
 using NUnit.Framework;
@@ -29,6 +35,35 @@ namespace Moonglade.Web.Tests.Middleware
 
             await middleware.Invoke(httpContextMock.Object, null, null, null);
 
+            Assert.Pass();
+        }
+
+        [Test]
+        public async Task Invoke_SiteMapRequestPath()
+        {
+            Mock<IBlogConfig> blogConfigMock = new();
+            Mock<IBlogCache> blogCacheMock = new();
+            Mock<ISiteMapWriter> siteMapWriterMock = new();
+
+            blogConfigMock.Setup(p => p.GeneralSettings).Returns(new GeneralSettings
+            {
+                CanonicalPrefix = "https://996.icu"
+            });
+
+            blogCacheMock
+                .Setup(p => p.GetOrCreateAsync(CacheDivision.General, "sitemap",
+                    It.IsAny<Func<ICacheEntry, Task<string>>>())).Returns(Task.FromResult("<xml></xml>"));
+
+            static Task RequestDelegate(HttpContext context) => Task.CompletedTask;
+            var middleware = new SiteMapMiddleware(RequestDelegate);
+
+            var ctx = new DefaultHttpContext();
+            ctx.Response.Body = new MemoryStream();
+            ctx.Request.Path = "/sitemap.xml";
+
+            await middleware.Invoke(ctx, blogConfigMock.Object, blogCacheMock.Object, siteMapWriterMock.Object);
+
+            Assert.AreEqual("text/xml", ctx.Response.ContentType);
             Assert.Pass();
         }
     }
