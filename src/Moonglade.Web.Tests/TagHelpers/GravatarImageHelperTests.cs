b55ed7a6f1b665d3b4def764fb5b66622c2414b7
Edi Wang <Edi.Wang@outlook.com>
2021-03-11 15:32:12 +0800

    Split UT file

---
+++ src/Moonglade.Web.Tests/TagHelpers/GravatarImageHelperTests.cs
@@ -0,0 +1,83 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Moonglade.Web.TagHelpers;
+using NUnit.Framework;
+
+namespace Moonglade.Web.Tests.TagHelpers
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class GravatarImageHelperTests
+    {
+        [Test]
+        public void GravatarImage_Process()
+        {
+            var tagHelper = new GravatarImageHelper
+            {
+                Email = "996@icu.com"
+            };
+
+            var outputAttributes = new TagHelperAttributeList();
+            var context = TagHelperTestsHelpers.MakeTagHelperContext("img");
+            var output = TagHelperTestsHelpers.MakeTagHelperOutput("img", outputAttributes);
+
+            tagHelper.Process(context, output);
+
+            var expectedAttributeList = new TagHelperAttributeList
+            {
+                new("src", "https://secure.gravatar.com/avatar/a7da2236b13239fc32f99efa50540744?s=58&d=&r=g"),
+                new("alt", "Gravatar image"),
+            };
+
+            Assert.AreEqual(expectedAttributeList, output.Attributes);
+        }
+
+        [Test]
+        public void GravatarImage_Process_ImageSize()
+        {
+            var tagHelper = new GravatarImageHelper
+            {
+                Email = "996@icu.com",
+                Size = 996
+            };
+
+            var outputAttributes = new TagHelperAttributeList();
+            var context = TagHelperTestsHelpers.MakeTagHelperContext("img");
+            var output = TagHelperTestsHelpers.MakeTagHelperOutput("img", outputAttributes);
+
+            tagHelper.Process(context, output);
+
+            var expectedAttributeList = new TagHelperAttributeList
+            {
+                new("src", "https://secure.gravatar.com/avatar/a7da2236b13239fc32f99efa50540744?s=996&d=&r=g"),
+                new("alt", "Gravatar image"),
+            };
+
+            Assert.AreEqual(expectedAttributeList, output.Attributes);
+        }
+
+        [Test]
+        public void GravatarImage_Process_NoHttps()
+        {
+            var tagHelper = new GravatarImageHelper
+            {
+                Email = "996@icu.com",
+                PreferHttps = false
+            };
+
+            var outputAttributes = new TagHelperAttributeList();
+            var context = TagHelperTestsHelpers.MakeTagHelperContext("img");
+            var output = TagHelperTestsHelpers.MakeTagHelperOutput("img", outputAttributes);
+
+            tagHelper.Process(context, output);
+
+            var expectedAttributeList = new TagHelperAttributeList
+            {
+                new("src", "http://www.gravatar.com/avatar/a7da2236b13239fc32f99efa50540744?s=58&d=&r=g"),
+                new("alt", "Gravatar image"),
+            };
+
+            Assert.AreEqual(expectedAttributeList, output.Attributes);
+        }
+    }
+}
