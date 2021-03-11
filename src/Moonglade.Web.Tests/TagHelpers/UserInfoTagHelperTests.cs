5a55982d832100c158ab8ae6ef3f3841b02693e6
Edi Wang <Edi.Wang@outlook.com>
2021-03-11 15:39:32 +0800

    Update UserInfoTagHelperTests.cs

--- src/Moonglade.Web.Tests/TagHelpers/UserInfoTagHelperTests.cs
+++ src/Moonglade.Web.Tests/TagHelpers/UserInfoTagHelperTests.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Moonglade.Web.TagHelpers;
 using NUnit.Framework;
 
 namespace Moonglade.Web.Tests.TagHelpers
@@ -13,9 +16,39 @@ namespace Moonglade.Web.Tests.TagHelpers
     public class UserInfoTagHelperTests
     {
         [Test]
-        public void Process_UserNotLogin()
+        public void Process_UserNull()
         {
+            var tagHelper = new UserInfoTagHelper
+            {
+                User = null
+            };
 
+            var outputAttributes = new TagHelperAttributeList();
+            var context = TagHelperTestsHelpers.MakeTagHelperContext("div");
+            var output = TagHelperTestsHelpers.MakeTagHelperOutput("div", outputAttributes);
+
+            tagHelper.Process(context, output);
+
+            var expectedAttributeList = new TagHelperAttributeList();
+            Assert.AreEqual(expectedAttributeList, output.Attributes);
+        }
+
+        [Test]
+        public void Process_UserIdentityNull()
+        {
+            var tagHelper = new UserInfoTagHelper
+            {
+                User = new()
+            };
+
+            var outputAttributes = new TagHelperAttributeList();
+            var context = TagHelperTestsHelpers.MakeTagHelperContext("div");
+            var output = TagHelperTestsHelpers.MakeTagHelperOutput("div", outputAttributes);
+
+            tagHelper.Process(context, output);
+
+            var expectedAttributeList = new TagHelperAttributeList();
+            Assert.AreEqual(expectedAttributeList, output.Attributes);
         }
     }
 }
