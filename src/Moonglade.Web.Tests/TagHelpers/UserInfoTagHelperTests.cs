30caa529f5682bfa65d73092a09326649fd21bf4
Edi Wang <Edi.Wang@outlook.com>
2021-03-11 15:34:45 +0800

    Create UserInfoTagHelperTests.cs

---
+++ src/Moonglade.Web.Tests/TagHelpers/UserInfoTagHelperTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Moonglade.Web.Tests.TagHelpers
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class UserInfoTagHelperTests
+    {
+        [Test]
+        public void Process_UserNotLogin()
+        {
+
+        }
+    }
+}
