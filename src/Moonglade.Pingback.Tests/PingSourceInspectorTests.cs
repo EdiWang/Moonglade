c70c5b690953af02aabaef600f226873455e13eb
Edi Wang <Edi.Wang@outlook.com>
2021-03-11 21:08:10 +0800

    Create PingSourceInspectorTests.cs

---
+++ src/Moonglade.Pingback.Tests/PingSourceInspectorTests.cs
@@ -0,0 +1,52 @@
+using Microsoft.Extensions.Logging;
+using Moonglade.Pingback;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace Moonglade.Pingback.Tests
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class PingSourceInspectorTests
+    {
+        private MockRepository _mockRepository;
+
+        private Mock<ILogger<PingSourceInspector>> _mockLogger;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockRepository = new(MockBehavior.Default);
+            _mockLogger = _mockRepository.Create<ILogger<PingSourceInspector>>();
+        }
+
+        private PingSourceInspector CreatePingSourceInspector()
+        {
+            return new(
+                _mockLogger.Object);
+        }
+
+        //[Test]
+        //public async Task ExamineSourceAsync_StateUnderTest_ExpectedBehavior()
+        //{
+        //    // Arrange
+        //    var pingSourceInspector = this.CreatePingSourceInspector();
+        //    string sourceUrl = null;
+        //    string targetUrl = null;
+        //    int timeoutSeconds = 0;
+
+        //    // Act
+        //    var result = await pingSourceInspector.ExamineSourceAsync(
+        //        sourceUrl,
+        //        targetUrl,
+        //        timeoutSeconds);
+
+        //    // Assert
+        //    Assert.Fail();
+        //    this.mockRepository.VerifyAll();
+        //}
+    }
+}
