9f14e84bdd3e605327c91489253daccf9b7f2df8
Edi Wang <Edi.Wang@outlook.com>
2021-03-12 13:38:49 +0800

    Update DefaultImageMiddlewareTests.cs

--- src/Moonglade.Web.Tests/Middleware/DefaultImageMiddlewareTests.cs
+++ src/Moonglade.Web.Tests/Middleware/DefaultImageMiddlewareTests.cs
@@ -1,7 +1,12 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Moonglade.Configuration.Abstraction;
 using Moonglade.Web.Middleware;
+using Moq;
 using NUnit.Framework;
 
 namespace Moonglade.Web.Tests.Middleware
@@ -10,6 +15,16 @@ namespace Moonglade.Web.Tests.Middleware
     [ExcludeFromCodeCoverage]
     public class DefaultImageMiddlewareTests
     {
+        private MockRepository _mockRepository;
+        private Mock<IBlogConfig> _mockBlogConfig;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockRepository = new(MockBehavior.Default);
+            _mockBlogConfig = _mockRepository.Create<IBlogConfig>();
+        }
+
         [Test]
         public void UseDefaultImageMiddlewareExtensions()
         {
@@ -22,6 +37,22 @@ namespace Moonglade.Web.Tests.Middleware
 
             var type = app.Target.GetType();
             Assert.AreEqual(nameof(UseMiddlewareExtensions), type.DeclaringType.Name);
+        }
+
+        [Test]
+        public async Task Invoke_Not404()
+        {
+            var ctx = new DefaultHttpContext();
+            ctx.Response.Body = new MemoryStream();
+            ctx.Request.Path = "/996";
+            ctx.Request.ContentType = "text/html";
+            ctx.Response.StatusCode = 200;
+
+            static Task RequestDelegate(HttpContext context) => Task.CompletedTask;
+            var middleware = new DefaultImageMiddleware(RequestDelegate);
+
+            await middleware.Invoke(ctx, _mockBlogConfig.Object);
+            Assert.AreEqual("text/html", ctx.Request.ContentType);
         }
     }
 }
