1fdc67cbc02063fdb9e467f5af22671df90ea206
Edi Wang <Edi.Wang@outlook.com>
2021-03-12 13:23:07 +0800

    Create AppSettingsGetApiKeyQueryTests.cs

---
+++ src/Moonglade.Auth.Tests/AppSettingsGetApiKeyQueryTests.cs
@@ -0,0 +1,57 @@
+using Microsoft.Extensions.Options;
+using Moonglade.Auth;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace Moonglade.Auth.Tests
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class AppSettingsGetApiKeyQueryTests
+    {
+        private MockRepository _mockRepository;
+        private Mock<IOptions<AuthenticationSettings>> _mockOptions;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockRepository = new MockRepository(MockBehavior.Default);
+            _mockOptions = _mockRepository.Create<IOptions<AuthenticationSettings>>();
+        }
+
+        private AppSettingsGetApiKeyQuery CreateAppSettingsGetApiKeyQuery()
+        {
+            return new(_mockOptions.Object);
+        }
+
+        [Test]
+        public async Task Execute_ExpectedBehavior()
+        {
+            _mockOptions.Setup(p => p.Value).Returns(new AuthenticationSettings
+            {
+                Provider = AuthenticationProvider.AzureAD,
+                MetaWeblog = new MetaWeblogCredential
+                {
+                    Password = "work996",
+                    Username = "getintoicu"
+                },
+                ApiKeys = new ApiKey[]
+                {
+                    new () { Id = 1, Key = "fuck996", Owner = "996fucker" },
+                    new () { Id = 2, Key = "pdd007", Owner = "gotoicu" },
+                    new () { Id = 3, Key = "251", Owner = "hwaiguo" }
+                }
+            });
+
+            var appSettingsGetApiKeyQuery = CreateAppSettingsGetApiKeyQuery();
+            string providedApiKey = "251";
+
+            var result = await appSettingsGetApiKeyQuery.Execute(providedApiKey);
+
+            Assert.AreEqual("251", result.Key);
+        }
+    }
+}
