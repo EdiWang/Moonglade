1e25baf02258e7c9d9647e63f5e547be25c5f519
Edi Wang <Edi.Wang@outlook.com>
2021-03-15 13:38:54 +0800

    Update PostManageControllerTests.cs

--- src/Moonglade.Web.Tests/Controllers/PostManageControllerTests.cs
+++ src/Moonglade.Web.Tests/Controllers/PostManageControllerTests.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using Moonglade.Configuration;
+using Moonglade.Data.Entities;
 using Moonglade.Data.Spec;
 using Moonglade.Pingback;
 using Moonglade.Web.Models;
@@ -28,7 +29,39 @@ namespace Moonglade.Web.Tests.Controllers
         private Mock<IBlogConfig> _mockBlogConfig;
         private Mock<ITZoneResolver> _mockTZoneResolver;
         private Mock<ILogger<PostManageController>> _mockLogger;
+
         private static readonly Guid Uid = Guid.Parse("76169567-6ff3-42c0-b163-a883ff2ac4fb");
+        private static readonly Category Cat = new()
+        {
+            DisplayName = "WTF",
+            Id = Guid.Parse("6364e9be-2423-44da-bd11-bc6fa9c3fa5d"),
+            Note = "A wonderful contry",
+            RouteName = "wtf"
+        };
+
+        private static readonly Post Post = new()
+        {
+            Id = Uid,
+            Title = "Work 996 and Get into ICU",
+            Slug = "work-996-and-get-into-icu",
+            ContentAbstract = "Get some fubao",
+            RawPostContent = "<p>Get some fubao</p>",
+            ContentLanguageCode = "en-us",
+            Featured = true,
+            ExposedToSiteMap = true,
+            IsFeedIncluded = true,
+            IsPublished = true,
+            CommentEnabled = true,
+            PubDateUtc = new DateTime(2019, 9, 6, 6, 35, 7),
+            LastModifiedUtc = new DateTime(2020, 9, 6, 6, 35, 7),
+            CreateTimeUtc = new DateTime(2018, 9, 6, 6, 35, 7),
+            Tags = new[]
+                {
+                    new Tag { DisplayName = "Fubao", Id = 996, NormalizedName = "fubao" },
+                    new Tag { DisplayName = "996", Id = 251, NormalizedName = "996" }
+                },
+            Categories = new[] { Cat }
+        };
 
         [SetUp]
         public void SetUp()
@@ -131,41 +164,9 @@ namespace Moonglade.Web.Tests.Controllers
         [Test]
         public async Task Edit_View()
         {
-            var cat = new Category
-            {
-                DisplayName = "WTF",
-                Id = Guid.Parse("6364e9be-2423-44da-bd11-bc6fa9c3fa5d"),
-                Note = "A wonderful contry",
-                RouteName = "wtf"
-            };
+            IReadOnlyList<Category> cats = new List<Category> { Cat };
 
-            var post = new Post
-            {
-                Id = Uid,
-                Title = "Work 996 and Get into ICU",
-                Slug = "work-996-and-get-into-icu",
-                ContentAbstract = "Get some fubao",
-                RawPostContent = "<p>Get some fubao</p>",
-                ContentLanguageCode = "en-us",
-                Featured = true,
-                ExposedToSiteMap = true,
-                IsFeedIncluded = true,
-                IsPublished = true,
-                CommentEnabled = true,
-                PubDateUtc = new DateTime(2019, 9, 6, 6, 35, 7),
-                LastModifiedUtc = new DateTime(2020, 9, 6, 6, 35, 7),
-                CreateTimeUtc = new DateTime(2018, 9, 6, 6, 35, 7),
-                Tags = new[]
-                {
-                    new Tag { DisplayName = "Fubao", Id = 996, NormalizedName = "fubao" },
-                    new Tag { DisplayName = "996", Id = 251, NormalizedName = "996" }
-                },
-                Categories = new[] { cat }
-            };
-
-            IReadOnlyList<Category> cats = new List<Category> { cat };
-
-            _mockPostService.Setup(p => p.GetAsync(Uid)).Returns(Task.FromResult(post));
+            _mockPostService.Setup(p => p.GetAsync(Uid)).Returns(Task.FromResult(Post));
             _mockCategoryService.Setup(p => p.GetAll()).Returns(Task.FromResult(cats));
 
             var postManageController = CreatePostManageController();
@@ -186,6 +187,40 @@ namespace Moonglade.Web.Tests.Controllers
             var result = await postManageController.CreateOrEdit(model, mockLinkGenerator.Object, mockPingbackSender.Object);
 
             Assert.IsInstanceOf<ConflictObjectResult>(result);
+        }
+
+        [Test]
+        public async Task CreateOrEdit_Create_Draft()
+        {
+            var postManageController = CreatePostManageController();
+            PostEditModel model = new()
+            {
+                PostId = Guid.Empty,
+                Title = Post.Title,
+                Slug = Post.Slug,
+                EditorContent = Post.RawPostContent,
+                LanguageCode = Post.ContentLanguageCode,
+                IsPublished = false,
+                Featured = true,
+                ExposedToSiteMap = true,
+                ChangePublishDate = false,
+                EnableComment = true,
+                FeedIncluded = true,
+                Tags = "996,icu",
+                CategoryList = new List<CheckBoxViewModel>(),
+                SelectedCategoryIds = new[] { Guid.Parse("6364e9be-2423-44da-bd11-bc6fa9c3fa5d") }
+            };
+
+            Mock<LinkGenerator> mockLinkGenerator = new();
+            Mock<IPingbackSender> mockPingbackSender = new();
+
+            _mockPostService.Setup(p => p.CreateAsync(It.IsAny<UpdatePostRequest>())).Returns(Task.FromResult(new PostEntity
+            {
+                Id = Uid
+            }));
+
+            var result = await postManageController.CreateOrEdit(model, mockLinkGenerator.Object, mockPingbackSender.Object);
+            Assert.IsInstanceOf<JsonResult>(result);
         }
 
         [Test]
