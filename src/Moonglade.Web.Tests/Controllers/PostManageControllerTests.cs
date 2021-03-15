f2c75540c1f9060be23e1d260152762c1e5839e7
Edi Wang <Edi.Wang@outlook.com>
2021-03-15 10:40:18 +0800

    Create PostManageControllerTests.cs

---
+++ src/Moonglade.Web.Tests/Controllers/PostManageControllerTests.cs
@@ -0,0 +1,221 @@
+using Microsoft.Extensions.Logging;
+using Moonglade.Configuration.Abstraction;
+using Moonglade.Core;
+using Moonglade.Web;
+using Moonglade.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Routing;
+using Moonglade.Pingback;
+using Moonglade.Web.Models;
+
+namespace Moonglade.Web.Tests.Controllers
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class PostManageControllerTests
+    {
+        private MockRepository _mockRepository;
+
+        private Mock<IPostService> _mockPostService;
+        private Mock<ICategoryService> _mockCategoryService;
+        private Mock<IBlogConfig> _mockBlogConfig;
+        private Mock<ITZoneResolver> _mockTZoneResolver;
+        private Mock<ILogger<PostManageController>> _mockLogger;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockRepository = new MockRepository(MockBehavior.Default);
+
+            _mockPostService = _mockRepository.Create<IPostService>();
+            _mockCategoryService = _mockRepository.Create<ICategoryService>();
+            _mockBlogConfig = _mockRepository.Create<IBlogConfig>();
+            _mockTZoneResolver = _mockRepository.Create<ITZoneResolver>();
+            _mockLogger = _mockRepository.Create<ILogger<PostManageController>>();
+        }
+
+        private PostManageController CreatePostManageController()
+        {
+            return new(
+                _mockPostService.Object,
+                _mockCategoryService.Object,
+                _mockBlogConfig.Object,
+                _mockTZoneResolver.Object,
+                _mockLogger.Object);
+        }
+
+        //[Test]
+        //public async Task ListPublished_StateUnderTest_ExpectedBehavior()
+        //{
+        //    // Arrange
+        //    var postManageController = CreatePostManageController();
+        //    DataTableRequest model = null;
+
+        //    // Act
+        //    var result = await postManageController.ListPublished(
+        //        model);
+
+        //    // Assert
+        //    Assert.Fail();
+        //    _mockRepository.VerifyAll();
+        //}
+
+        //[Test]
+        //public async Task Draft_StateUnderTest_ExpectedBehavior()
+        //{
+        //    // Arrange
+        //    var postManageController = CreatePostManageController();
+
+        //    // Act
+        //    var result = await postManageController.Draft();
+
+        //    // Assert
+        //    Assert.Fail();
+        //    _mockRepository.VerifyAll();
+        //}
+
+        //[Test]
+        //public async Task RecycleBin_StateUnderTest_ExpectedBehavior()
+        //{
+        //    // Arrange
+        //    var postManageController = CreatePostManageController();
+
+        //    // Act
+        //    var result = await postManageController.RecycleBin();
+
+        //    // Assert
+        //    Assert.Fail();
+        //    _mockRepository.VerifyAll();
+        //}
+
+        //[Test]
+        //public async Task Create_StateUnderTest_ExpectedBehavior()
+        //{
+        //    // Arrange
+        //    var postManageController = CreatePostManageController();
+
+        //    // Act
+        //    var result = await postManageController.Create();
+
+        //    // Assert
+        //    Assert.Fail();
+        //    _mockRepository.VerifyAll();
+        //}
+
+        //[Test]
+        //public async Task Edit_StateUnderTest_ExpectedBehavior()
+        //{
+        //    // Arrange
+        //    var postManageController = CreatePostManageController();
+        //    Guid id = default(Guid);
+
+        //    // Act
+        //    var result = await postManageController.Edit(
+        //        id);
+
+        //    // Assert
+        //    Assert.Fail();
+        //    _mockRepository.VerifyAll();
+        //}
+
+        //[Test]
+        //public async Task CreateOrEdit_StateUnderTest_ExpectedBehavior()
+        //{
+        //    // Arrange
+        //    var postManageController = CreatePostManageController();
+        //    PostEditModel model = null;
+        //    LinkGenerator linkGenerator = null;
+        //    IPingbackSender pingbackSender = null;
+
+        //    // Act
+        //    var result = await postManageController.CreateOrEdit(
+        //        model,
+        //        linkGenerator,
+        //        pingbackSender);
+
+        //    // Assert
+        //    Assert.Fail();
+        //    _mockRepository.VerifyAll();
+        //}
+
+        //[Test]
+        //public async Task Restore_StateUnderTest_ExpectedBehavior()
+        //{
+        //    // Arrange
+        //    var postManageController = CreatePostManageController();
+        //    Guid postId = default(Guid);
+
+        //    // Act
+        //    var result = await postManageController.Restore(
+        //        postId);
+
+        //    // Assert
+        //    Assert.Fail();
+        //    _mockRepository.VerifyAll();
+        //}
+
+        //[Test]
+        //public async Task Delete_StateUnderTest_ExpectedBehavior()
+        //{
+        //    // Arrange
+        //    var postManageController = CreatePostManageController();
+        //    Guid postId = default(Guid);
+
+        //    // Act
+        //    var result = await postManageController.Delete(
+        //        postId);
+
+        //    // Assert
+        //    Assert.Fail();
+        //    _mockRepository.VerifyAll();
+        //}
+
+        //[Test]
+        //public async Task DeleteFromRecycleBin_StateUnderTest_ExpectedBehavior()
+        //{
+        //    // Arrange
+        //    var postManageController = CreatePostManageController();
+        //    Guid postId = default(Guid);
+
+        //    // Act
+        //    var result = await postManageController.DeleteFromRecycleBin(
+        //        postId);
+
+        //    // Assert
+        //    Assert.Fail();
+        //    _mockRepository.VerifyAll();
+        //}
+
+        //[Test]
+        //public async Task EmptyRecycleBin_StateUnderTest_ExpectedBehavior()
+        //{
+        //    // Arrange
+        //    var postManageController = CreatePostManageController();
+
+        //    // Act
+        //    var result = await postManageController.EmptyRecycleBin();
+
+        //    // Assert
+        //    Assert.Fail();
+        //    _mockRepository.VerifyAll();
+        //}
+
+        //[Test]
+        //public async Task Insights_StateUnderTest_ExpectedBehavior()
+        //{
+        //    // Arrange
+        //    var postManageController = CreatePostManageController();
+
+        //    // Act
+        //    var result = await postManageController.Insights();
+
+        //    // Assert
+        //    Assert.Fail();
+        //    _mockRepository.VerifyAll();
+        //}
+    }
+}
