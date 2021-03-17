b28ee21f6b532f41af77ccf3e38da0ce4812b703
Edi Wang <Edi.Wang@outlook.com>
2021-03-17 13:43:52 +0800

    Update PageControllerTests.cs

--- src/Moonglade.Web.Tests/Controllers/PageControllerTests.cs
+++ src/Moonglade.Web.Tests/Controllers/PageControllerTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -56,6 +57,39 @@ namespace Moonglade.Web.Tests.Controllers
 
             var result = await ctl.CreateOrEdit(new());
             Assert.IsInstanceOf<BadRequestObjectResult>(result);
+        }
+
+        [Test]
+        public async Task Segment_OK()
+        {
+            IReadOnlyList<PageSegment> ps = new List<PageSegment>
+            {
+                new()
+                {
+                    Id = Guid.Empty,
+                    CreateTimeUtc = new DateTime(1996,9,9,6,3,5),
+                    IsPublished = true,
+                    Slug = "work-996",
+                    Title = "Work 996"
+                }
+            };
+            _mockPageService.Setup(p => p.ListSegment()).Returns(Task.FromResult(ps));
+
+            var ctl = CreatePageController();
+            var result = await ctl.Segment();
+
+            Assert.IsInstanceOf<OkObjectResult>(result);
+        }
+
+        [Test]
+        public async Task Segment_Error()
+        {
+            _mockPageService.Setup(p => p.ListSegment()).Returns(Task.FromResult((IReadOnlyList<PageSegment>)null));
+
+            var ctl = CreatePageController();
+            var result = await ctl.Segment();
+
+            Assert.IsInstanceOf<StatusCodeResult>(result);
         }
     }
 }
