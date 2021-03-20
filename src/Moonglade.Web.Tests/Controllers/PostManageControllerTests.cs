385a36d96afa06da3d59e40f5d31f835d8d63070
Edi Wang <Edi.Wang@outlook.com>
2021-03-20 20:34:48 +0800

    Update PostManageControllerTests.cs

--- src/Moonglade.Web.Tests/Controllers/PostManageControllerTests.cs
+++ src/Moonglade.Web.Tests/Controllers/PostManageControllerTests.cs
@@ -32,7 +32,38 @@ namespace Moonglade.Web.Tests.Controllers
         private Mock<ILogger<PostManageController>> _mockLogger;
 
         private static readonly Guid Uid = Guid.Parse("76169567-6ff3-42c0-b163-a883ff2ac4fb");
-        
+        private static readonly Category Cat = new()
+        {
+            DisplayName = "WTF",
+            Id = Guid.Parse("6364e9be-2423-44da-bd11-bc6fa9c3fa5d"),
+            Note = "A wonderful contry",
+            RouteName = "wtf"
+        };
+
+        private static readonly Post Post = new()
+        {
+            Id = Uid,
+            Title = "Work 996 and Get into ICU",
+            Slug = "work-996-and-get-into-icu",
+            ContentAbstract = "Get some fubao",
+            RawPostContent = "<p>Get some fubao</p>",
+            ContentLanguageCode = "en-us",
+            Featured = true,
+            ExposedToSiteMap = true,
+            IsFeedIncluded = true,
+            IsPublished = true,
+            CommentEnabled = true,
+            PubDateUtc = new(2019, 9, 6, 6, 35, 7),
+            LastModifiedUtc = new(2020, 9, 6, 6, 35, 7),
+            CreateTimeUtc = new(2018, 9, 6, 6, 35, 7),
+            Tags = new[]
+            {
+                new Tag { DisplayName = "Fubao", Id = 996, NormalizedName = "fubao" },
+                new Tag { DisplayName = "996", Id = 251, NormalizedName = "996" }
+            },
+            Categories = new[] { Cat }
+        };
+
         [SetUp]
         public void SetUp()
         {
@@ -73,161 +104,170 @@ namespace Moonglade.Web.Tests.Controllers
             Assert.IsInstanceOf<JsonResult>(result);
         }
 
-        //[Test]
-        //public async Task CreateOrEdit_BadModelState()
-        //{
-        //    var postManageController = CreatePostManageController();
-        //    postManageController.ModelState.AddModelError("", "996");
+        [Test]
+        public async Task CreateOrEdit_BadModelState()
+        {
+            var postManageController = CreatePostManageController();
+            postManageController.ModelState.AddModelError("", "996");
 
-        //    PostEditModel model = new();
-        //    Mock<LinkGenerator> mockLinkGenerator = new();
-        //    Mock<IPingbackSender> mockPingbackSender = new();
+            PostEditModelWrapper model = new();
+            Mock<LinkGenerator> mockLinkGenerator = new();
+            Mock<IPingbackSender> mockPingbackSender = new();
 
-        //    var result = await postManageController.CreateOrEdit(model, mockLinkGenerator.Object, mockPingbackSender.Object);
+            var result = await postManageController.CreateOrEdit(model, mockLinkGenerator.Object, mockPingbackSender.Object);
 
-        //    Assert.IsInstanceOf<ConflictObjectResult>(result);
-        //}
+            Assert.IsInstanceOf<ConflictObjectResult>(result);
+        }
 
-        //[Test]
-        //public async Task CreateOrEdit_Exception()
-        //{
-        //    var postManageController = CreatePostManageController();
-        //    postManageController.ControllerContext = new()
-        //    {
-        //        HttpContext = new DefaultHttpContext()
-        //    };
+        [Test]
+        public async Task CreateOrEdit_Exception()
+        {
+            var postManageController = CreatePostManageController();
+            postManageController.ControllerContext = new()
+            {
+                HttpContext = new DefaultHttpContext()
+            };
 
-        //    PostEditModel model = new()
-        //    {
-        //        PostId = Guid.Empty,
-        //        Title = Post.Title,
-        //        Slug = Post.Slug,
-        //        EditorContent = Post.RawPostContent,
-        //        LanguageCode = Post.ContentLanguageCode,
-        //        IsPublished = false,
-        //        Featured = true,
-        //        ExposedToSiteMap = true,
-        //        ChangePublishDate = false,
-        //        EnableComment = true,
-        //        FeedIncluded = true,
-        //        Tags = "996,icu",
-        //        CategoryList = new List<CheckBoxViewModel>(),
-        //        SelectedCategoryIds = new[] { Guid.Parse("6364e9be-2423-44da-bd11-bc6fa9c3fa5d") }
-        //    };
+            PostEditModelWrapper model = new()
+            {
+                PostEditModel = new()
+                {
+                    PostId = Guid.Empty,
+                    Title = Post.Title,
+                    Slug = Post.Slug,
+                    EditorContent = Post.RawPostContent,
+                    LanguageCode = Post.ContentLanguageCode,
+                    IsPublished = false,
+                    Featured = true,
+                    ExposedToSiteMap = true,
+                    ChangePublishDate = false,
+                    EnableComment = true,
+                    FeedIncluded = true,
+                    Tags = "996,icu",
+                    CategoryList = new List<CheckBoxViewModel>(),
+                    SelectedCategoryIds = new[] { Guid.Parse("6364e9be-2423-44da-bd11-bc6fa9c3fa5d") }
+                }
+            };
 
-        //    Mock<LinkGenerator> mockLinkGenerator = new();
-        //    Mock<IPingbackSender> mockPingbackSender = new();
+            Mock<LinkGenerator> mockLinkGenerator = new();
+            Mock<IPingbackSender> mockPingbackSender = new();
 
-        //    _mockPostService.Setup(p => p.CreateAsync(It.IsAny<UpdatePostRequest>())).Throws(new("Work 996"));
+            _mockPostService.Setup(p => p.CreateAsync(It.IsAny<UpdatePostRequest>())).Throws(new("Work 996"));
 
-        //    var result = await postManageController.CreateOrEdit(model, mockLinkGenerator.Object, mockPingbackSender.Object);
-        //    Assert.IsInstanceOf<JsonResult>(result);
+            var result = await postManageController.CreateOrEdit(model, mockLinkGenerator.Object, mockPingbackSender.Object);
+            Assert.IsInstanceOf<JsonResult>(result);
 
-        //    var statusCode = postManageController.HttpContext.Response.StatusCode;
-        //    Assert.AreEqual(500, statusCode);
+            var statusCode = postManageController.HttpContext.Response.StatusCode;
+            Assert.AreEqual(500, statusCode);
 
-        //    mockPingbackSender.Verify(p => p.TrySendPingAsync(It.IsAny<string>(), It.IsAny<string>()), Times.Never);
-        //}
+            mockPingbackSender.Verify(p => p.TrySendPingAsync(It.IsAny<string>(), It.IsAny<string>()), Times.Never);
+        }
 
-        //[Test]
-        //public async Task CreateOrEdit_Create_Draft()
-        //{
-        //    var postManageController = CreatePostManageController();
-        //    PostEditModel model = new()
-        //    {
-        //        PostId = Guid.Empty,
-        //        Title = Post.Title,
-        //        Slug = Post.Slug,
-        //        EditorContent = Post.RawPostContent,
-        //        LanguageCode = Post.ContentLanguageCode,
-        //        IsPublished = false,
-        //        Featured = true,
-        //        ExposedToSiteMap = true,
-        //        ChangePublishDate = false,
-        //        EnableComment = true,
-        //        FeedIncluded = true,
-        //        Tags = "996,icu",
-        //        CategoryList = new List<CheckBoxViewModel>(),
-        //        SelectedCategoryIds = new[] { Guid.Parse("6364e9be-2423-44da-bd11-bc6fa9c3fa5d") }
-        //    };
+        [Test]
+        public async Task CreateOrEdit_Create_Draft()
+        {
+            var postManageController = CreatePostManageController();
+            PostEditModelWrapper model = new()
+            {
+                PostEditModel = new()
+                {
+                    PostId = Guid.Empty,
+                    Title = Post.Title,
+                    Slug = Post.Slug,
+                    EditorContent = Post.RawPostContent,
+                    LanguageCode = Post.ContentLanguageCode,
+                    IsPublished = false,
+                    Featured = true,
+                    ExposedToSiteMap = true,
+                    ChangePublishDate = false,
+                    EnableComment = true,
+                    FeedIncluded = true,
+                    Tags = "996,icu",
+                    CategoryList = new List<CheckBoxViewModel>(),
+                    SelectedCategoryIds = new[] { Guid.Parse("6364e9be-2423-44da-bd11-bc6fa9c3fa5d") }
+                }
+            };
 
-        //    Mock<LinkGenerator> mockLinkGenerator = new();
-        //    Mock<IPingbackSender> mockPingbackSender = new();
+            Mock<LinkGenerator> mockLinkGenerator = new();
+            Mock<IPingbackSender> mockPingbackSender = new();
 
-        //    _mockPostService.Setup(p => p.CreateAsync(It.IsAny<UpdatePostRequest>())).Returns(Task.FromResult(new PostEntity
-        //    {
-        //        Id = Uid
-        //    }));
+            _mockPostService.Setup(p => p.CreateAsync(It.IsAny<UpdatePostRequest>())).Returns(Task.FromResult(new PostEntity
+            {
+                Id = Uid
+            }));
 
-        //    var result = await postManageController.CreateOrEdit(model, mockLinkGenerator.Object, mockPingbackSender.Object);
-        //    Assert.IsInstanceOf<JsonResult>(result);
-        //    mockPingbackSender.Verify(p => p.TrySendPingAsync(It.IsAny<string>(), It.IsAny<string>()), Times.Never);
-        //}
+            var result = await postManageController.CreateOrEdit(model, mockLinkGenerator.Object, mockPingbackSender.Object);
+            Assert.IsInstanceOf<JsonResult>(result);
+            mockPingbackSender.Verify(p => p.TrySendPingAsync(It.IsAny<string>(), It.IsAny<string>()), Times.Never);
+        }
 
-        //[Test]
-        //public async Task CreateOrEdit_Create_Publish_EnablePingback()
-        //{
-        //    var postManageController = CreatePostManageController();
-        //    postManageController.ControllerContext = new()
-        //    {
-        //        HttpContext = new DefaultHttpContext()
-        //    };
+        [Test]
+        public async Task CreateOrEdit_Create_Publish_EnablePingback()
+        {
+            var postManageController = CreatePostManageController();
+            postManageController.ControllerContext = new()
+            {
+                HttpContext = new DefaultHttpContext()
+            };
 
-        //    PostEditModel model = new()
-        //    {
-        //        PostId = Guid.Empty,
-        //        Title = Post.Title,
-        //        Slug = Post.Slug,
-        //        EditorContent = Post.RawPostContent,
-        //        LanguageCode = Post.ContentLanguageCode,
-        //        IsPublished = true,
-        //        Featured = true,
-        //        ExposedToSiteMap = true,
-        //        ChangePublishDate = false,
-        //        EnableComment = true,
-        //        FeedIncluded = true,
-        //        Tags = "996,icu",
-        //        CategoryList = new List<CheckBoxViewModel>(),
-        //        SelectedCategoryIds = new[] { Guid.Parse("6364e9be-2423-44da-bd11-bc6fa9c3fa5d") }
-        //    };
+            PostEditModelWrapper model = new()
+            {
+                PostEditModel = new()
+                {
+                    PostId = Guid.Empty,
+                    Title = Post.Title,
+                    Slug = Post.Slug,
+                    EditorContent = Post.RawPostContent,
+                    LanguageCode = Post.ContentLanguageCode,
+                    IsPublished = false,
+                    Featured = true,
+                    ExposedToSiteMap = true,
+                    ChangePublishDate = false,
+                    EnableComment = true,
+                    FeedIncluded = true,
+                    Tags = "996,icu",
+                    CategoryList = new List<CheckBoxViewModel>(),
+                    SelectedCategoryIds = new[] { Guid.Parse("6364e9be-2423-44da-bd11-bc6fa9c3fa5d") }
+                }
+            };
 
-        //    Mock<LinkGenerator> mockLinkGenerator = new();
-        //    mockLinkGenerator.Setup(p => p.GetUriByAddress(
-        //        It.IsAny<HttpContext>(),
-        //        It.IsAny<RouteValuesAddress>(),
-        //        It.IsAny<RouteValueDictionary>(),
-        //        It.IsAny<RouteValueDictionary>(),
-        //        It.IsAny<string>(),
-        //        It.IsAny<HostString>(),
-        //        It.IsAny<PathString>(),
-        //        It.IsAny<FragmentString>(),
-        //        It.IsAny<LinkOptions>()
-        //        ))
-        //        .Returns("https://996.icu/1996/7/2/work-996-and-get-into-icu");
+            Mock<LinkGenerator> mockLinkGenerator = new();
+            mockLinkGenerator.Setup(p => p.GetUriByAddress(
+                It.IsAny<HttpContext>(),
+                It.IsAny<RouteValuesAddress>(),
+                It.IsAny<RouteValueDictionary>(),
+                It.IsAny<RouteValueDictionary>(),
+                It.IsAny<string>(),
+                It.IsAny<HostString>(),
+                It.IsAny<PathString>(),
+                It.IsAny<FragmentString>(),
+                It.IsAny<LinkOptions>()
+                ))
+                .Returns("https://996.icu/1996/7/2/work-996-and-get-into-icu");
 
-        //    Mock<IPingbackSender> mockPingbackSender = new();
-        //    var trySendPingAsyncCalled = new ManualResetEvent(false);
-        //    mockPingbackSender.Setup(p => p.TrySendPingAsync(It.IsAny<string>(), It.IsAny<string>())).Callback(() =>
-        //    {
-        //        trySendPingAsyncCalled.Set();
-        //    });
+            Mock<IPingbackSender> mockPingbackSender = new();
+            var trySendPingAsyncCalled = new ManualResetEvent(false);
+            mockPingbackSender.Setup(p => p.TrySendPingAsync(It.IsAny<string>(), It.IsAny<string>())).Callback(() =>
+            {
+                trySendPingAsyncCalled.Set();
+            });
 
-        //    _mockPostService.Setup(p => p.CreateAsync(It.IsAny<UpdatePostRequest>())).Returns(Task.FromResult(new PostEntity
-        //    {
-        //        Id = Uid,
-        //        PubDateUtc = new(1996, 7, 2, 5, 1, 0),
-        //        ContentAbstract = Post.ContentAbstract
-        //    }));
+            _mockPostService.Setup(p => p.CreateAsync(It.IsAny<UpdatePostRequest>())).Returns(Task.FromResult(new PostEntity
+            {
+                Id = Uid,
+                PubDateUtc = new(1996, 7, 2, 5, 1, 0),
+                ContentAbstract = Post.ContentAbstract
+            }));
 
-        //    _mockBlogConfig.Setup(p => p.AdvancedSettings).Returns(new AdvancedSettings { EnablePingBackSend = true });
+            _mockBlogConfig.Setup(p => p.AdvancedSettings).Returns(new AdvancedSettings { EnablePingBackSend = true });
 
-        //    var result = await postManageController.CreateOrEdit(model, mockLinkGenerator.Object, mockPingbackSender.Object);
+            var result = await postManageController.CreateOrEdit(model, mockLinkGenerator.Object, mockPingbackSender.Object);
 
-        //    trySendPingAsyncCalled.WaitOne(TimeSpan.FromSeconds(2));
-        //    Assert.IsInstanceOf<JsonResult>(result);
+            trySendPingAsyncCalled.WaitOne(TimeSpan.FromSeconds(2));
+            Assert.IsInstanceOf<JsonResult>(result);
 
-        //    mockPingbackSender.Verify(p => p.TrySendPingAsync(It.IsAny<string>(), It.IsAny<string>()));
-        //}
+            mockPingbackSender.Verify(p => p.TrySendPingAsync(It.IsAny<string>(), It.IsAny<string>()));
+        }
 
         [Test]
         public async Task Restore_EmptyId()
