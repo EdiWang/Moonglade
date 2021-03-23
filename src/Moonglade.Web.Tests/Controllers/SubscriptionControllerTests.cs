56234b05e514054e052a17f1af4cb62b2f3ba455
Edi Wang <Edi.Wang@outlook.com>
2021-03-23 11:05:57 +0800

    Update SubscriptionControllerTests.cs

--- src/Moonglade.Web.Tests/Controllers/SubscriptionControllerTests.cs
+++ src/Moonglade.Web.Tests/Controllers/SubscriptionControllerTests.cs
@@ -6,8 +6,12 @@ using Moonglade.Web.Controllers;
 using Moq;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moonglade.Configuration;
 
 namespace Moonglade.Web.Tests.Controllers
 {
@@ -45,6 +49,33 @@ namespace Moonglade.Web.Tests.Controllers
                 _mockOpmlWriter.Object);
         }
 
-        
+        [Test]
+        public async Task Opml_Content()
+        {
+            IReadOnlyList<Category> cats = new List<Category>
+            {
+                new()
+                {
+                    Id = Guid.Empty, DisplayName = "Work 996", Note = "This is fubao", RouteName = "work-996"
+                }
+            };
+
+            _mockCategoryService.Setup(p => p.GetAll()).Returns(Task.FromResult(cats));
+            _mockBlogConfig.Setup(p => p.GeneralSettings).Returns(new GeneralSettings
+            {
+                CanonicalPrefix = "https://996.icu",
+                SiteTitle = "996 ICU"
+            });
+
+            var ctl = CreateSubscriptionController();
+            ctl.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+
+            var result = await ctl.Opml();
+            Assert.IsInstanceOf<ContentResult>(result);
+            Assert.AreEqual("text/xml", ((ContentResult)result).ContentType);
+        }
     }
 }
