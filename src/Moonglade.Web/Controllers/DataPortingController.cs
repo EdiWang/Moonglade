06611e8dd6ec7d73eed58c28716a532150f08c81
Edi Wang <Edi.Wang@outlook.com>
2021-03-27 08:20:14 +0800

    Create DataPortingController.cs

---
+++ src/Moonglade.Web/Controllers/DataPortingController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Moonglade.DataPorting;
+using Moonglade.Utils;
+using Moonglade.Web.Filters;
+
+namespace Moonglade.Web.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [AppendAppVersion]
+    [Route("api/[controller]")]
+    public class DataPortingController : ControllerBase
+    {
+        private readonly IExportManager _expman;
+
+        public DataPortingController(IExportManager expman)
+        {
+            _expman = expman;
+        }
+
+        [HttpGet("export/{type}")]
+        public async Task<IActionResult> ExportDownload(ExportDataType type)
+        {
+            var exportResult = await _expman.ExportData(type);
+            switch (exportResult.ExportFormat)
+            {
+                case ExportFormat.SingleJsonFile:
+                    return new FileContentResult(exportResult.Content, exportResult.ContentType)
+                    {
+                        FileDownloadName = $"moonglade-{type.ToString().ToLowerInvariant()}-{DateTime.UtcNow:yyyy-MM-dd-HH-mm-ss}.json"
+                    };
+                case ExportFormat.SingleCSVFile:
+                    Response.Headers.Add("Content-Disposition", $"attachment;filename={Path.GetFileName(exportResult.FilePath)}");
+                    return PhysicalFile(exportResult.FilePath, exportResult.ContentType, Path.GetFileName(exportResult.FilePath));
+                case ExportFormat.ZippedJsonFiles:
+                    return PhysicalFile(exportResult.FilePath, exportResult.ContentType, Path.GetFileName(exportResult.FilePath));
+                default:
+                    return BadRequest(ModelState.CombineErrorMessages());
+            }
+        }
+    }
+}
