b30b435fab9888a319c4dd2723b2456eea7d4690
Edi Wang <Edi.Wang@outlook.com>
2021-03-27 08:38:43 +0800

    Update PostServiceTests.cs

--- src/Moonglade.Core.Tests/PostServiceTests.cs
+++ src/Moonglade.Core.Tests/PostServiceTests.cs
@@ -1,6 +1,19 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using Moonglade.Utils;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moonglade.Auditing;
+using Moonglade.Caching;
+using Moonglade.Configuration.Settings;
+using Moonglade.Core;
+using Moonglade.Data.Entities;
+using Moonglade.Data.Infrastructure;
+using Moq;
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Moonglade.Data.Spec;
+using Moonglade.Utils;
 
 namespace Moonglade.Core.Tests
 {
@@ -8,6 +21,48 @@ namespace Moonglade.Core.Tests
     [ExcludeFromCodeCoverage]
     public class PostServiceTests
     {
+        private MockRepository _mockRepository;
+
+        private Mock<ILogger<PostService>> _mockLogger;
+        private Mock<IOptions<AppSettings>> _mockOptionsAppSettings;
+        private Mock<IRepository<PostEntity>> _mockRepositoryPostEntity;
+        private Mock<IRepository<TagEntity>> _mockRepositoryTagEntity;
+        private Mock<IRepository<PostTagEntity>> _mockRepositoryPostTagEntity;
+        private Mock<IRepository<PostCategoryEntity>> _mockRepositoryPostCategoryEntity;
+        private Mock<IBlogAudit> _mockBlogAudit;
+        private Mock<IBlogCache> _mockBlogCache;
+        private Mock<IOptions<List<TagNormalization>>> _mockOptionsListTagNormalization;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockRepository = new(MockBehavior.Default);
+
+            _mockLogger = _mockRepository.Create<ILogger<PostService>>();
+            _mockOptionsAppSettings = _mockRepository.Create<IOptions<AppSettings>>();
+            _mockRepositoryPostEntity = _mockRepository.Create<IRepository<PostEntity>>();
+            _mockRepositoryTagEntity = _mockRepository.Create<IRepository<TagEntity>>();
+            _mockRepositoryPostTagEntity = _mockRepository.Create<IRepository<PostTagEntity>>();
+            _mockRepositoryPostCategoryEntity = _mockRepository.Create<IRepository<PostCategoryEntity>>();
+            _mockBlogAudit = _mockRepository.Create<IBlogAudit>();
+            _mockBlogCache = _mockRepository.Create<IBlogCache>();
+            _mockOptionsListTagNormalization = _mockRepository.Create<IOptions<List<TagNormalization>>>();
+        }
+
+        private PostService CreateService()
+        {
+            return new(
+                _mockLogger.Object,
+                _mockOptionsAppSettings.Object,
+                _mockRepositoryPostEntity.Object,
+                _mockRepositoryTagEntity.Object,
+                _mockRepositoryPostTagEntity.Object,
+                _mockRepositoryPostCategoryEntity.Object,
+                _mockBlogAudit.Object,
+                _mockBlogCache.Object,
+                _mockOptionsListTagNormalization.Object);
+        }
+
         [Test]
         public void LazyLoadToImgTag_ExistLoading()
         {
