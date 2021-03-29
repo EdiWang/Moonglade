31cc44a12cf280d74057e732b06c2fcd23871009
Edi Wang <Edi.Wang@outlook.com>
2021-03-29 10:14:49 +0800

    Create ManifestMiddlewareTests.cs

---
+++ src/Moonglade.Web.Tests/Middleware/ManifestMiddlewareTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Moonglade.Web.Middleware;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Moonglade.Configuration;
+using Moonglade.Configuration.Abstraction;
+
+namespace Moonglade.Web.Tests.Middleware
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class ManifestMiddlewareTests
+    {
+        private MockRepository _mockRepository;
+        private Mock<IBlogConfig> _mockBlogConfig;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockRepository = new(MockBehavior.Default);
+            _mockBlogConfig = _mockRepository.Create<IBlogConfig>();
+
+            _mockBlogConfig.Setup(bc => bc.GeneralSettings).Returns(new GeneralSettings
+            {
+                SiteTitle = "Fake Title"
+            });
+        }
+
+        [Test]
+        public void UseManifestMiddlewareExtensions()
+        {
+            var serviceCollection = new ServiceCollection();
+            var applicationBuilder = new ApplicationBuilder(serviceCollection.BuildServiceProvider());
+
+            applicationBuilder.UseManifest(options => { });
+
+            var app = applicationBuilder.Build();
+
+            var type = app.Target.GetType();
+            Assert.AreEqual(nameof(UseMiddlewareExtensions), type.DeclaringType.Name);
+        }
+    }
+}
