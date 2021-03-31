e4a8e88533b00e488d7fc4b385f72fd8f6f2568e
Edi Wang <Edi.Wang@outlook.com>
2021-03-31 14:24:08 +0800

    Create CaptchaImageMiddlewareTests.cs

---
+++ src/Moonglade.Web.Tests/Middleware/CaptchaImageMiddlewareTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Moonglade.Web.Middleware;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Edi.Captcha;
+using Microsoft.Extensions.Options;
+using Moonglade.Configuration.Settings;
+
+namespace Moonglade.Web.Tests.Middleware
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class CaptchaImageMiddlewareTests
+    {
+        private MockRepository _mockRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockRepository = new MockRepository(MockBehavior.Default);
+        }
+    }
+}
