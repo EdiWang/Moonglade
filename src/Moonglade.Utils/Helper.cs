a20bbcf005f6eed4fd5e0c42ed1b4bc4dff7b322
Edi Wang <Edi.Wang@outlook.com>
2021-04-03 13:32:09 +0800

    fix version display

--- src/Moonglade.Utils/Helper.cs
+++ src/Moonglade.Utils/Helper.cs
@@ -21,18 +21,22 @@ namespace Moonglade.Utils
             {
                 var asm = Assembly.GetEntryAssembly();
                 if (null == asm) return "N/A";
+
+                // e.g. 11.2.0.0
                 var fileVersion = asm.GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version;
 
                 // e.g. 11.2-preview+e57ab0321ae44bd778c117646273a77123b6983f
                 var version = asm.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
                 if (!string.IsNullOrWhiteSpace(version) && version.IndexOf('+') > 0)
                 {
-                    var gitHash = version[(version.IndexOf('+') + 1)..];
+                    var gitHash = version[(version.IndexOf('+') + 1)..]; // e57ab0321ae44bd778c117646273a77123b6983f
+                    var prefix = version[..version.IndexOf('+')]; // 11.2-preview
+
                     if (gitHash.Length <= 6) return version;
 
                     // consider valid hash
                     var gitHashShort = gitHash.Substring(gitHash.Length - 6, 6);
-                    return !string.IsNullOrWhiteSpace(gitHashShort) ? $"{fileVersion} ({gitHashShort})" : fileVersion;
+                    return !string.IsNullOrWhiteSpace(gitHashShort) ? $"{prefix} ({gitHashShort})" : fileVersion;
                 }
 
                 return version ?? fileVersion;
