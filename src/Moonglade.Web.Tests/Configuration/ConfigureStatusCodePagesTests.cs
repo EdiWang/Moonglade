fb334c57462b77933041d3777e9218f70ddaa3e2
Edi Wang <Edi.Wang@outlook.com>
2021-04-08 12:44:45 +0800

    Create ConfigureStatusCodePagesTests.cs

---
+++ src/Moonglade.Web.Tests/Configuration/ConfigureStatusCodePagesTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Moonglade.Web.Configuration;
+using NUnit.Framework;
+
+namespace Moonglade.Web.Tests.Configuration
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class ConfigureStatusCodePagesTests
+    {
+        [Test]
+        public async Task Handler_OK()
+        {
+            var ctx = new DefaultHttpContext();
+            ctx.HttpContext.Response.StatusCode = 404;
+
+            var context = new StatusCodeContext(ctx, new(),
+                _ => Task.CompletedTask);
+
+            var task = ConfigureStatusCodePages.Handler(context);
+            await task;
+
+            Assert.AreEqual("application/json; charset=utf-8", ctx.HttpContext.Response.ContentType);
+        }
+    }
+}
