feecaa5628095550645c5de49e298ca8d0ff2754
Edi Wang <Edi.Wang@outlook.com>
2021-04-09 19:30:42 +0800

    Create ConfigureApiBehaviorTests.cs

---
+++ src/Moonglade.Web.Tests/Configuration/ConfigureApiBehaviorTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Mvc;
+using Moonglade.Web.Configuration;
+using NUnit.Framework;
+
+namespace Moonglade.Web.Tests.Configuration
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class ConfigureApiBehaviorTests
+    {
+        [Test]
+        public void BlogApiBehavior_OK()
+        {
+            var target = new ApiBehaviorOptions();
+            ConfigureApiBehavior.BlogApiBehavior(target);
+
+            Assert.IsNotNull(target.InvalidModelStateResponseFactory);
+        }
+    }
+}
