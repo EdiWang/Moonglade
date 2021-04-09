2805bd6d8e75a193253b1e291c5ad66f0c360382
Edi Wang <Edi.Wang@outlook.com>
2021-04-09 19:19:00 +0800

    Update SearchServiceTests.cs

--- src/Moonglade.Core.Tests/SearchServiceTests.cs
+++ src/Moonglade.Core.Tests/SearchServiceTests.cs
@@ -4,8 +4,13 @@ using Moonglade.Data.Infrastructure;
 using Moq;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Query;
 
 namespace Moonglade.Core.Tests
 {
@@ -29,6 +34,8 @@ namespace Moonglade.Core.Tests
         }
 
         [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
         public void SearchAsync_EmptyTerm(string keyword)
         {
             var service = CreateService();
@@ -38,5 +45,22 @@ namespace Moonglade.Core.Tests
                 var result = await service.SearchAsync(keyword);
             });
         }
+
+        //[Test]
+        //public async Task SearchAsync_HappyPath()
+        //{
+        //    var entities = new List<PostEntity>();
+        //    IReadOnlyList<PostDigest> digests = new List<PostDigest>();
+
+        //    var postQuery = new TestAsyncEnumerable<PostEntity>(entities).AsQueryable();
+        //    _mockPostEntityRepo.Setup(p => p.GetAsQueryable()).Returns(postQuery);
+        //    _mockPostEntityRepo.Setup(p => p.Select(It.IsAny<Expression<Func<PostEntity, PostDigest>>>(), true))
+        //        .Returns(digests);
+
+        //    var service = CreateService();
+        //    var result = await service.SearchAsync("996");
+
+        //    Assert.IsNotNull(result);
+        //}
     }
 }
