dcd9b30eded8fb7dd783f14848979db54e605c01
Edi Wang <Edi.Wang@outlook.com>
2021-04-09 17:05:13 +0800

    Create SearchServiceTests.cs

---
+++ src/Moonglade.Core.Tests/SearchServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using Moonglade.Core;
+using Moonglade.Data.Entities;
+using Moonglade.Data.Infrastructure;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace Moonglade.Core.Tests
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class SearchServiceTests
+    {
+        private MockRepository _mockRepository;
+        private Mock<IRepository<PostEntity>> _mockPostEntityRepo;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockRepository = new(MockBehavior.Default);
+            _mockPostEntityRepo = _mockRepository.Create<IRepository<PostEntity>>();
+        }
+
+        private SearchService CreateService()
+        {
+            return new(_mockPostEntityRepo.Object);
+        }
+
+        [TestCase(null)]
+        public void SearchAsync_EmptyTerm(string keyword)
+        {
+            var service = CreateService();
+
+            Assert.ThrowsAsync<ArgumentNullException>(async () =>
+            {
+                var result = await service.SearchAsync(keyword);
+            });
+        }
+    }
+}
