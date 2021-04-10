0da5555ba3ee95a71937141a81628fdb54b8ea39
Edi Wang <Edi.Wang@outlook.com>
2021-04-10 15:01:06 +0800

    Create MockHttpSession.cs

---
+++ src/Moonglade.Web.Tests/MockHttpSession.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Moonglade.Web.Tests
+{
+    [ExcludeFromCodeCoverage]
+    internal class MockHttpSession: ISession
+    {
+        readonly Dictionary<string, object> _sessionStorage = new();
+        string ISession.Id => throw new NotImplementedException();
+        bool ISession.IsAvailable => throw new NotImplementedException();
+        IEnumerable<string> ISession.Keys => _sessionStorage.Keys;
+        void ISession.Clear()
+        {
+            _sessionStorage.Clear();
+        }
+        Task ISession.CommitAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+        Task ISession.LoadAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+        void ISession.Remove(string key)
+        {
+            _sessionStorage.Remove(key);
+        }
+        void ISession.Set(string key, byte[] value)
+        {
+            _sessionStorage[key] = Encoding.UTF8.GetString(value);
+        }
+        bool ISession.TryGetValue(string key, out byte[] value)
+        {
+            if (_sessionStorage[key] != null)
+            {
+                value = Encoding.ASCII.GetBytes(_sessionStorage[key].ToString());
+                return true;
+            }
+            value = null;
+            return false;
+        }
+    }
+}
