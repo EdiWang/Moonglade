fed026cf816336f7625c2947de89827eb446bc60
Edi Wang <Edi.Wang@outlook.com>
2021-04-10 14:29:32 +0800

    Update NotificationClientTests.cs

--- src/Moonglade.Notification.Client.Tests/NotificationClientTests.cs
+++ src/Moonglade.Notification.Client.Tests/NotificationClientTests.cs
@@ -88,80 +88,92 @@ namespace Moonglade.Notification.Client.Tests
             );
         }
 
-        //[Test]
-        //public async Task NotifyCommentAsync_StateUnderTest_ExpectedBehavior()
-        //{
-        //    // Arrange
-        //    var notificationClient = CreateNotificationClient();
-        //    string username = null;
-        //    string email = null;
-        //    string ipAddress = null;
-        //    string postTitle = null;
-        //    string commentContent = null;
-        //    DateTime createTimeUtc = default(DateTime);
+        [Test]
+        public async Task NotifyCommentAsync_StateUnderTest_ExpectedBehavior()
+        {
+            var notificationClient = CreateNotificationClient();
+            string username = "Fubao";
+            string email = "fubao@996.icu";
+            string ipAddress = "9.9.6.007";
+            string postTitle = "Work 996 and get into ICU";
+            string commentContent = "This is fubao";
+            DateTime createTimeUtc = default(DateTime);
 
-        //    // Act
-        //    await notificationClient.NotifyCommentAsync(
-        //        username,
-        //        email,
-        //        ipAddress,
-        //        postTitle,
-        //        commentContent,
-        //        createTimeUtc);
+            await notificationClient.NotifyCommentAsync(
+                username,
+                email,
+                ipAddress,
+                postTitle,
+                commentContent,
+                createTimeUtc);
 
-        //    // Assert
-        //    Assert.Fail();
-        //    _mockRepository.VerifyAll();
-        //}
+            _handlerMock.Protected().Verify(
+                "SendAsync",
+                Times.Exactly(1),
+                ItExpr.Is<HttpRequestMessage>(req =>
+                    req.Method == HttpMethod.Post
+                    && req.RequestUri == new Uri(_mockBlogConfig.Object.NotificationSettings.AzureFunctionEndpoint)
+                ),
+                ItExpr.IsAny<CancellationToken>()
+            );
+        }
 
-        //[Test]
-        //public async Task NotifyCommentReplyAsync_StateUnderTest_ExpectedBehavior()
-        //{
-        //    // Arrange
-        //    var notificationClient = CreateNotificationClient();
-        //    string email = null;
-        //    string commentContent = null;
-        //    string title = null;
-        //    string replyContentHtml = null;
-        //    string postLink = null;
+        [Test]
+        public async Task NotifyCommentReplyAsync_StateUnderTest_ExpectedBehavior()
+        {
+            var notificationClient = CreateNotificationClient();
+            string email = "fubao@996.icu";
+            string commentContent = "This is fubao";
+            string title = "Work 996 and get into ICU";
+            string replyContentHtml = "<p>Jack Ma's fubao</p>";
+            string postLink = "https://996.icu/fuck-jack-ma";
 
-        //    // Act
-        //    await notificationClient.NotifyCommentReplyAsync(
-        //        email,
-        //        commentContent,
-        //        title,
-        //        replyContentHtml,
-        //        postLink);
+            await notificationClient.NotifyCommentReplyAsync(
+                email,
+                commentContent,
+                title,
+                replyContentHtml,
+                postLink);
 
-        //    // Assert
-        //    Assert.Fail();
-        //    _mockRepository.VerifyAll();
-        //}
+            _handlerMock.Protected().Verify(
+                "SendAsync",
+                Times.Exactly(1),
+                ItExpr.Is<HttpRequestMessage>(req =>
+                    req.Method == HttpMethod.Post
+                    && req.RequestUri == new Uri(_mockBlogConfig.Object.NotificationSettings.AzureFunctionEndpoint)
+                ),
+                ItExpr.IsAny<CancellationToken>()
+            );
+        }
 
-        //[Test]
-        //public async Task NotifyPingbackAsync_StateUnderTest_ExpectedBehavior()
-        //{
-        //    // Arrange
-        //    var notificationClient = CreateNotificationClient();
-        //    string targetPostTitle = null;
-        //    DateTime pingTimeUtc = default(DateTime);
-        //    string domain = null;
-        //    string sourceIp = null;
-        //    string sourceUrl = null;
-        //    string sourceTitle = null;
+        [Test]
+        public async Task NotifyPingbackAsync_StateUnderTest_ExpectedBehavior()
+        {
+            var notificationClient = CreateNotificationClient();
+            string targetPostTitle = "Work 996 and get into ICU";
+            DateTime pingTimeUtc = default(DateTime);
+            string domain = "996.icu";
+            string sourceIp = "9.9.6.007";
+            string sourceUrl = "https://996.icu/fuck-jack-ma";
+            string sourceTitle = "996 is Fubao";
 
-        //    // Act
-        //    await notificationClient.NotifyPingbackAsync(
-        //        targetPostTitle,
-        //        pingTimeUtc,
-        //        domain,
-        //        sourceIp,
-        //        sourceUrl,
-        //        sourceTitle);
+            await notificationClient.NotifyPingbackAsync(
+                targetPostTitle,
+                pingTimeUtc,
+                domain,
+                sourceIp,
+                sourceUrl,
+                sourceTitle);
 
-        //    // Assert
-        //    Assert.Fail();
-        //    _mockRepository.VerifyAll();
-        //}
+            _handlerMock.Protected().Verify(
+                "SendAsync",
+                Times.Exactly(1),
+                ItExpr.Is<HttpRequestMessage>(req =>
+                    req.Method == HttpMethod.Post
+                    && req.RequestUri == new Uri(_mockBlogConfig.Object.NotificationSettings.AzureFunctionEndpoint)
+                ),
+                ItExpr.IsAny<CancellationToken>()
+            );
+        }
     }
 }
