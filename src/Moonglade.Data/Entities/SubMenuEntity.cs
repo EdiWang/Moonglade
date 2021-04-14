d31b9f75d2d714d320462b5b2320165c383b85d3
Edi Wang <Edi.Wang@outlook.com>
2021-04-14 14:27:18 +0800

    submenu EF change

---
+++ src/Moonglade.Data/Entities/SubMenuEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Moonglade.Data.Entities
+{
+    public class SubMenuEntity
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+
+        public Guid MenuId { get; set; }
+
+        public virtual MenuEntity Menu { get; set; }
+    }
+}
