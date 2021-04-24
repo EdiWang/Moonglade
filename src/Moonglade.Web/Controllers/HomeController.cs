e9de964297e21e83d727ef28feff0793fbf62fa6
Edi Wang <Edi.Wang@outlook.com>
2021-04-24 08:53:27 +0800

    move views

--- src/Moonglade.Web/Controllers/HomeController.cs
+++ src/Moonglade.Web/Controllers/HomeController.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.FeatureManagement.Mvc;
+using Moonglade.Auth;
 using Moonglade.Caching;
 using Moonglade.Configuration.Settings;
+using Moonglade.Core;
+using Moonglade.Data.Spec;
 using Moonglade.Pages;
+using Moonglade.Pingback.AspNetCore;
 
 namespace Moonglade.Web.Controllers
 {
@@ -14,6 +22,7 @@ namespace Moonglade.Web.Controllers
     public class HomeController : Controller
     {
         private readonly IPageService _pageService;
+        private readonly IPostQueryService _postQueryService;
         private readonly IBlogCache _cache;
         private readonly ILogger<HomeController> _logger;
         private readonly AppSettings _settings;
@@ -22,12 +31,33 @@ namespace Moonglade.Web.Controllers
             IPageService pageService,
             IBlogCache cache,
             ILogger<HomeController> logger,
-            IOptions<AppSettings> settingsOptions)
+            IOptions<AppSettings> settingsOptions, 
+            IPostQueryService postQueryService)
         {
             _pageService = pageService;
             _cache = cache;
             _logger = logger;
+            _postQueryService = postQueryService;
             _settings = settingsOptions.Value;
+        }
+
+        [HttpGet("post/segment/published")]
+        [FeatureGate(FeatureFlags.EnableWebApi)]
+        [Authorize(AuthenticationSchemes = ApiKeyAuthenticationOptions.DefaultScheme)]
+        [ProducesResponseType(typeof(IEnumerable<PostSegment>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Segment()
+        {
+            try
+            {
+                // for security, only allow published posts to be listed to third party API calls
+                var list = await _postQueryService.ListSegment(PostStatus.Published);
+                return Ok(list);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         [Route("page/{slug:regex(^(?!-)([[a-zA-Z0-9-]]+)$)}")]
@@ -45,6 +75,33 @@ namespace Moonglade.Web.Controllers
 
             if (page is null || !page.IsPublished) return NotFound();
             return View(page);
+        }
+
+        [Route("post/{year:int:min(1975):length(4)}/{month:int:range(1,12)}/{day:int:range(1,31)}/{slug:regex(^(?!-)([[a-zA-Z0-9-]]+)$)}")]
+        [AddPingbackHeader("pingback")]
+        public async Task<IActionResult> Slug(int year, int month, int day, string slug)
+        {
+            if (year > DateTime.UtcNow.Year || string.IsNullOrWhiteSpace(slug)) return NotFound();
+
+            var slugInfo = new PostSlug(year, month, day, slug);
+            var post = await _postQueryService.GetAsync(slugInfo);
+
+            if (post is null) return NotFound();
+
+            ViewBag.TitlePrefix = $"{post.Title}";
+            return View(post);
+        }
+
+        [Authorize]
+        [HttpGet("post/preview/{postId:guid}")]
+        public async Task<IActionResult> Preview(Guid postId)
+        {
+            var post = await _postQueryService.GetDraft(postId);
+            if (post is null) return NotFound();
+
+            ViewBag.TitlePrefix = $"{post.Title}";
+            ViewBag.IsDraftPreview = true;
+            return View("Slug", post);
         }
 
         [HttpGet("set-lang")]
