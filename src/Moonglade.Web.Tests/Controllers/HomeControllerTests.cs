a6379bec404e040e0850c17772da746c2ed3e313
Edi Wang <Edi.Wang@outlook.com>
2021-04-24 08:57:04 +0800

    fix UT

--- src/Moonglade.Web.Tests/Controllers/HomeControllerTests.cs
+++ src/Moonglade.Web.Tests/Controllers/HomeControllerTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -8,6 +9,8 @@ using Microsoft.Extensions.Options;
 using Moonglade.Caching;
 using Moonglade.Configuration;
 using Moonglade.Configuration.Settings;
+using Moonglade.Core;
+using Moonglade.Data.Spec;
 using Moonglade.Pages;
 using Moonglade.Web.Controllers;
 using Moq;
@@ -21,6 +24,7 @@ namespace Moonglade.Web.Tests.Controllers
     {
         private MockRepository _mockRepository;
 
+        private Mock<IPostQueryService> _mockPostService;
         private Mock<IPageService> _mockPageService;
         private Mock<IBlogCache> _mockBlogCache;
         private Mock<IBlogConfig> _mockBlogConfig;
@@ -32,6 +36,7 @@ namespace Moonglade.Web.Tests.Controllers
         {
             _mockRepository = new(MockBehavior.Default);
 
+            _mockPostService = _mockRepository.Create<IPostQueryService>();
             _mockPageService = _mockRepository.Create<IPageService>();
             _mockBlogCache = _mockRepository.Create<IBlogCache>();
             _mockBlogConfig = _mockRepository.Create<IBlogConfig>();
@@ -50,7 +55,8 @@ namespace Moonglade.Web.Tests.Controllers
                 _mockPageService.Object,
                 _mockBlogCache.Object,
                 _mockLogger.Object,
-                _mockAppSettingsOptions.Object);
+                _mockAppSettingsOptions.Object,
+                _mockPostService.Object);
         }
 
         [TestCase(null)]
@@ -110,6 +116,108 @@ namespace Moonglade.Web.Tests.Controllers
             var result = ctl.SetLanguage("en-US", "/996/icu");
 
             Assert.IsInstanceOf<LocalRedirectResult>(result);
+        }
+
+        [Test]
+        public async Task Slug_YearOutOfRange()
+        {
+            var ctl = CreateHomeController();
+            var result = await ctl.Slug(DateTime.UtcNow.Year + 1, 9, 9, "6");
+
+            Assert.IsInstanceOf<NotFoundResult>(result);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public async Task Slug_EmptySlug(string slug)
+        {
+            var ctl = CreateHomeController();
+            var result = await ctl.Slug(DateTime.UtcNow.Year + 1, 9, 9, slug);
+
+            Assert.IsInstanceOf<NotFoundResult>(result);
+        }
+
+        [Test]
+        public async Task Slug_NullPost()
+        {
+            _mockPostService.Setup(p => p.GetAsync(It.IsAny<PostSlug>()))
+                .Returns(Task.FromResult((Post)null));
+
+            var ctl = CreateHomeController();
+            var result = await ctl.Slug(DateTime.UtcNow.Year, 1, 9, "work-996");
+
+            Assert.IsInstanceOf<NotFoundResult>(result);
+        }
+
+        [Test]
+        public async Task Slug_View()
+        {
+            _mockPostService.Setup(p => p.GetAsync(It.IsAny<PostSlug>()))
+                .Returns(Task.FromResult(new Post
+                {
+                    Id = Guid.Empty,
+                    Slug = "work-996",
+                    Title = "Work 996"
+                }));
+
+            var ctl = CreateHomeController();
+            var result = await ctl.Slug(DateTime.UtcNow.Year, 1, 9, "work-996");
+
+            Assert.IsInstanceOf<ViewResult>(result);
+        }
+
+        [Test]
+        public async Task Preview_NullPost()
+        {
+            _mockPostService.Setup(p => p.GetDraft(It.IsAny<Guid>()))
+                .Returns(Task.FromResult((Post)null));
+
+            var ctl = CreateHomeController();
+            var result = await ctl.Preview(Guid.Empty);
+
+            Assert.IsInstanceOf<NotFoundResult>(result);
+        }
+
+        [Test]
+        public async Task Preview_View()
+        {
+            _mockPostService.Setup(p => p.GetDraft(It.IsAny<Guid>()))
+                .Returns(Task.FromResult(new Post
+                {
+                    Id = Guid.Empty,
+                    Slug = "work-996",
+                    Title = "Work 996"
+                }));
+
+            var ctl = CreateHomeController();
+            var result = await ctl.Preview(Guid.Parse("e172b031-1c9a-4e4c-b2ea-07469e7b963a"));
+
+            Assert.IsInstanceOf<ViewResult>(result);
+        }
+
+        [Test]
+        public async Task Segment_OK()
+        {
+            IReadOnlyList<PostSegment> ps = new List<PostSegment>();
+            _mockPostService.Setup(p => p.ListSegment(PostStatus.Published)).Returns(Task.FromResult(ps));
+
+            var ctl = CreateHomeController();
+            var result = await ctl.Segment();
+
+            Assert.IsInstanceOf<OkObjectResult>(result);
+        }
+
+        [Test]
+        public async Task Segment_Error()
+        {
+            IReadOnlyList<PostSegment> ps = new List<PostSegment>();
+            _mockPostService.Setup(p => p.ListSegment(PostStatus.Published)).Throws(new ArgumentOutOfRangeException("996"));
+
+            var ctl = CreateHomeController();
+            var result = await ctl.Segment();
+
+            Assert.IsInstanceOf<StatusCodeResult>(result);
         }
     }
 }
