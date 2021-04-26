63dc52bf571dab225ed81f467926f29bc0aeb79f
Edi Wang <Edi.Wang@outlook.com>
2021-04-26 20:26:29 +0800

    using clean up

--- src/Moonglade.Web/Controllers/AdminController.cs
+++ src/Moonglade.Web/Controllers/AdminController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
--- src/Moonglade.Web/Controllers/AuthController.cs
+++ src/Moonglade.Web/Controllers/AuthController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
