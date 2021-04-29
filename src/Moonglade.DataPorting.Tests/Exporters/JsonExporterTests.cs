09c51cd83393c4ab9cf862e3f267bea424423bff
Edi Wang <Edi.Wang@outlook.com>
2021-04-29 14:20:23 +0800

    Create JsonExporterTests.cs

---
+++ src/Moonglade.DataPorting.Tests/Exporters/JsonExporterTests.cs
@@ -0,0 +1,50 @@
+﻿using Moonglade.Data.Infrastructure;
+using Moonglade.DataPorting.Exporters;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Moonglade.DataPorting.Tests.Exporters
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class JsonExporterTests
+    {
+        private MockRepository _mockRepository;
+        private Mock<IRepository<int>> _mockRepo;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockRepository = new(MockBehavior.Default);
+            _mockRepo = _mockRepository.Create<IRepository<int>>();
+        }
+
+        private JsonExporter<int> CreateJsonExporter()
+        {
+            return new(_mockRepo.Object);
+        }
+
+        [Test]
+        public async Task ExportData_StateUnderTest_ExpectedBehavior()
+        {
+            IReadOnlyList<string> data = new List<string>
+            {
+                "996", "ICU"
+            };
+
+            _mockRepo.Setup(p => p.SelectAsync(It.IsAny<Expression<Func<int, string>>>(), true)).Returns(Task.FromResult(data));
+            var jsonExporter = CreateJsonExporter();
+
+            var result = await jsonExporter.ExportData(p => 1);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ExportFormat.SingleJsonFile, result.ExportFormat);
+            Assert.IsNotNull(result.Content);
+        }
+    }
+}
