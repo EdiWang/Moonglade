5015d03d456f8d6011eae03d14f5444c8db51221
Edi Wang <Edi.Wang@outlook.com>
2021-04-30 09:46:10 +0800

    fix build warnings

--- src/Moonglade.Web.Tests/Pages/ErrorModelTests.cs
+++ src/Moonglade.Web.Tests/Pages/ErrorModelTests.cs
@@ -73,16 +73,17 @@ namespace Moonglade.Web.Tests.Pages
     [ExcludeFromCodeCoverage]
     internal class FakeConnectionInfo : ConnectionInfo
     {
-        public override async Task<X509Certificate2?> GetClientCertificateAsync(CancellationToken cancellationToken = new CancellationToken())
+        public override async Task<X509Certificate2> GetClientCertificateAsync(CancellationToken cancellationToken = new())
         {
+            await Task.CompletedTask;
             throw new NotImplementedException();
         }
 
         public override string Id { get; set; }
-        public override IPAddress? RemoteIpAddress { get; set; }
+        public override IPAddress RemoteIpAddress { get; set; }
         public override int RemotePort { get; set; }
-        public override IPAddress? LocalIpAddress { get; set; }
+        public override IPAddress LocalIpAddress { get; set; }
         public override int LocalPort { get; set; }
-        public override X509Certificate2? ClientCertificate { get; set; }
+        public override X509Certificate2 ClientCertificate { get; set; }
     }
 }
