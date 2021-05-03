0114e34db58065e5ee24ee65726989c3348cf84b
Edi Wang <Edi.Wang@outlook.com>
2021-05-03 14:12:30 +0800

    Update BlogStatisticsTests.cs

--- src/Moonglade.Core.Tests/BlogStatisticsTests.cs
+++ src/Moonglade.Core.Tests/BlogStatisticsTests.cs
@@ -26,21 +26,27 @@ namespace Moonglade.Core.Tests
             return new(_mockPostExtensionRepo.Object);
         }
 
-        //[Test]
-        //public async Task GetStatisticAsync_StateUnderTest_ExpectedBehavior()
-        //{
-        //    // Arrange
-        //    var blogStatistics = CreateBlogStatistics();
-        //    Guid postId = default(Guid);
+        [Test]
+        public async Task GetStatisticAsync_StateUnderTest_ExpectedBehavior()
+        {
+            _mockPostExtensionRepo.Setup(p => p.GetAsync(It.IsAny<Guid>()))
+                .Returns(ValueTask.FromResult(new PostExtensionEntity()
+                {
+                    Hits = 996,
+                    Likes = 251
+                }));
 
-        //    // Act
-        //    var result = await blogStatistics.GetStatisticAsync(
-        //        postId);
+            // Arrange
+            var blogStatistics = CreateBlogStatistics();
+            Guid postId = Guid.Empty;
 
-        //    // Assert
-        //    Assert.Fail();
-        //    _mockRepository.VerifyAll();
-        //}
+            // Act
+            var result = await blogStatistics.GetStatisticAsync(postId);
+
+            // Assert
+            Assert.AreEqual(996, result.Hits);
+            Assert.AreEqual(251, result.Likes);
+        }
 
         //[Test]
         //public async Task UpdateStatisticAsync_StateUnderTest_ExpectedBehavior()
