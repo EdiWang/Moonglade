f85bc43621eaa82db90d7225c7d785e53fdcdf1a
Edi Wang <Edi.Wang@outlook.com>
2021-05-04 09:45:46 +0800

    Update MockHttpSession.cs

--- src/Moonglade.Web.Tests/MockHttpSession.cs
+++ src/Moonglade.Web.Tests/MockHttpSession.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,7 +7,6 @@ using Microsoft.AspNetCore.Http;
 
 namespace Moonglade.Web.Tests
 {
-    [ExcludeFromCodeCoverage]
     internal class MockHttpSession : ISession
     {
         readonly Dictionary<string, object> _sessionStorage = new();
