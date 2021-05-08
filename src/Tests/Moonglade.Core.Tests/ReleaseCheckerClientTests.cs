9ab0cc47a9b20d6ea0712385a3618dbc64897220
Edi Wang <Edi.Wang@outlook.com>
2021-05-08 13:21:04 +0800

    Update ReleaseCheckerClientTests.cs

--- src/Tests/Moonglade.Core.Tests/ReleaseCheckerClientTests.cs
+++ src/Tests/Moonglade.Core.Tests/ReleaseCheckerClientTests.cs
@@ -6,7 +6,9 @@ using Moonglade.Core;
 using Moq;
 using NUnit.Framework;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
+using Moq.Protected;
 
 namespace Moonglade.Core.Tests
 {
@@ -16,8 +18,9 @@ namespace Moonglade.Core.Tests
         private MockRepository _mockRepository;
 
         private Mock<IConfiguration> _mockConfiguration;
-        private Mock<HttpClient> _mockHttpClient;
+        private Mock<HttpMessageHandler> _handlerMock;
         private Mock<ILogger<ReleaseCheckerClient>> _mockLogger;
+        private HttpClient _magicHttpClient;
 
         [SetUp]
         public void SetUp()
@@ -25,16 +28,14 @@ namespace Moonglade.Core.Tests
             _mockRepository = new(MockBehavior.Default);
 
             _mockConfiguration = _mockRepository.Create<IConfiguration>();
-            _mockHttpClient = _mockRepository.Create<HttpClient>();
+            _handlerMock = _mockRepository.Create<HttpMessageHandler>();
             _mockLogger = _mockRepository.Create<ILogger<ReleaseCheckerClient>>();
         }
 
         private ReleaseCheckerClient CreateReleaseCheckerClient()
         {
-            return new(
-                _mockConfiguration.Object,
-                _mockHttpClient.Object,
-                _mockLogger.Object);
+            _magicHttpClient = new(_handlerMock.Object);
+            return new(_mockConfiguration.Object, _magicHttpClient, _mockLogger.Object);
         }
 
         [TestCase(null)]
@@ -58,6 +59,33 @@ namespace Moonglade.Core.Tests
             Assert.Throws<InvalidOperationException>(() =>
             {
                 var releaseCheckerClient = CreateReleaseCheckerClient();
+            });
+        }
+
+        [Test]
+        public void CheckNewReleaseAsync_UnsuccessResponse()
+        {
+            _mockConfiguration.Setup(p => p["ReleaseCheckApiAddress"]).Returns("https://996.icu");
+
+            _handlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Content = new StringContent("")
+                })
+                .Verifiable();
+
+            var releaseCheckerClient = CreateReleaseCheckerClient();
+
+            Assert.ThrowsAsync<Exception>(async () =>
+            {
+                var result = await releaseCheckerClient.CheckNewReleaseAsync();
             });
         }
     }
