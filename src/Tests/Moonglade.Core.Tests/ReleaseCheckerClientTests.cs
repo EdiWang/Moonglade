87ddbfaa161d6da209a3021c85357ac10b760054
Edi Wang <Edi.Wang@outlook.com>
2021-05-08 11:11:41 +0800

    Create ReleaseCheckerClientTests.cs

---
+++ src/Tests/Moonglade.Core.Tests/ReleaseCheckerClientTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moonglade.Core;
+using Moq;
+using NUnit.Framework;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Moonglade.Core.Tests
+{
+    [TestFixture]
+    public class ReleaseCheckerClientTests
+    {
+        private MockRepository _mockRepository;
+
+        private Mock<IConfiguration> _mockConfiguration;
+        private Mock<HttpClient> _mockHttpClient;
+        private Mock<ILogger<ReleaseCheckerClient>> _mockLogger;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockRepository = new(MockBehavior.Default);
+
+            _mockConfiguration = _mockRepository.Create<IConfiguration>();
+            _mockHttpClient = _mockRepository.Create<HttpClient>();
+            _mockLogger = _mockRepository.Create<ILogger<ReleaseCheckerClient>>();
+        }
+
+        private ReleaseCheckerClient CreateReleaseCheckerClient()
+        {
+            return new(
+                _mockConfiguration.Object,
+                _mockHttpClient.Object,
+                _mockLogger.Object);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void CheckNewReleaseAsync_EmptyApiAddress(string apiAddress)
+        {
+            _mockConfiguration.Setup(p => p["ReleaseCheckApiAddress"]).Returns(apiAddress);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var releaseCheckerClient = CreateReleaseCheckerClient();
+            });
+        }
+    }
+}
