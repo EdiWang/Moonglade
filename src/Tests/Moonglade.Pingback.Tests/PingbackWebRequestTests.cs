6120ffd1089a7c46a19487778810ff60721d5674
Edi Wang <Edi.Wang@outlook.com>
2021-05-10 12:29:16 +0800

    Create PingbackWebRequestTests.cs

---
+++ src/Tests/Moonglade.Pingback.Tests/PingbackWebRequestTests.cs
@@ -0,0 +1,42 @@
+﻿using Moonglade.Pingback;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Net;
+
+namespace Moonglade.Pingback.Tests
+{
+    [TestFixture]
+    public class PingbackWebRequestTests
+    {
+        private MockRepository _mockRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this._mockRepository = new(MockBehavior.Default);
+        }
+
+        private PingbackWebRequest CreatePingbackWebRequest()
+        {
+            return new PingbackWebRequest();
+        }
+
+        [Test]
+        public void BuildHttpWebRequest_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var pingbackWebRequest = this.CreatePingbackWebRequest();
+            Uri sourceUrl = new Uri("https://251.today/hw/copy-cat");
+            Uri targetUrl = new Uri("https://greenhat.today/papapa");
+            Uri url = new Uri("https://996.icu/ping");
+
+            // Act
+            var result = pingbackWebRequest.BuildHttpWebRequest(sourceUrl, targetUrl, url);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Address, url);
+            Assert.AreEqual(result.ContentType, "text/xml");
+        }
+    }
+}
