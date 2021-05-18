86f64a279e56790af11ff34d8189029c38b6938f
Edi Wang <Edi.Wang@outlook.com>
2021-05-18 10:55:25 +0800

    move files

---
+++ src/Moonglade.Web/Filters/ClearPagingCountCache.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Moonglade.Caching;
+
+namespace Moonglade.Web.Filters
+{
+    public class ClearPagingCountCache : ActionFilterAttribute
+    {
+        private readonly IBlogCache _cache;
+
+        public ClearPagingCountCache(IBlogCache cache)
+        {
+            _cache = cache;
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            base.OnActionExecuted(context);
+            _cache.Remove(CacheDivision.General, "postcount");
+            _cache.Remove(CacheDivision.PostCountCategory);
+            _cache.Remove(CacheDivision.PostCountTag);
+        }
+    }
+}
