6389eb2c5062eae56d5733ce56da4a8863d55a3b
Edi Wang <Edi.Wang@outlook.com>
2021-05-18 10:31:17 +0800

    Create BlogDbContextTests.cs

---
+++ src/Tests/Moonglade.Core.Tests/BlogDbContextTests.cs
@@ -0,0 +1,30 @@
+﻿using Moonglade.Data;
+using Moq;
+using NUnit.Framework;
+
+namespace Moonglade.Core.Tests
+{
+    [TestFixture]
+    public class BlogDbContextTests
+    {
+        private MockRepository _mockRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockRepository = new(MockBehavior.Default);
+        }
+
+        private BlogDbContext CreateBlogDbContext()
+        {
+            return new();
+        }
+
+        [Test]
+        public void CreateBlogDbContext_OK()
+        {
+            var blogDbContext = CreateBlogDbContext();
+            Assert.IsNotNull(blogDbContext);
+        }
+    }
+}
