e856574b65b7af8b657a08c1e2c80eaebf7db84d
Edi Wang <Edi.Wang@outlook.com>
2021-05-18 10:21:04 +0800

    Create ServiceCollectionExtensionTests.cs

---
+++ src/Tests/Moonglade.Notification.Client.Tests/ServiceCollectionExtensionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace Moonglade.Notification.Client.Tests
+{
+    [TestFixture]
+    public class ServiceCollectionExtensionTests
+    {
+        [Test]
+        public void AddNotificationClient_OK()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddNotificationClient();
+
+            var obj1 = services.FirstOrDefault(p => p.ServiceType == typeof(IBlogNotificationClient));
+            Assert.IsNotNull(obj1);
+        }
+    }
+}
