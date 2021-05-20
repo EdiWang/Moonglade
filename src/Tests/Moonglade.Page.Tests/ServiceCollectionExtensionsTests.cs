b9931b89efaa56ce5b34669017814e626fd7a069
Edi Wang <Edi.Wang@outlook.com>
2021-05-20 12:51:04 +0800

    Create ServiceCollectionExtensionsTests.cs

---
+++ src/Tests/Moonglade.Page.Tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using Moonglade.Page;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Moonglade.Page.Tests
+{
+    [TestFixture]
+    public class ServiceCollectionExtensionsTests
+    {
+        [Test]
+        public void AddBlogPage_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            IServiceCollection services = new ServiceCollection();
+
+            // Act
+            services.AddBlogPage();
+
+            var obj1 = services.FirstOrDefault(p => p.ServiceType == typeof(IBlogPageService));
+            Assert.IsNotNull(obj1);
+        }
+    }
+}
