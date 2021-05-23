d84840feff841a53fa2b24bb85c1ca8a9c0cc0d1
Edi Wang <Edi.Wang@outlook.com>
2021-05-23 08:41:14 +0800

    Create ServiceCollectionExtensionsTests.cs

---
+++ src/Tests/Moonglade.Core.Tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,47 @@
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Moonglade.Caching;
+using Moonglade.Caching.Filters;
+using NUnit.Framework;
+
+namespace Moonglade.Core.Tests
+{
+    [TestFixture]
+    public class ServiceCollectionExtensionsTests
+    {
+        [Test]
+        public void AddCoreBloggingServices_OK()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddCoreBloggingServices();
+
+            var obj1 = services.FirstOrDefault(p => p.ServiceType == typeof(IBlogStatistics));
+            Assert.IsNotNull(obj1);
+
+            var obj2 = services.FirstOrDefault(p => p.ServiceType == typeof(ICategoryService));
+            Assert.IsNotNull(obj2);
+
+            var obj3 = services.FirstOrDefault(p => p.ServiceType == typeof(IPostManageService));
+            Assert.IsNotNull(obj3);
+
+            var obj4 = services.FirstOrDefault(p => p.ServiceType == typeof(IPostQueryService));
+            Assert.IsNotNull(obj4);
+
+            var obj5 = services.FirstOrDefault(p => p.ServiceType == typeof(ISearchService));
+            Assert.IsNotNull(obj5);
+
+            var obj6 = services.FirstOrDefault(p => p.ServiceType == typeof(ITagService));
+            Assert.IsNotNull(obj6);
+        }
+
+        [Test]
+        public void AddReleaseCheckerClient_OK()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddReleaseCheckerClient();
+
+            var obj1 = services.FirstOrDefault(p => p.ServiceType == typeof(IReleaseCheckerClient));
+            Assert.IsNotNull(obj1);
+        }
+    }
+}
