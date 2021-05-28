2fc4d353412e56e38169be9f7edb70e13e04d80a
Edi Wang <Edi.Wang@outlook.com>
2021-05-28 09:57:25 +0800

    code clean up

--- src/Moonglade.Web/Models/PageEditModel.cs
+++ src/Moonglade.Web/Models/PageEditModel.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
 
 namespace Moonglade.Web.Models
 {
--- src/Moonglade.Web/Startup.cs
+++ src/Moonglade.Web/Startup.cs
@@ -11,7 +11,6 @@ using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.ApplicationInsights.Extensibility.Implementation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Rewrite;
