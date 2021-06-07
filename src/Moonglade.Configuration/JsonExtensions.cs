5e817328530015bee43ef6817b201eb431879509
Edi Wang <Edi.Wang@outlook.com>
2021-06-07 12:58:17 +0800

    support Chinese in JSON encoding for settings

--- src/Moonglade.Configuration/JsonExtensions.cs
+++ src/Moonglade.Configuration/JsonExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Json;
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
 
 namespace Moonglade.Configuration
 {
@@ -6,6 +8,17 @@ namespace Moonglade.Configuration
     {
         private static readonly JsonSerializerOptions JsonOptions = new()
         {
+            // https://en.wikipedia.org/wiki/CJK_Unified_Ideographs_(Unicode_block)
+            Encoder = JavaScriptEncoder.Create(
+                UnicodeRanges.BasicLatin, 
+                UnicodeRanges.CjkCompatibility,
+                UnicodeRanges.CjkCompatibilityForms,
+                UnicodeRanges.CjkCompatibilityIdeographs,
+                UnicodeRanges.CjkRadicalsSupplement,
+                UnicodeRanges.CjkStrokes,
+                UnicodeRanges.CjkUnifiedIdeographs,
+                UnicodeRanges.CjkUnifiedIdeographsExtensionA,
+                UnicodeRanges.CjkSymbolsandPunctuation),
             PropertyNameCaseInsensitive = true
         };
 
