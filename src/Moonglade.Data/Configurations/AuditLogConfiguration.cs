8a479e325ca123a8a988cf8aca3611c929adf689
Edi Wang <Edi.Wang@outlook.com>
2021-06-11 13:54:08 +0800

    Create AuditLogConfiguration.cs

---
+++ src/Moonglade.Data/Configurations/AuditLogConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Moonglade.Data.Entities;
+
+namespace Moonglade.Data.Configurations
+{
+    public class AuditLogConfiguration : IEntityTypeConfiguration<AuditLogEntity>
+    {
+        public void Configure(EntityTypeBuilder<AuditLogEntity> builder)
+        {
+            builder.Property(e => e.Id).UseIdentityColumn();
+        }
+    }
+}
