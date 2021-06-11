1e68f15234dc05e982330ae298f90ef2bad4502a
Edi Wang <Edi.Wang@outlook.com>
2021-06-11 14:32:27 +0800

    Create AuditPagingSpec.cs

---
+++ src/Moonglade.Data/Spec/AuditPagingSpec.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moonglade.Data.Entities;
+using Moonglade.Data.Infrastructure;
+
+namespace Moonglade.Data.Spec
+{
+    public sealed class AuditPagingSpec : BaseSpecification<AuditLogEntity>
+    {
+        public AuditPagingSpec(int pageSize, int offset) : base()
+        {
+            ApplyPaging(offset, pageSize);
+            ApplyOrderByDescending(p => p.EventTimeUtc);
+        }
+    }
+}
