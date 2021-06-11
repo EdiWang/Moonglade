4a05135b762e57b0d1c1c8aaa8c8e8412167c5f1
Edi Wang <Edi.Wang@outlook.com>
2021-06-11 13:52:24 +0800

    Create AuditLogEntity.cs

---
+++ src/Moonglade.Data/Entities/AuditLogEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moonglade.Data.Entities
+{
+    public class AuditLogEntity
+    {
+        public long Id { get; set; }
+
+        public int EventId { get; set; }
+
+        public int EventType { get; set; }
+
+        public DateTime EventTimeUtc { get; set; }
+
+        public string WebUsername { get; set; }
+
+        public string IpAddressV4 { get; set; }
+
+        public string MachineName { get; set; }
+
+        public string Message { get; set; }
+    }
+}
