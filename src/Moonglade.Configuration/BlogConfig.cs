ed4e9046fe5389f1dc121f903028fe4622df8c83
Edi Wang <Edi.Wang@outlook.com>
2021-06-12 16:41:25 +0800

    remove stupid try catch

--- src/Moonglade.Configuration/BlogConfig.cs
+++ src/Moonglade.Configuration/BlogConfig.cs
@@ -4,14 +4,11 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Dapper;
-using Microsoft.Extensions.Logging;
 
 namespace Moonglade.Configuration
 {
     public class BlogConfig : IBlogConfig
     {
-        private readonly ILogger<BlogConfig> _logger;
-
         private readonly IDbConnection _dbConnection;
 
         public GeneralSettings GeneralSettings { get; set; }
@@ -30,12 +27,9 @@ namespace Moonglade.Configuration
 
         private bool _hasInitialized;
 
-        public BlogConfig(
-            ILogger<BlogConfig> logger,
-            IDbConnection dbConnection)
+        public BlogConfig(IDbConnection dbConnection)
         {
             _dbConnection = dbConnection;
-            _logger = logger;
 
             ContentSettings = new();
             GeneralSettings = new();
@@ -80,16 +74,8 @@ namespace Moonglade.Configuration
             var json = blogSettings.ToJson();
             var task = SetConfiguration(typeof(T).Name, json);
 
-            try
-            {
-                await task;
-                Dirty();
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, e.Message);
-                throw;
-            }
+            await task;
+            Dirty();
         }
 
         public async Task SaveAssetAsync(Guid assetId, string assetBase64)
@@ -97,72 +83,48 @@ namespace Moonglade.Configuration
             if (assetId == Guid.Empty) throw new ArgumentOutOfRangeException(nameof(assetId));
             if (string.IsNullOrWhiteSpace(assetBase64)) throw new ArgumentNullException(nameof(assetBase64));
 
-            try
-            {
-                var exists = await
-                    _dbConnection.ExecuteScalarAsync<int>("SELECT TOP 1 1 FROM BlogAsset ba WHERE ba.Id = @assetId",
-                        new { assetId });
+            var exists = await
+                _dbConnection.ExecuteScalarAsync<int>("SELECT TOP 1 1 FROM BlogAsset ba WHERE ba.Id = @assetId",
+                    new { assetId });
 
-                if (exists == 0)
-                {
-                    await _dbConnection.ExecuteAsync(
-                        "INSERT INTO BlogAsset(Id, Base64Data, LastModifiedTimeUtc) VALUES (@assetId, @assetBase64, @utcNow)",
-                        new
-                        {
-                            assetId,
-                            assetBase64,
-                            DateTime.UtcNow
-                        });
-                }
-                else
-                {
-                    await _dbConnection.ExecuteAsync(
-                        "UPDATE BlogAsset SET Base64Data = @assetBase64, LastModifiedTimeUtc = @utcNow WHERE Id = @assetId",
-                        new
-                        {
-                            assetId,
-                            assetBase64,
-                            DateTime.UtcNow
-                        });
-                }
-            }
-            catch (Exception e)
+            if (exists == 0)
             {
-                _logger.LogError(e, e.Message);
-                throw;
+                await _dbConnection.ExecuteAsync(
+                    "INSERT INTO BlogAsset(Id, Base64Data, LastModifiedTimeUtc) VALUES (@assetId, @assetBase64, @utcNow)",
+                    new
+                    {
+                        assetId,
+                        assetBase64,
+                        DateTime.UtcNow
+                    });
+            }
+            else
+            {
+                await _dbConnection.ExecuteAsync(
+                    "UPDATE BlogAsset SET Base64Data = @assetBase64, LastModifiedTimeUtc = @utcNow WHERE Id = @assetId",
+                    new
+                    {
+                        assetId,
+                        assetBase64,
+                        DateTime.UtcNow
+                    });
             }
         }
 
         public string GetAssetData(Guid assetId)
         {
-            try
-            {
-                var asset = _dbConnection.QueryFirstOrDefault<BlogAsset>
-                    ("SELECT TOP 1 * FROM BlogAsset ba WHERE ba.Id = @assetId", new { assetId });
+            var asset = _dbConnection.QueryFirstOrDefault<BlogAsset>
+                ("SELECT TOP 1 * FROM BlogAsset ba WHERE ba.Id = @assetId", new { assetId });
 
-                return asset?.Base64Data;
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, e.Message);
-                throw;
-            }
+            return asset?.Base64Data;
         }
 
         public async Task<string> GetAssetDataAsync(Guid assetId)
         {
-            try
-            {
-                var asset = await _dbConnection.QueryFirstOrDefaultAsync<BlogAsset>
-                    ("SELECT TOP 1 * FROM BlogAsset ba WHERE ba.Id = @assetId", new { assetId });
+            var asset = await _dbConnection.QueryFirstOrDefaultAsync<BlogAsset>
+                ("SELECT TOP 1 * FROM BlogAsset ba WHERE ba.Id = @assetId", new { assetId });
 
-                return asset?.Base64Data;
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, e.Message);
-                throw;
-            }
+            return asset?.Base64Data;
         }
 
         protected void Dirty()
@@ -172,21 +134,13 @@ namespace Moonglade.Configuration
 
         private IDictionary<string, string> GetAllConfigurations()
         {
-            try
-            {
-                var sql = $"SELECT {nameof(BlogConfiguration.CfgKey)}, " +
-                          $"{nameof(BlogConfiguration.CfgValue)} " +
-                          $"FROM {nameof(BlogConfiguration)}";
+            var sql = $"SELECT {nameof(BlogConfiguration.CfgKey)}, " +
+                      $"{nameof(BlogConfiguration.CfgValue)} " +
+                      $"FROM {nameof(BlogConfiguration)}";
 
-                var data = _dbConnection.Query<(string CfgKey, string CfgValue)>(sql);
-                var dic = data.ToDictionary(c => c.CfgKey, c => c.CfgValue);
-                return dic;
-            }
-            catch (Exception e)
-            {
-                _logger.LogCritical(e, $"Error {nameof(GetAllConfigurations)}");
-                throw;
-            }
+            var data = _dbConnection.Query<(string CfgKey, string CfgValue)>(sql);
+            var dic = data.ToDictionary(c => c.CfgKey, c => c.CfgValue);
+            return dic;
         }
     }
 }
