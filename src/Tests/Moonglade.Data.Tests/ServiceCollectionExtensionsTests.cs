71c79f042c9185d7fc4878734773ad1b7e35586f
Edi Wang <Edi.Wang@outlook.com>
2021-06-14 16:15:36 +0800

    Create ServiceCollectionExtensionsTests.cs

---
+++ src/Tests/Moonglade.Data.Tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Moonglade.Data.Infrastructure;
+using NUnit.Framework;
+
+namespace Moonglade.Data.Tests
+{
+    [TestFixture]
+    public class ServiceCollectionExtensionsTests
+    {
+        [Test]
+        public void AddCoreBloggingServices_OK()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddDataStorage(@"Server=(localdb)\\MSSQLLocalDB;Database=moonglade;Trusted_Connection=True;");
+
+            var obj1 = services.FirstOrDefault(p => p.ServiceType == typeof(IDbConnection));
+            Assert.IsNotNull(obj1);
+
+            var obj2 = services.FirstOrDefault(p => p.ServiceType == typeof(IRepository<>));
+            Assert.IsNotNull(obj2);
+
+            var obj3 = services.FirstOrDefault(p => p.ServiceType == typeof(BlogDbContext));
+            Assert.IsNotNull(obj3);
+        }
+    }
+}
