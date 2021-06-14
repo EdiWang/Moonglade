0154079c90698c0403135163c4f2e042174f7387
Edi Wang <Edi.Wang@outlook.com>
2021-06-14 16:38:24 +0800

    SpecificationEvaluatorTests

---
+++ src/Tests/Moonglade.Core.Tests/Infrastructure/SpecificationEvaluatorTests.cs
@@ -0,0 +1,49 @@
+﻿using Moonglade.Data.Infrastructure;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Moonglade.Core.Tests.Infrastructure
+{
+    [TestFixture]
+    public class SpecificationEvaluatorTests
+    {
+        [Test]
+        public void GetQuery_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var inputQuery = new List<Fubao>
+            {
+                new() { Id = 1, Name = "Fubao1" },
+                new() { Id = 2, Name = "Fubao2" },
+                new() { Id = 3, Name = "996-1" },
+                new() { Id = 4, Name = "996-2" }
+            }.AsQueryable();
+            ISpecification<Fubao> specification = new FubaoSpec();
+
+            // Act
+            var result = SpecificationEvaluator<Fubao>.GetQuery(
+                inputQuery,
+                specification).ToList();
+
+            // Assert
+            Assert.AreEqual(2, result.Count);
+        }
+    }
+
+    internal class Fubao
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    internal sealed class FubaoSpec : BaseSpecification<Fubao>
+    {
+        public FubaoSpec() : base(p => p.Id > 1)
+        {
+            AddCriteria(p => p.Name.StartsWith("996"));
+            ApplyOrderByDescending(p => p.Name);
+            ApplyPaging(0, 996);
+        }
+    }
+}
--- src/Tests/Moonglade.Data.Tests/ServiceCollectionExtensionsTests.cs
+++ src/Tests/Moonglade.Data.Tests/ServiceCollectionExtensionsTests.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Moonglade.Data.Infrastructure;
 using NUnit.Framework;
