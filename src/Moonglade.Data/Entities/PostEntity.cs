dc04adeec3f7e9bc1a1582fa6a5498371a40a6ac
Edi Wang <Edi.Wang@outlook.com>
2021-06-15 13:40:28 +0800

    Update PostEntity.cs

--- src/Moonglade.Data/Entities/PostEntity.cs
+++ src/Moonglade.Data/Entities/PostEntity.cs
@@ -15,6 +15,7 @@ namespace Moonglade.Data.Entities
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Slug { get; set; }
+        public string Author { get; set; }
         public string PostContent { get; set; }
         public bool CommentEnabled { get; set; }
         public DateTime CreateTimeUtc { get; set; }
