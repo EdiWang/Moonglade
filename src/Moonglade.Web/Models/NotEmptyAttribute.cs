51c0aa2c7b8bc955464e53058d0c3153b46e98bf
Edi Wang <Edi.Wang@outlook.com>
2021-06-15 11:56:52 +0800

    Create NotEmptyAttribute.cs

---
+++ src/Moonglade.Web/Models/NotEmptyAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Moonglade.Web.Models
+{
+    // https://andrewlock.net/creating-an-empty-guid-validation-attribute/
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+    public class NotEmptyAttribute : ValidationAttribute
+    {
+        public const string DefaultErrorMessage = "The {0} field must not be empty";
+        public NotEmptyAttribute() : base(DefaultErrorMessage) { }
+
+        public override bool IsValid(object value)
+        {
+            // NotEmpty doesn't necessarily mean required
+            if (value is null) return true;
+
+            return value switch
+            {
+                Guid guid => guid != Guid.Empty,
+                _ => true
+            };
+        }
+    }
+}
