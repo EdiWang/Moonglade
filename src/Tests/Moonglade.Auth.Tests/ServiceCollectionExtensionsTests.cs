fc0658fcb157edf4f0ea775d087c3b56cd09adcb
Edi Wang <Edi.Wang@outlook.com>
2021-06-18 09:29:19 +0800

    Update ServiceCollectionExtensionsTests.cs

--- src/Tests/Moonglade.Auth.Tests/ServiceCollectionExtensionsTests.cs
+++ src/Tests/Moonglade.Auth.Tests/ServiceCollectionExtensionsTests.cs
@@ -2,12 +2,31 @@
 using Moq;
 using NUnit.Framework;
 using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Moonglade.Auth.Tests
 {
     [TestFixture]
     public class ServiceCollectionExtensionsTests
     {
+        [Test]
+        public void AddBlogAuthenticaton_None()
+        {
+            var config = @"{""Authentication"":{""Provider"": ""996""}}";
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonStream(new MemoryStream(Encoding.UTF8.GetBytes(config)));
+            var configuration = builder.Build();
 
+            IServiceCollection services = new ServiceCollection();
+
+            Assert.Throws<NotSupportedException>(() =>
+            {
+                services.AddBlogAuthenticaton(configuration);
+            });
+        }
     }
 }
