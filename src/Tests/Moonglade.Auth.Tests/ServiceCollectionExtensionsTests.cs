4f4c6c2275fea3aa44c019d6e49acd11d595fb6a
Edi Wang <Edi.Wang@outlook.com>
2021-06-18 09:32:55 +0800

    Update ServiceCollectionExtensionsTests.cs

--- src/Tests/Moonglade.Auth.Tests/ServiceCollectionExtensionsTests.cs
+++ src/Tests/Moonglade.Auth.Tests/ServiceCollectionExtensionsTests.cs
@@ -13,20 +13,40 @@ namespace Moonglade.Auth.Tests
     [TestFixture]
     public class ServiceCollectionExtensionsTests
     {
-        [Test]
-        public void AddBlogAuthenticaton_None()
+        private static IConfiguration GetConfiguration(string json)
         {
-            var config = @"{""Authentication"":{""Provider"": ""996""}}";
             var builder = new ConfigurationBuilder();
-            builder.AddJsonStream(new MemoryStream(Encoding.UTF8.GetBytes(config)));
+            builder.AddJsonStream(new MemoryStream(Encoding.UTF8.GetBytes(json)));
             var configuration = builder.Build();
+            return configuration;
+        }
 
+        [Test]
+        public void AddBlogAuthenticaton_NotSupportedProvider()
+        {
+            var json = @"{""Authentication"":{""Provider"": ""996""}}";
+            var config = GetConfiguration(json);
             IServiceCollection services = new ServiceCollection();
 
             Assert.Throws<NotSupportedException>(() =>
             {
-                services.AddBlogAuthenticaton(configuration);
+                services.AddBlogAuthenticaton(config);
             });
+        }
+
+        [Test]
+        public void AddBlogAuthenticaton_None()
+        {
+            var json = @"{""Authentication"":{""Provider"": ""None""}}";
+            var config = GetConfiguration(json);
+            IServiceCollection services = new ServiceCollection();
+            services.AddBlogAuthenticaton(config);
+
+            var obj1 = services.FirstOrDefault(p => p.ServiceType == typeof(IGetApiKeyQuery));
+            Assert.IsNotNull(obj1);
+
+            var obj2 = services.FirstOrDefault(p => p.ServiceType == typeof(ILocalAccountService));
+            Assert.IsNotNull(obj2);
         }
     }
 }
