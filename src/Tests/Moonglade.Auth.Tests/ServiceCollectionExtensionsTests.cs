f46eb9bca518a42ce8ae5e52435ba4f0e3eef257
Edi Wang <Edi.Wang@outlook.com>
2021-06-18 09:23:13 +0800

    Create ServiceCollectionExtensionsTests.cs

---
+++ src/Tests/Moonglade.Auth.Tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,13 @@
+﻿using Moonglade.Auth;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace Moonglade.Auth.Tests
+{
+    [TestFixture]
+    public class ServiceCollectionExtensionsTests
+    {
+
+    }
+}
