080c9fda76fefeb2002e66d6b2129c477252045a
Edi Wang <Edi.Wang@outlook.com>
2021-06-27 12:26:27 +0800

    Create CreateThemeRequest.cs

---
+++ src/Moonglade.Web/Models/Settings/CreateThemeRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Moonglade.Web.Models.Settings
+{
+    public class CreateThemeRequest
+    {
+        [Required]
+        [MaxLength(32)]
+        public string Name { get; set; }
+
+        [Required]
+        public string AccentColor1 { get; set; }
+
+        [Required]
+        public string AccentColor2 { get; set; }
+
+        [Required]
+        public string AccentColor3 { get; set; }
+    }
+}
