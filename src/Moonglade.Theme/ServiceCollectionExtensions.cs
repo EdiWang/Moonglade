fbf8ceb55e4902f65deaa21b4f961ddebe1ab10b
Edi Wang <Edi.Wang@outlook.com>
2021-06-27 10:06:30 +0800

    Create ServiceCollectionExtensions.cs

---
+++ src/Moonglade.Theme/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Moonglade.Theme
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddBlogTheme(this IServiceCollection services)
+        {
+            services.AddScoped<IThemeService, ThemeService>();
+            return services;
+        }
+    }
+}
