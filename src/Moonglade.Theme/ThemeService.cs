73a3fcbc6c2523212fb49d643452a3977f26c4ea
Edi Wang <Edi.Wang@outlook.com>
2021-06-27 10:18:07 +0800

    Update ThemeService.cs

--- src/Moonglade.Theme/ThemeService.cs
+++ src/Moonglade.Theme/ThemeService.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Moonglade.Data.Entities;
 using Moonglade.Data.Infrastructure;
@@ -10,7 +12,8 @@ namespace Moonglade.Theme
 {
     public interface IThemeService
     {
-
+        Task<IReadOnlyList<string>> GetAllNames();
+        Task<string> GetStyleSheet(string themeName);
     }
 
     public class ThemeService : IThemeService
@@ -20,6 +23,44 @@ namespace Moonglade.Theme
         public ThemeService(IRepository<BlogThemeEntity> themeRepo)
         {
             _themeRepo = themeRepo;
+        }
+
+        public Task<IReadOnlyList<string>> GetAllNames()
+        {
+            return _themeRepo.SelectAsync(p => p.ThemeName);
+        }
+
+        public async Task<string> GetStyleSheet(string themeName)
+        {
+            var theme = await _themeRepo.GetAsync(p => p.ThemeName == themeName);
+            if (null == theme)
+            {
+                throw new KeyNotFoundException($"theme with name '{themeName}' does not exist");
+            }
+
+            if (string.IsNullOrWhiteSpace(theme.CssRules))
+            {
+                throw new InvalidDataException($"'{themeName}' is having empty CSS Rules");
+            }
+
+            var rules = JsonSerializer.Deserialize<IDictionary<string, string>>(theme.CssRules);
+            if (null == rules)
+            {
+                throw new InvalidDataException($"'{themeName}' CssRules is not a valid json");
+            }
+
+            var sb = new StringBuilder();
+            sb.Append(":root {");
+            foreach (var (key, value) in rules)
+            {
+                if (null != key && null != value)
+                {
+                    sb.Append($"{key}: {value};");
+                }
+            }
+            sb.Append('}');
+
+            return sb.ToString();
         }
     }
 }
