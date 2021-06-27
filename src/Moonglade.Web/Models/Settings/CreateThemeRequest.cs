da288039f4735b84e2ec289e5058f670e84ae3c0
Edi Wang <Edi.Wang@outlook.com>
2021-06-27 12:37:13 +0800

    Update CreateThemeRequest.cs

--- src/Moonglade.Web/Models/Settings/CreateThemeRequest.cs
+++ src/Moonglade.Web/Models/Settings/CreateThemeRequest.cs
@@ -13,12 +13,15 @@ namespace Moonglade.Web.Models.Settings
         public string Name { get; set; }
 
         [Required]
+        [RegularExpression(@"(#([\da-f]{3}){1,2}|(rgb|hsl)a\((\d{1,3}%?,\s?){3}(1|0?\.\d+)\)|(rgb|hsl)\(\d{1,3}%?(,\s?\d{1,3}%?){2}\))")]
         public string AccentColor1 { get; set; }
 
         [Required]
+        [RegularExpression(@"(#([\da-f]{3}){1,2}|(rgb|hsl)a\((\d{1,3}%?,\s?){3}(1|0?\.\d+)\)|(rgb|hsl)\(\d{1,3}%?(,\s?\d{1,3}%?){2}\))")]
         public string AccentColor2 { get; set; }
 
         [Required]
+        [RegularExpression(@"(#([\da-f]{3}){1,2}|(rgb|hsl)a\((\d{1,3}%?,\s?){3}(1|0?\.\d+)\)|(rgb|hsl)\(\d{1,3}%?(,\s?\d{1,3}%?){2}\))")]
         public string AccentColor3 { get; set; }
     }
 }
