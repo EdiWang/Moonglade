a2b27e24f54c798c6cf9b248a66a651eaab9e34b
Edi Wang <Edi.Wang@outlook.com>
2021-06-27 15:33:50 +0800

    code clean up

--- src/Moonglade.Web/Controllers/ThemeController.cs
+++ src/Moonglade.Web/Controllers/ThemeController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
