c802b5da8519e4fb264ca5ee4c85c83d60ac7726
Edi Wang <Edi.Wang@outlook.com>
2021-07-30 18:39:47 +0800

    use health check

--- src/Moonglade.Web/Startup.cs
+++ src/Moonglade.Web/Startup.cs
@@ -131,6 +131,8 @@ namespace Moonglade.Web
                 options.AppendTrailingSlash = false;
             });
 
+            services.AddHealthChecks();
+
             // Blog Services
             services.AddCoreBloggingServices()
                     .AddPingback()
