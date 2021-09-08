32b236a2ff715e037578943fbe39078aaa0ee39b
Edi Wang <Edi.Wang@outlook.com>
2021-09-08 19:27:23 +0800

    Update PostQueryService.cs

--- src/Moonglade.Core/PostQueryService.cs
+++ src/Moonglade.Core/PostQueryService.cs
@@ -20,13 +20,9 @@ namespace Moonglade.Core
 
     public class PostQueryService : IPostQueryService
     {
-        #region Repository Objects
-
         private readonly IRepository<PostEntity> _postRepo;
         private readonly IRepository<PostTagEntity> _postTagRepo;
         private readonly IRepository<PostCategoryEntity> _postCatRepo;
-
-        #endregion
 
         public PostQueryService(
             IRepository<PostEntity> postRepo,
@@ -38,8 +34,6 @@ namespace Moonglade.Core
             _postCatRepo = postCatRepo;
         }
 
-        #region Counts
-
         public int CountPublic() => _postRepo.Count(p => p.IsPublished && !p.IsDeleted);
 
         public int CountByCategory(Guid catId) => _postCatRepo.Count(c => c.CategoryId == catId
@@ -48,8 +42,6 @@ namespace Moonglade.Core
         public int CountByTag(int tagId) => _postTagRepo.Count(p => p.TagId == tagId && p.Post.IsPublished && !p.Post.IsDeleted);
 
         public int CountByFeatured() => _postRepo.Count(p => p.IsFeatured && p.IsPublished && !p.IsDeleted);
-
-        #endregion
 
         public async Task<(IReadOnlyList<PostSegment> Posts, int TotalRows)> ListSegmentAsync(
             PostStatus status, int offset, int pageSize, string keyword = null)
