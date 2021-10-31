dd5a7f4ca6ed2693adde96db177ff09df7986f5e
Edi Wang <Edi.Wang@outlook.com>
2021-10-31 08:56:45 +0800

    fix UT

--- src/Tests/Moonglade.Web.Tests/DateTimeResolverTests.cs
+++ src/Tests/Moonglade.Web.Tests/DateTimeResolverTests.cs
@@ -1,14 +1,37 @@
-﻿using NUnit.Framework;
+﻿using Moonglade.Configuration;
+using Moq;
+using NUnit.Framework;
 
 namespace Moonglade.Web.Tests;
 
 [TestFixture]
 public class DateTimeResolverTests
 {
+    private MockRepository _mockRepository;
+    private Mock<IBlogConfig> _mockBlogConfig;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _mockRepository = new(MockBehavior.Default);
+        _mockBlogConfig = _mockRepository.Create<IBlogConfig>();
+    }
+
+    private BlogTimeZoneResolver CreateResolver(string timeZoneUtcOffset)
+    {
+        _mockBlogConfig.Setup(p => p.GeneralSettings).Returns(new GeneralSettings
+        {
+            TimeZoneUtcOffset = timeZoneUtcOffset
+        });
+
+        var resolver = new BlogTimeZoneResolver(_mockBlogConfig.Object);
+        return resolver;
+    }
+
     [Test]
     public void InvalidTimeSpanFormat()
     {
-        var resolver = new BlogTimeZoneResolver("996ICU");
+        var resolver = CreateResolver("996.ICU");
         var utc = new DateTime(2000, 1, 1, 0, 0, 0);
 
         Assert.Throws<FormatException>(() =>
@@ -20,7 +43,7 @@ public class DateTimeResolverTests
     [Test]
     public void GetDateTime_UserTZoneZero()
     {
-        var resolver = new BlogTimeZoneResolver(string.Empty);
+        var resolver = CreateResolver(string.Empty);
 
         var utc = new DateTime(2000, 1, 1, 0, 0, 0);
         var dt = resolver.ToTimeZone(utc);
@@ -32,7 +55,7 @@ public class DateTimeResolverTests
     public void GetDateTime_UserTZone()
     {
         var tSpan = "02:51:00";
-        var resolver = new BlogTimeZoneResolver(tSpan);
+        var resolver = CreateResolver(tSpan);
 
         var utc = new DateTime(2000, 1, 1, 0, 0, 0);
         var dt = resolver.ToTimeZone(utc);
@@ -44,7 +67,7 @@ public class DateTimeResolverTests
     public void GetUtcTimeFromUserTZone()
     {
         var tSpan = "10:55:00";
-        var resolver = new BlogTimeZoneResolver(tSpan);
+        var resolver = CreateResolver(tSpan);
 
         var dt = resolver.ToUtc(DateTime.Parse("2000/1/1 10:55:00"));
         var utc = new DateTime(2000, 1, 1, 0, 0, 0);
@@ -56,7 +79,7 @@ public class DateTimeResolverTests
     public void GetUtcTimeFromUserTZoneStd()
     {
         var tSpan = "8:00:00";
-        var resolver = new BlogTimeZoneResolver(tSpan);
+        var resolver = CreateResolver(tSpan);
 
         var dt = resolver.ToUtc(DateTime.Parse("2000/1/1 8:00:00"));
         var utc = new DateTime(2000, 1, 1, 0, 0, 0);
@@ -69,7 +92,7 @@ public class DateTimeResolverTests
     public void GetTimeSpanByZoneId()
     {
         var tSpan = "10:55:00";
-        var resolver = new BlogTimeZoneResolver(tSpan);
+        var resolver = CreateResolver(tSpan);
         var ts = resolver.GetTimeSpanByZoneId("China Standard Time");
         Assert.AreEqual(TimeSpan.FromHours(8), ts);
     }
@@ -78,7 +101,7 @@ public class DateTimeResolverTests
     public void GetTimeSpanByZoneId_Empty()
     {
         var tSpan = "10:55:00";
-        var resolver = new BlogTimeZoneResolver(tSpan);
+        var resolver = CreateResolver(tSpan);
         var ts = resolver.GetTimeSpanByZoneId(string.Empty);
         Assert.AreEqual(TimeSpan.Zero, ts);
     }
@@ -87,7 +110,7 @@ public class DateTimeResolverTests
     public void GetNow_UserTZone()
     {
         var tSpan = "8:00:00";
-        var resolver = new BlogTimeZoneResolver(tSpan);
+        var resolver = CreateResolver(tSpan);
         var utc = DateTime.UtcNow;
         var dt = resolver.NowOfTimeZone;
         Assert.AreEqual(utc.AddHours(8).Date, dt.Date);
