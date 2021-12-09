29181a1df73f8808e8d1fcf44fbb301e28a75520
Edi Wang <Edi.Wang@outlook.com>
2021-12-09 14:16:18 +0800

    AddXRobotsTagAttribute

---
+++ src/Moonglade.Web/AddXRobotsTagAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Moonglade.Web;
+
+public class AddXRobotsTagAttribute : ResultFilterAttribute
+{
+    private readonly string _content;
+
+    public AddXRobotsTagAttribute(string content)
+    {
+        _content = content;
+    }
+
+    public override void OnResultExecuting(ResultExecutingContext context)
+    {
+        if (!context.HttpContext.Response.Headers.ContainsKey("X-Robots-Tag"))
+        {
+            context.HttpContext.Response.Headers.Add("X-Robots-Tag", _content);
+        }
+
+        base.OnResultExecuting(context);
+    }
+}
