925a9ac45a0b21a96031fc00dc678a3ad961c7c1
Edi Wang <Edi.Wang@outlook.com>
2021-12-21 11:02:40 +0800

    Update ContentSettings.cs

--- src/Moonglade.Configuration/ContentSettings.cs
+++ src/Moonglade.Configuration/ContentSettings.cs
@@ -4,6 +4,13 @@ namespace Moonglade.Configuration;
 
 public class ContentSettings : IBlogSettings
 {
+    [Display(Name = "Comment provider")]
+    public CommentProvider CommentProvider { get; set; }
+
+    [Display(Name = "Third party comment html pitch")]
+    [MaxLength(1024)]
+    public string ThirdPartyCommentHtmlPitch { get; set; }
+    
     [Display(Name = "Enable comments")]
     public bool EnableComments { get; set; }
 
