d2ae250a58530b43dcc0c0aebe7c91a28f73b47e
pzy <m@pzy.io>
2022-01-01 19:04:45 +0800

    Add regular file name generator.

---
+++ src/Moonglade.ImageStorage/RegularFileNameGenerator.cs
@@ -0,0 +1,27 @@
+﻿namespace Moonglade.ImageStorage
+{
+    public class RegularFileNameGenerator : IFileNameGenerator
+    {
+        public string Name => nameof(RegularFileNameGenerator);
+
+        public string GetFileName(string fileName, string appendixName = "")
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentNullException(nameof(fileName));
+            }
+
+            var ext = Path.GetExtension(fileName);
+            if (string.IsNullOrEmpty(ext) ||
+                string.IsNullOrWhiteSpace(Path.GetFileNameWithoutExtension(fileName)))
+            {
+                throw new ArgumentException("Invalid File Name", nameof(fileName));
+            }
+
+            var uniqueId = DateTime.Now.ToString("yyMMdd") + Guid.NewGuid().ToString("N")[..6];
+
+            var newFileName = $"img-{uniqueId}{(string.IsNullOrWhiteSpace(appendixName) ? string.Empty : "-" + appendixName)}{ext}".ToLower();
+            return newFileName;
+        }
+    }
+}
