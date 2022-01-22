05928bef216631aa5379c61eb470c21687b3e05c
Edi Wang <Edi.Wang@outlook.com>
2022-01-22 09:36:04 +0800

    BlogConfigurationEntity

---
+++ src/Moonglade.Data/Entities/BlogConfigurationEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Moonglade.Data.Entities;
+
+public class BlogConfigurationEntity
+{
+    public int Id { get; set; }
+
+    public string CfgKey { get; set; }
+
+    public string CfgValue { get; set; }
+
+    public DateTime? LastModifiedTimeUtc { get; set; }
+}
+
+[ExcludeFromCodeCoverage]
+internal class BlogConfigurationConfiguration : IEntityTypeConfiguration<BlogConfigurationEntity>
+{
+    public void Configure(EntityTypeBuilder<BlogConfigurationEntity> builder)
+    {
+        builder.Property(e => e.CfgKey).HasMaxLength(64);
+    }
+}
