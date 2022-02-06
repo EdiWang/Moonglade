81f59861376d3d8fecdeb2bb6c3d19165bc734e9
Edi Wang <Edi.Wang@outlook.com>
2022-02-06 09:55:16 +0800

    remove ut

--- src/Moonglade.Data/Seed.cs
+++ src/Moonglade.Data/Seed.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Logging;
 using Moonglade.Data.Entities;
 
 namespace Moonglade.Data;
 
+[ExcludeFromCodeCoverage]
 public class Seed
 {
     public static async Task SeedAsync(BlogDbContext dbContext, ILogger logger, int retry = 0)
