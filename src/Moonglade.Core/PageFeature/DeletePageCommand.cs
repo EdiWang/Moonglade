867bef490e463696c0e6bac1daa5f10f68100613
Edi Wang <Edi.Wang@outlook.com>
2022-04-10 10:47:34 +0800

    Update DeletePageCommand.cs

--- src/Moonglade.Core/PageFeature/DeletePageCommand.cs
+++ src/Moonglade.Core/PageFeature/DeletePageCommand.cs
@@ -7,6 +7,6 @@ public class DeletePageCommandHandler : AsyncRequestHandler<DeletePageCommand>
     private readonly IRepository<PageEntity> _pageRepo;
     public DeletePageCommandHandler(IRepository<PageEntity> pageRepo) => _pageRepo = pageRepo;
 
-    protected override async Task Handle(DeletePageCommand request, CancellationToken cancellationToken) => 
+    protected override async Task Handle(DeletePageCommand request, CancellationToken cancellationToken) =>
         await _pageRepo.DeleteAsync(request.Id, cancellationToken);
 }
