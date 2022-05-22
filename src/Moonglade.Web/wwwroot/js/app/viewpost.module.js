2be54aced8b5a1179ac2ad2aa11a11ac8f6f86ae
Edi Wang <Edi.Wang@outlook.com>
2022-05-22 13:13:47 +0800

    use module

--- src/Moonglade.Web/wwwroot/js/app/viewpost.module.js
+++ src/Moonglade.Web/wwwroot/js/app/viewpost.module.js
@@ -118,3 +118,39 @@ export function resetCaptchaImage() {
     const d = new Date();
     document.querySelector('#img-captcha').src = `/captcha-image?${d.getTime()}`;
 }
+
+var btnSubmitComment = '#btn-submit-comment';
+
+export function submitComment(pid) {
+    document.querySelector('#thx-for-comment').style.display = 'none';
+    document.querySelector('#thx-for-comment-non-review').style.display = 'none';
+
+    document.querySelector('#loadingIndicator').style.display = 'block';
+    document.querySelector(btnSubmitComment).classList.add('disabled');
+    document.querySelector(btnSubmitComment).setAttribute('disabled', 'disabled');
+
+    callApi(`/api/comment/${pid}`, 'POST',
+        {
+            username: document.querySelector('#input-comment-name').value,
+            content: document.querySelector('#input-comment-content').value,
+            email: document.querySelector('#input-comment-email').value,
+            captchaCode: document.querySelector('#input-comment-captcha').value
+        },
+        (success) => {
+            document.querySelector('#comment-form').reset();
+            resetCaptchaImage();
+
+            var httpCode = success.status;
+            if (httpCode === 201) {
+                document.querySelector('#thx-for-comment').style.display = 'block';
+            }
+            if (httpCode === 200) {
+                document.querySelector('#thx-for-comment-non-review').style.display = 'block';
+            }
+        },
+        (always) => {
+            document.querySelector('#loadingIndicator').style.display = 'none';
+            document.querySelector(btnSubmitComment).classList.remove('disabled');
+            document.querySelector(btnSubmitComment).removeAttribute('disabled');
+        });
+}
