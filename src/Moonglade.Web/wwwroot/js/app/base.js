17494059a5320746bd6cf1df861bdb45c194256e
Edi Wang <Edi.Wang@outlook.com>
2022-05-23 13:10:07 +0800

    code refact

--- src/Moonglade.Web/wwwroot/js/app/base.js
+++ src/Moonglade.Web/wwwroot/js/app/base.js
@@ -1,4 +1,7 @@
-﻿var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+﻿import * as blogToast from '/js/app/blogtoast.module.js'
+window.blogToast = blogToast;
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl);
 });
