d77d4de01f66ffd3c3552e6cad508228f72b4ed3
Edi Wang <edi.wang@outlook.com>
2022-08-26 08:18:08 +0800

    HashPassword2 method

--- src/Moonglade.Data/Entities/LocalAccountEntity.cs
+++ src/Moonglade.Data/Entities/LocalAccountEntity.cs
@@ -4,6 +4,7 @@ public class LocalAccountEntity
 {
     public Guid Id { get; set; }
     public string Username { get; set; }
+    public string PasswordSalt { get; set; }
     public string PasswordHash { get; set; }
     public DateTime? LastLoginTimeUtc { get; set; }
     public string LastLoginIp { get; set; }
--- src/Moonglade.Utils/Helper.cs
+++ src/Moonglade.Utils/Helper.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.Win32;
 using System.Net;
@@ -161,6 +162,30 @@ public static class Helper
         using var sha = SHA256.Create();
         sha.TransformFinalBlock(data, 0, data.Length);
         return Convert.ToBase64String(sha.Hash ?? throw new InvalidOperationException());
+    }
+
+    // https://docs.microsoft.com/en-us/aspnet/core/security/data-protection/consumer-apis/password-hashing?view=aspnetcore-6.0
+    // This is not secure, but better than nothing.
+    public static string HashPassword2(string clearPassword, string saltBase64)
+    {
+        var salt = Convert.FromBase64String(saltBase64);
+
+        // derive a 256-bit subkey (use HMACSHA256 with 100,000 iterations)
+        string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: clearPassword!,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+            numBytesRequested: 256 / 8));
+
+        return hashed;
+    }
+
+    public static string GenerateSalt()
+    {
+        // Generate a 128-bit salt using a sequence of cryptographically strong random bytes.
+        byte[] salt = RandomNumberGenerator.GetBytes(128 / 8); // divide by 8 to convert bits to bytes
+        return Convert.ToBase64String(salt);
     }
 
     public static string ResolveRootUrl(HttpContext ctx, string canonicalPrefix, bool preferCanonical = false, bool removeTailSlash = false)
