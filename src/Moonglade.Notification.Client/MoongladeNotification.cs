bb672cdd707ec306502103c6bb8f4aecd3cde02f
Edi Wang <edi.wang@outlook.com>
2022-09-09 09:14:48 +0800

    Update MoongladeNotification.cs

--- src/Moonglade.Notification.Client/MoongladeNotification.cs
+++ src/Moonglade.Notification.Client/MoongladeNotification.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moonglade.Configuration;
+using Moonglade.Data.Entities;
+using Moonglade.Data.Infrastructure;
 
 namespace Moonglade.Notification.Client;
 
@@ -13,8 +18,24 @@ public interface IMoongladeNotification
 
 public class MoongladeNotification : IMoongladeNotification
 {
-    public Task<Guid> EnqueueNotification<T>(MailMesageTypes type, string[] toAddresses, T payload) where T : class
+    private readonly bool _isEnabled;
+    private readonly ILogger<MoongladeNotification> _logger;
+    private readonly IRepository<EmailNotificationEntity> _repo;
+
+    public MoongladeNotification(
+        ILogger<MoongladeNotification> logger,
+        IRepository<EmailNotificationEntity> repo,
+        IBlogConfig blogConfig)
     {
+        _logger = logger;
+        _repo = repo;
+        _isEnabled = blogConfig.NotificationSettings.EnableEmailSending;
+    }
+
+    public async Task<Guid> EnqueueNotification<T>(MailMesageTypes type, string[] toAddresses, T payload) where T : class
+    {
+        if (!_isEnabled) return Guid.Empty;
+
         throw new NotImplementedException();
     }
 }
