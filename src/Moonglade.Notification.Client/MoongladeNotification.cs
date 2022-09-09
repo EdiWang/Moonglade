da378b248d4f2198e4dc01e16b080cf7e0cdc6af
Edi Wang <edi.wang@outlook.com>
2022-09-09 09:10:28 +0800

    Create MoongladeNotification.cs

---
+++ src/Moonglade.Notification.Client/MoongladeNotification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moonglade.Notification.Client;
+
+public interface IMoongladeNotification
+{
+    Task<Guid> EnqueueNotification<T>(MailMesageTypes type, string[] toAddresses, T payload) where T : class;
+}
+
+public class MoongladeNotification : IMoongladeNotification
+{
+    public Task<Guid> EnqueueNotification<T>(MailMesageTypes type, string[] toAddresses, T payload) where T : class
+    {
+        throw new NotImplementedException();
+    }
+}
