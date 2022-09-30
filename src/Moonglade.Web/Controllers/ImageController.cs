88fa442a7a114010f9f800c189b8d8609d8a80f4
Edi Wang <edi.wang@outlook.com>
2022-09-30 19:51:45 +0800

    clean up

--- src/Moonglade.Web/Controllers/ImageController.cs
+++ src/Moonglade.Web/Controllers/ImageController.cs
@@ -143,14 +143,6 @@ public class ImageController : ControllerBase
         var location = $"/image/{finalName}";
         var filename = location;
 
-        // Do NOT use CDN url here because CDN endpoint can change
-        //if (_blogConfig.ImageSettings.EnableCDNRedirect)
-        //{
-        //    var url = _blogConfig.ImageSettings.CDNEndpoint.CombineUrl(finalName);
-        //    location = url;
-        //    filename = url;
-        //}
-
         return Ok(new
         {
             location,
