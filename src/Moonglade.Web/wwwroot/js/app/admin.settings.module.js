64f6cff12500f5a59f43994db012b13f8d039ff4
Edi Wang <Edi.Wang@outlook.com>
2022-12-07 11:02:36 +0800

    code refact

--- src/Moonglade.Web/wwwroot/js/app/admin.settings.module.js
+++ src/Moonglade.Web/wwwroot/js/app/admin.settings.module.js
@@ -19,3 +19,46 @@ export function handleSettingsSubmit(event) {
             document.querySelector(btnSaveSettings).removeAttribute('disabled');
         });
 }
+
+export function compareVersionNumbers(v1, v2) {
+    var v1parts = v1.split('.');
+    var v2parts = v2.split('.');
+
+    // First, validate both numbers are true version numbers
+    function validateParts(parts) {
+        for (var i = 0; i < parts.length; ++i) {
+            if (!isPositiveInteger(parts[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+    if (!validateParts(v1parts) || !validateParts(v2parts)) {
+        return NaN;
+    }
+
+    for (var i = 0; i < v1parts.length; ++i) {
+        if (v2parts.length === i) {
+            return 1;
+        }
+
+        if (v1parts[i] === v2parts[i]) {
+            continue;
+        }
+        if (v1parts[i] > v2parts[i]) {
+            return 1;
+        }
+        return -1;
+    }
+
+    if (v1parts.length != v2parts.length) {
+        return -1;
+    }
+
+    return 0;
+}
+
+function isPositiveInteger(x) {
+    // http://stackoverflow.com/a/1019526/11236
+    return /^\d+$/.test(x);
+}
