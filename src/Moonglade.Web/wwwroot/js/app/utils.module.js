66dff9f6a523d3784d0642a910dae7bfb08babb9
Edi Wang <Edi.Wang@outlook.com>
2022-12-07 14:28:30 +0800

    remove SubscriptionModel

--- src/Moonglade.Web/wwwroot/js/app/utils.module.js
+++ src/Moonglade.Web/wwwroot/js/app/utils.module.js
@@ -4,15 +4,19 @@
         if (Object.prototype.hasOwnProperty.call(value, item)) {
             if (!value[item]) {
                 newValue[item.replace('ViewModel.', '')] = null;
+                newValue[item.replace('settings.', '')] = null;
             }
             else if (value[item] && !Array.isArray(value[item]) && value[item].toLowerCase() === 'true') {
                 newValue[item.replace('ViewModel.', '')] = true;
+                newValue[item.replace('settings.', '')] = true;
             }
             else if (value[item] && !Array.isArray(value[item]) && value[item].toLowerCase() === 'false') {
                 newValue[item.replace('ViewModel.', '')] = false;
+                newValue[item.replace('settings.', '')] = false;
             }
             else {
                 newValue[item.replace('ViewModel.', '')] = value[item];
+                newValue[item.replace('settings.', '')] = value[item];
             }
         }
     }
