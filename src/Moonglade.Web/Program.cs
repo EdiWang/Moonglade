ee5becbb865091105cdf344e282bbdd56e148430
Edi Wang <Edi.Wang@outlook.com>
2022-12-20 14:52:25 +0800

    rename AzureKnownProxies to KnownProxies

--- src/Moonglade.Web/Program.cs
+++ src/Moonglade.Web/Program.cs
@@ -64,7 +64,7 @@ void ConfigureServices(IServiceCollection services)
     // Fix docker deployments on Azure App Service blows up with Azure AD authentication
     // https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/proxy-load-balancer?view=aspnetcore-6.0
     // "Outside of using IIS Integration when hosting out-of-process, Forwarded Headers Middleware isn't enabled by default."
-    var knownProxies = builder.Configuration.GetSection("AzureKnownProxies").Get<string[]>();
+    var knownProxies = builder.Configuration.GetSection("KnownProxies").Get<string[]>();
     builder.Services.Configure<ForwardedHeadersOptions>(options =>
     {
         options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
