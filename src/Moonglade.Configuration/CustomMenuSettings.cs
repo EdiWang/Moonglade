eceae21645587acc5798aa24ae12b2877a315a2c
Edi Wang <Edi.Wang@outlook.com>
2023-03-24 12:57:29 +0800

    Create CustomMenuSettings.cs

---
+++ src/Moonglade.Configuration/CustomMenuSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Moonglade.Configuration;
+
+public class CustomMenuSettings : IBlogSettings
+{
+    [Display(Name = "Enable custom menus")]
+    public bool IsEnabled { get; set; }
+
+    [MaxLength(10240)]
+    public string MenuJson { get; set; }
+}
