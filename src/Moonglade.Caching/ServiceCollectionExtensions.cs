392e213d5b04b5d9b9f7911935ed808f9d85c5f8
Edi Wang <Edi.Wang@outlook.com>
2023-06-14 10:32:00 +0800

    rename to AddInMemoryCacheAside

--- src/Moonglade.Caching/ServiceCollectionExtensions.cs
+++ src/Moonglade.Caching/ServiceCollectionExtensions.cs
@@ -4,7 +4,7 @@ namespace Moonglade.CacheAside.InMemory;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddBlogCache(this IServiceCollection services)
+    public static IServiceCollection AddInMemoryCacheAside(this IServiceCollection services)
     {
         services.AddMemoryCache();
         services.AddSingleton<ICacheAside, MemoryCacheAside>();
