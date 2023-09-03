4b8aae6b43016962d7c7ab10c12ce5fcc8d59e85
Edi Wang <Edi.Wang@outlook.com>
2023-09-03 16:20:04 +0800

    Create DeleteStyleSheetCommand.cs

---
+++ src/Moonglade.Core/DeleteStyleSheetCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace Moonglade.Core;
+
+public record DeleteStyleSheetCommand(Guid Id) : IRequest;
+
+public class DeleteStyleSheetCommandHandler : IRequestHandler<DeleteStyleSheetCommand>
+{
+    private readonly IRepository<StyleSheetEntity> _repo;
+
+    public DeleteStyleSheetCommandHandler(IRepository<StyleSheetEntity> repo) => _repo = repo;
+
+    public async Task Handle(DeleteStyleSheetCommand request, CancellationToken cancellationToken)
+    {
+        var styleSheet = await _repo.GetAsync(request.Id, cancellationToken);
+        if (styleSheet is null)
+        {
+            throw new InvalidOperationException($"StyleSheetEntity with Id '{request.Id}' not found.");
+        }
+
+        await _repo.DeleteAsync(styleSheet, cancellationToken);
+    }
+}
