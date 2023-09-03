4295dffa31095d1150fd29349c04a2e2ded1927e
Edi Wang <Edi.Wang@outlook.com>
2023-09-03 15:09:45 +0800

    Create StyleSheetEntity.cs

---
+++ src/Moonglade.Data/Entities/StyleSheetEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Moonglade.Data.Entities;
+
+public class StyleSheetEntity
+{
+    public Guid Id { get; set; }
+
+    public string FriendlyName { get; set; }
+
+    public string Hash { get; set; }
+
+    public string CssContent { get; set; }
+
+    public DateTime LastModifiedTimeUtc { get; set; }
+}
