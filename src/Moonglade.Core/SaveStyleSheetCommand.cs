2a95c57b59b267020b2de2ff4883210c6c9c08a6
Edi Wang <Edi.Wang@outlook.com>
2023-09-03 15:46:04 +0800

    Create SaveStyleSheetCommand.cs

---
+++ src/Moonglade.Core/SaveStyleSheetCommand.cs
@@ -0,0 +1,55 @@
+﻿using System.Security.Cryptography;
+using Moonglade.Data.Spec;
+
+namespace Moonglade.Core;
+
+public record SaveStyleSheetCommand(string Slug, string CssContent) : IRequest<Guid>;
+
+public class SaveStyleSheetCommandHandler : IRequestHandler<SaveStyleSheetCommand, Guid>
+{
+    private readonly IRepository<StyleSheetEntity> _repo;
+
+    public SaveStyleSheetCommandHandler(IRepository<StyleSheetEntity> repo) => _repo = repo;
+
+    public async Task<Guid> Handle(SaveStyleSheetCommand request, CancellationToken cancellationToken)
+    {
+        var slug = request.Slug.ToLower().Trim();
+        var css = request.CssContent.Trim();
+        var hash = CalculateHash($"{slug}_{css}");
+
+        var entity = await _repo.GetAsync(new StyleSheetByFriendlyNameSpec(slug), cancellationToken);
+        if (entity is null)
+        {
+            entity = new()
+            {
+                Id = Guid.NewGuid(),
+                FriendlyName = slug,
+                CssContent = css,
+                Hash = hash,
+                LastModifiedTimeUtc = DateTime.UtcNow
+            };
+
+            await _repo.AddAsync(entity, cancellationToken);
+        }
+        else
+        {
+            entity.CssContent = css;
+            entity.Hash = hash;
+            entity.LastModifiedTimeUtc = DateTime.UtcNow;
+
+            await _repo.UpdateAsync(entity, cancellationToken);
+        }
+
+        return entity.Id;
+    }
+
+    private string CalculateHash(string content)
+    {
+        var sha256 = SHA256.Create();
+
+        byte[] inputBytes = Encoding.ASCII.GetBytes(content);
+        byte[] outputBytes = sha256.ComputeHash(inputBytes);
+
+        return Convert.ToBase64String(outputBytes);
+    }
+}
