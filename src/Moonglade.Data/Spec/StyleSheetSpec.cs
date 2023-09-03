d2328821a35af903ef1fb3a443785b1e0b1f3b15
Edi Wang <Edi.Wang@outlook.com>
2023-09-03 15:46:02 +0800

    Create StyleSheetSpec.cs

---
+++ src/Moonglade.Data/Spec/StyleSheetSpec.cs
@@ -0,0 +1,11 @@
+﻿using Moonglade.Data.Entities;
+using Moonglade.Data.Infrastructure;
+
+namespace Moonglade.Data.Spec;
+
+public class StyleSheetByFriendlyNameSpec : BaseSpecification<StyleSheetEntity>
+{
+    public StyleSheetByFriendlyNameSpec(string friendlyName) : base(p => p.FriendlyName == friendlyName)
+    {
+    }
+}
