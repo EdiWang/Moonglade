b1932510bc97671a7d368474b7aeadd290e65a5b
Edi Wang <Edi.Wang@outlook.com>
2023-09-04 09:58:10 +0800

    Create GetStyleSheetQuery.cs

---
+++ src/Moonglade.Core/GetStyleSheetQuery.cs
@@ -0,0 +1,16 @@
+﻿namespace Moonglade.Core;
+
+public record GetStyleSheetQuery(Guid Id) : IRequest<StyleSheetEntity>;
+
+public class GetStyleSheetQueryHandler : IRequestHandler<GetStyleSheetQuery, StyleSheetEntity>
+{
+    private readonly IRepository<StyleSheetEntity> _repo;
+
+    public GetStyleSheetQueryHandler(IRepository<StyleSheetEntity> repo) => _repo = repo;
+
+    public async Task<StyleSheetEntity> Handle(GetStyleSheetQuery request, CancellationToken cancellationToken)
+    {
+        var result = await _repo.GetAsync(request.Id, cancellationToken);
+        return result;
+    }
+}
