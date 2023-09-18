db02fa9e90732517a350eac2e9207e672f89c07e
Edi Wang <Edi.Wang@outlook.com>
2023-09-18 10:20:14 +0800

    use css over js

--- src/Moonglade.Web/wwwroot/js/app/theme.module.js
+++ src/Moonglade.Web/wwwroot/js/app/theme.module.js
@@ -9,7 +9,7 @@ export function useDarkMode() {
 
     $('body').addClass('bg-moca-dark text-light darkmode');
     $('body.body-post-slug').removeClass('bg-gray-1');
-    $('.article-post-slug').removeClass('bg-white p-4 border');
+    $('.article-post-slug').removeClass('p-4 border');
 
     $('.card').addClass('text-white bg-dark');
     $('.list-group-item, .card-body').addClass('bg-moca-dark text-light');
@@ -40,7 +40,7 @@ export function useLightMode() {
 
     $('body').removeClass('bg-moca-dark text-light darkmode');
     $('body.body-post-slug').addClass('bg-gray-1');
-    $('.article-post-slug').addClass('bg-white p-4 border');
+    $('.article-post-slug').addClass('p-4 border');
 
     $('.card').removeClass('text-white bg-dark');
     $('.list-group-item, .card-body').removeClass('bg-moca-dark text-light');
