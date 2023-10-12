9561c0bcbfa521388d580b51c588a4b4f5d46266
Edi Wang <Edi.Wang@outlook.com>
2023-10-12 13:45:04 +0800

    remove unused code

--- src/Moonglade.Web/PagedList/PagedList.cs
+++ src/Moonglade.Web/PagedList/PagedList.cs
@@ -2,54 +2,6 @@
 
 namespace Moonglade.Web.PagedList;
 
-public class PagedList<T, TKey> : BasePagedList<T>
-{
-    /// <summary>
-    /// Initializes a new instance of the <see cref="PagedList{T}"/> class that divides the supplied superset into
-    /// subsets the size of the supplied pageSize. The instance then only contains the objects contained in the
-    /// subset specified by index.
-    /// </summary>
-    /// <param name="superset">
-    /// The collection of objects to be divided into subsets. If the collection
-    /// implements <see cref="IQueryable{T}"/>, it will be treated as such.
-    /// </param>
-    /// <param name="keySelector">Expression for Order</param>
-    /// <param name="pageNumber">
-    /// The one-based index of the subset of objects to be contained by this instance.
-    /// </param>
-    /// <param name="pageSize">The maximum size of any individual subset.</param>
-    /// <exception cref="ArgumentOutOfRangeException">The specified index cannot be less than zero.</exception>
-    /// <exception cref="ArgumentOutOfRangeException">The specified page size cannot be less than one.</exception>
-    public PagedList(IQueryable<T> superset, Expression<Func<T, TKey>> keySelector, int pageNumber, int pageSize)
-        : base(pageNumber, pageSize, superset?.Count() ?? 0)
-    {
-        // add items to internal list
-        if (TotalItemCount > 0)
-        {
-            InitSubset(superset, keySelector.Compile(), pageNumber, pageSize);
-        }
-    }
-
-    public PagedList(IQueryable<T> superset, Func<T, TKey> keySelectorMethod, int pageNumber, int pageSize)
-        : base(pageNumber, pageSize, superset?.Count() ?? 0)
-    {
-        if (TotalItemCount > 0)
-        {
-            InitSubset(superset, keySelectorMethod, pageNumber, pageSize);
-        }
-    }
-
-    private void InitSubset(IEnumerable<T> superset, Func<T, TKey> keySelectorMethod, int pageNumber, int pageSize)
-    {
-        // add items to internal list
-
-        var skip = (pageNumber - 1) * pageSize;
-        var items = superset.OrderBy(keySelectorMethod).Skip(skip).Take(pageSize).ToList();
-
-        Subset.AddRange(items);
-    }
-}
-
 /// <summary>
 /// Represents a subset of a collection of objects that can be individually accessed by index and containing
 /// metadata about the superset collection of objects this subset was created from.
@@ -108,39 +60,4 @@ public class PagedList<T> : BasePagedList<T>
         : this(superset.AsQueryable<T>(), pageNumber, pageSize)
     {
     }
-
-    /// <summary>
-    /// For Clone PagedList
-    /// </summary>
-    /// <param name="pagedList">Source PagedList</param>
-    /// <param name="collection">Source collection</param>
-    public PagedList(IPagedList pagedList, IEnumerable<T> collection)
-    {
-        TotalItemCount = pagedList.TotalItemCount;
-        PageSize = pagedList.PageSize;
-        PageNumber = pagedList.PageNumber;
-        PageCount = pagedList.PageCount;
-        HasPreviousPage = pagedList.HasPreviousPage;
-        HasNextPage = pagedList.HasNextPage;
-        IsFirstPage = pagedList.IsFirstPage;
-        IsLastPage = pagedList.IsLastPage;
-        FirstItemOnPage = pagedList.FirstItemOnPage;
-        LastItemOnPage = pagedList.LastItemOnPage;
-
-        Subset.AddRange(collection);
-
-        if (Subset.Count > PageSize)
-        {
-            throw new Exception($"{nameof(collection)} size can't be greater than PageSize");
-        }
-
-    }
-
-    /// <summary>
-    /// Create empty static paged list
-    /// </summary>
-    /// <param name="pageSize"></param>
-    /// <returns></returns>
-    public static PagedList<T> Empty(int pageSize = DefaultPageSize) =>
-        new(Array.Empty<T>(), 1, pageSize);
 }
