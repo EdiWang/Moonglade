31c67a22d71d047a23edf0ed8df61715721d2f64
Edi Wang <Edi.Wang@outlook.com>
2023-10-13 16:03:50 +0800

    C# 12 on PageFeature

--- src/Moonglade.Core/PageFeature/GetPageBySlugQuery.cs
+++ src/Moonglade.Core/PageFeature/GetPageBySlugQuery.cs
@@ -2,16 +2,12 @@
 
 public record GetPageBySlugQuery(string Slug) : IRequest<BlogPage>;
 
-public class GetPageBySlugQueryHandler : IRequestHandler<GetPageBySlugQuery, BlogPage>
+public class GetPageBySlugQueryHandler(IRepository<PageEntity> repo) : IRequestHandler<GetPageBySlugQuery, BlogPage>
 {
-    private readonly IRepository<PageEntity> _repo;
-
-    public GetPageBySlugQueryHandler(IRepository<PageEntity> repo) => _repo = repo;
-
     public async Task<BlogPage> Handle(GetPageBySlugQuery request, CancellationToken ct)
     {
         var lower = request.Slug.ToLower();
-        var entity = await _repo.GetAsync(p => p.Slug == lower);
+        var entity = await repo.GetAsync(p => p.Slug == lower);
         if (entity == null) return null;
 
         var item = new BlogPage(entity);
--- src/Moonglade.Core/PageFeature/UpdatePageCommand.cs
+++ src/Moonglade.Core/PageFeature/UpdatePageCommand.cs
@@ -2,21 +2,12 @@
 
 public record UpdatePageCommand(Guid Id, EditPageRequest Payload) : IRequest<Guid>;
 
-public class UpdatePageCommandHandler : IRequestHandler<UpdatePageCommand, Guid>
+public class UpdatePageCommandHandler(IRepository<PageEntity> repo, IMediator mediator) : IRequestHandler<UpdatePageCommand, Guid>
 {
-    private readonly IRepository<PageEntity> _repo;
-    private readonly IMediator _mediator;
-
-    public UpdatePageCommandHandler(IRepository<PageEntity> repo, IMediator mediator)
-    {
-        _repo = repo;
-        _mediator = mediator;
-    }
-
     public async Task<Guid> Handle(UpdatePageCommand request, CancellationToken ct)
     {
         var (guid, payload) = request;
-        var page = await _repo.GetAsync(guid, ct);
+        var page = await repo.GetAsync(guid, ct);
         if (page is null)
         {
             throw new InvalidOperationException($"PageEntity with Id '{guid}' not found.");
@@ -27,7 +18,7 @@ public class UpdatePageCommandHandler : IRequestHandler<UpdatePageCommand, Guid>
         Guid? cssId = null;
         if (!string.IsNullOrWhiteSpace(request.Payload.CssContent))
         {
-            cssId = await _mediator.Send(new SaveStyleSheetCommand(page.Id, slug, request.Payload.CssContent), ct);
+            cssId = await mediator.Send(new SaveStyleSheetCommand(page.Id, slug, request.Payload.CssContent), ct);
         }
 
         page.Title = payload.Title.Trim();
@@ -39,7 +30,7 @@ public class UpdatePageCommandHandler : IRequestHandler<UpdatePageCommand, Guid>
         page.IsPublished = payload.IsPublished;
         page.CssId = cssId.ToString();
 
-        await _repo.UpdateAsync(page, ct);
+        await repo.UpdateAsync(page, ct);
 
         return page.Id;
     }
