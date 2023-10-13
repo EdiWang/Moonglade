42179c1056af6172e1e9dbc9520fe94ebb174bba
Edi Wang <Edi.Wang@outlook.com>
2023-10-13 14:47:20 +0800

    C# 12 on settings razor pages

--- src/Moonglade.Web/Pages/Settings/General.cshtml.cs
+++ src/Moonglade.Web/Pages/Settings/General.cshtml.cs
@@ -2,24 +2,15 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Moonglade.Web.Pages.Settings;
 
-public class GeneralModel : PageModel
+public class GeneralModel(IBlogConfig blogConfig, ITimeZoneResolver timeZoneResolver) : PageModel
 {
-    private readonly IBlogConfig _blogConfig;
-    private readonly ITimeZoneResolver _timeZoneResolver;
-
     public GeneralSettings ViewModel { get; set; }
 
     public CreateThemeRequest ThemeRequest { get; set; }
 
-    public GeneralModel(IBlogConfig blogConfig, ITimeZoneResolver timeZoneResolver)
-    {
-        _blogConfig = blogConfig;
-        _timeZoneResolver = timeZoneResolver;
-    }
-
     public void OnGet()
     {
-        ViewModel = _blogConfig.GeneralSettings;
-        ViewModel.SelectedUtcOffset = _timeZoneResolver.GetTimeSpanByZoneId(_blogConfig.GeneralSettings.TimeZoneId);
+        ViewModel = blogConfig.GeneralSettings;
+        ViewModel.SelectedUtcOffset = timeZoneResolver.GetTimeSpanByZoneId(blogConfig.GeneralSettings.TimeZoneId);
     }
 }
