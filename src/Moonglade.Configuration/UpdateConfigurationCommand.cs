ac8ef1f4ea59c6f6b0f58a8787ea3d84b3534832
Edi Wang <Edi.Wang@outlook.com>
2023-10-14 14:55:02 +0800

    C# 12 on Moonglade.Configuration

--- src/Moonglade.Configuration/UpdateConfigurationCommand.cs
+++ src/Moonglade.Configuration/UpdateConfigurationCommand.cs
@@ -7,21 +7,18 @@ namespace Moonglade.Configuration;
 
 public record UpdateConfigurationCommand(string Name, string Json) : IRequest<OperationCode>;
 
-public class UpdateConfigurationCommandHandler : IRequestHandler<UpdateConfigurationCommand, OperationCode>
+public class UpdateConfigurationCommandHandler(IRepository<BlogConfigurationEntity> repository) : IRequestHandler<UpdateConfigurationCommand, OperationCode>
 {
-    private readonly IRepository<BlogConfigurationEntity> _repository;
-    public UpdateConfigurationCommandHandler(IRepository<BlogConfigurationEntity> repository) => _repository = repository;
-
     public async Task<OperationCode> Handle(UpdateConfigurationCommand request, CancellationToken ct)
     {
         var (name, json) = request;
-        var entity = await _repository.GetAsync(p => p.CfgKey == name);
+        var entity = await repository.GetAsync(p => p.CfgKey == name);
         if (entity == null) return OperationCode.ObjectNotFound;
 
         entity.CfgValue = json;
         entity.LastModifiedTimeUtc = DateTime.UtcNow;
 
-        await _repository.UpdateAsync(entity, ct);
+        await repository.UpdateAsync(entity, ct);
         return OperationCode.Done;
     }
 }
