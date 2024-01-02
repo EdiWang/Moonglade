28ca6d82ae94aa3de1e070f260c9920c138afcbb
Edi Wang <Edi.Wang@outlook.com>
2024-01-02 13:59:20 +0800

    remove simplemde

--- src/Moonglade.Web/wwwroot/js/app/admin.editor.module.js
+++ src/Moonglade.Web/wwwroot/js/app/admin.editor.module.js
@@ -165,35 +165,28 @@ export function loadTinyMCE(textareaSelector) {
 }
 
 export function loadMdEditor(textareaSelector) {
-    if (window.SimpleMDE) {
-        simplemde = new SimpleMDE({
-            element: $(textareaSelector)[0],
-            spellChecker: false,
-            status: false
-        });
 
-        inlineAttachment.editors.codemirror4.attach(simplemde.codemirror, {
-            uploadUrl: '/image',
-            urlText: '![file]({filename})',
-            onFileUploadResponse: function (xhr) {
-                var result = JSON.parse(xhr.responseText),
-                    filename = result[this.settings.jsonFieldName];
+    inlineAttachment.editors.codemirror4.attach(simplemde.codemirror, {
+        uploadUrl: '/image',
+        urlText: '![file]({filename})',
+        onFileUploadResponse: function (xhr) {
+            var result = JSON.parse(xhr.responseText),
+                filename = result[this.settings.jsonFieldName];
 
-                if (result && filename) {
-                    var newValue;
-                    if (typeof this.settings.urlText === 'function') {
-                        newValue = this.settings.urlText.call(this, filename, result);
-                    } else {
-                        newValue = this.settings.urlText.replace(this.filenameTag, filename);
-                    }
-                    var text = this.editor.getValue().replace(this.lastValue, newValue);
-                    this.editor.setValue(text);
-                    this.settings.onFileUploaded.call(this, filename);
+            if (result && filename) {
+                var newValue;
+                if (typeof this.settings.urlText === 'function') {
+                    newValue = this.settings.urlText.call(this, filename, result);
+                } else {
+                    newValue = this.settings.urlText.replace(this.filenameTag, filename);
                 }
-                return false;
+                var text = this.editor.getValue().replace(this.lastValue, newValue);
+                this.editor.setValue(text);
+                this.settings.onFileUploaded.call(this, filename);
             }
-        });
-    }
+            return false;
+        }
+    });
 }
 
 export function keepAlive() {
