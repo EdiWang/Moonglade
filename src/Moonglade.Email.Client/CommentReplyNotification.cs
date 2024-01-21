27122dddd3d9754005a13336e3757aba73b5ce84
Edi Wang <Edi.Wang@outlook.com>
2024-01-21 15:33:27 +0800

    Update CommentReplyNotification.cs

--- src/Moonglade.Email.Client/CommentReplyNotification.cs
+++ src/Moonglade.Email.Client/CommentReplyNotification.cs
@@ -9,23 +9,11 @@ public record CommentReplyNotification(
     string ReplyContentHtml,
     string PostLink) : INotification;
 
-internal record CommentReplyPayload(
-    string CommentContent,
-    string Title,
-    string ReplyContentHtml,
-    string PostLink);
-
 public class CommentReplyNotificationHandler(IMoongladeEmailClient moongladeEmailClient) : INotificationHandler<CommentReplyNotification>
 {
     public async Task Handle(CommentReplyNotification notification, CancellationToken ct)
     {
-        var payload = new CommentReplyPayload(
-            notification.CommentContent,
-            notification.Title,
-            notification.ReplyContentHtml,
-            notification.PostLink);
-
         var dl = new[] { notification.Email };
-        await moongladeEmailClient.SendEmail(MailMesageTypes.AdminReplyNotification, dl, payload);
+        await moongladeEmailClient.SendEmail(MailMesageTypes.AdminReplyNotification, dl, notification);
     }
 }
