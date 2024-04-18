22ca3b35e02a92e5ed88cda39cbdb86a737009a0
Edi Wang <Edi.Wang@outlook.com>
2024-04-18 15:22:15 +0800

    remove support for multiple local account phase 1

--- src/Moonglade.Web/Controllers/LocalAccountController.cs
+++ src/Moonglade.Web/Controllers/LocalAccountController.cs
@@ -9,47 +9,6 @@ namespace Moonglade.Web.Controllers;
 [Route("api/[controller]")]
 public class LocalAccountController(IMediator mediator) : ControllerBase
 {
-    [HttpPost]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status409Conflict)]
-    public async Task<IActionResult> Create(CreateAccountCommand command)
-    {
-        if (await mediator.Send(new AccountExistsQuery(command.Username)))
-        {
-            ModelState.AddModelError("username", $"User '{command.Username}' already exist.");
-            return Conflict(ModelState);
-        }
-
-        await mediator.Send(command);
-        return Ok();
-    }
-
-    [HttpDelete("{id:guid}")]
-    [ProducesResponseType(StatusCodes.Status204NoContent)]
-    [ProducesResponseType(StatusCodes.Status409Conflict)]
-    public async Task<IActionResult> Delete([NotEmpty] Guid id)
-    {
-        var uidClaim = User.Claims.FirstOrDefault(c => c.Type == "uid");
-        if (null == uidClaim || string.IsNullOrWhiteSpace(uidClaim.Value))
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError, "Can not get current uid.");
-        }
-
-        if (id.ToString() == uidClaim.Value)
-        {
-            return Conflict("Can not delete current user.");
-        }
-
-        var count = await mediator.Send(new CountAccountsQuery());
-        if (count == 1)
-        {
-            return Conflict("Can not delete last account.");
-        }
-
-        await mediator.Send(new DeleteAccountCommand(id));
-        return NoContent();
-    }
-
     [HttpPut("{id:guid}/password")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status409Conflict)]
