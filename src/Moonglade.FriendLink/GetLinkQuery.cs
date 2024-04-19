c8dd4f3f369778a45cb03da5a9449c2cdfb9cce0
Edi Wang <Edi.Wang@outlook.com>
2024-04-19 13:41:57 +0800

    Update GetLinkQuery.cs

--- src/Moonglade.FriendLink/GetLinkQuery.cs
+++ src/Moonglade.FriendLink/GetLinkQuery.cs
@@ -8,8 +8,5 @@ public record GetLinkQuery(Guid Id) : IRequest<FriendLinkEntity>;
 
 public class GetLinkQueryHandler(IRepository<FriendLinkEntity> repo) : IRequestHandler<GetLinkQuery, FriendLinkEntity>
 {
-    public async Task<FriendLinkEntity> Handle(GetLinkQuery request, CancellationToken ct)
-    {
-        return await repo.GetAsync(request.Id, ct);
-    }
+    public async Task<FriendLinkEntity> Handle(GetLinkQuery request, CancellationToken ct) => await repo.GetAsync(request.Id, ct);
 }
