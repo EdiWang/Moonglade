621b9f1dc2ece2e6a2217a2592a2dd796aad6d70
Edi Wang <Edi.Wang@outlook.com>
2024-04-25 14:11:40 +0800

    LoginHistorySpec

--- src/Moonglade.Auth/GetLoginHistoryQuery.cs
+++ src/Moonglade.Auth/GetLoginHistoryQuery.cs
@@ -1,16 +1,16 @@
-﻿using Moonglade.Data.Entities;
-using Moonglade.Data.Infrastructure;
+﻿using Moonglade.Data;
+using Moonglade.Data.Entities;
 using Moonglade.Data.Spec;
 
 namespace Moonglade.Auth;
 
 public record GetLoginHistoryQuery : IRequest<List<LoginHistoryEntity>>;
 
-public class GetLoginHistoryQueryHandler(IRepository<LoginHistoryEntity> repo) : IRequestHandler<GetLoginHistoryQuery, List<LoginHistoryEntity>>
+public class GetLoginHistoryQueryHandler(MoongladeRepository<LoginHistoryEntity> repo) : IRequestHandler<GetLoginHistoryQuery, List<LoginHistoryEntity>>
 {
     public async Task<List<LoginHistoryEntity>> Handle(GetLoginHistoryQuery request, CancellationToken ct)
     {
-        var history = await repo.ListAsync(new LoginHistorySpec(10));
+        var history = await repo.ListAsync(new LoginHistorySpec(10), ct);
         return history;
     }
 }
