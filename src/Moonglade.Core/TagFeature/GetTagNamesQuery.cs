cdafd58e96a4f5eb879b5dac19cb907d22c984a9
Edi Wang <Edi.Wang@outlook.com>
2024-04-25 12:38:59 +0800

    migrate a part of tags

--- src/Moonglade.Core/TagFeature/GetTagNamesQuery.cs
+++ src/Moonglade.Core/TagFeature/GetTagNamesQuery.cs
@@ -1,8 +1,10 @@
-﻿namespace Moonglade.Core.TagFeature;
+﻿using Moonglade.Data;
+
+namespace Moonglade.Core.TagFeature;
 
 public record GetTagNamesQuery : IRequest<List<string>>;
 
-public class GetTagNamesQueryHandler(IRepository<TagEntity> repo) : IRequestHandler<GetTagNamesQuery, List<string>>
+public class GetTagNamesQueryHandler(MoongladeRepository<TagEntity> repo) : IRequestHandler<GetTagNamesQuery, List<string>>
 {
     public Task<List<string>> Handle(GetTagNamesQuery request, CancellationToken ct) => repo.SelectAsync(t => t.DisplayName, ct);
 }
