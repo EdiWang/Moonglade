fb434044ebe84e8bf57c1f1aa4b2bb81b6ca10c7
Edi Wang <Edi.Wang@outlook.com>
2024-04-28 08:35:26 +0800

    Update DeleteCategoryCommand.cs

--- src/Moonglade.Core/CategoryFeature/DeleteCategoryCommand.cs
+++ src/Moonglade.Core/CategoryFeature/DeleteCategoryCommand.cs
@@ -5,18 +5,15 @@ namespace Moonglade.Core.CategoryFeature;
 
 public record DeleteCategoryCommand(Guid Id) : IRequest<OperationCode>;
 
-public class DeleteCategoryCommandHandler(
-        MoongladeRepository<CategoryEntity> catRepo,
-        MoongladeRepository<PostCategoryEntity> postCatRepo,
-        ICacheAside cache) : IRequestHandler<DeleteCategoryCommand, OperationCode>
+public class DeleteCategoryCommandHandler(MoongladeRepository<CategoryEntity> catRepo, ICacheAside cache)
+    : IRequestHandler<DeleteCategoryCommand, OperationCode>
 {
     public async Task<OperationCode> Handle(DeleteCategoryCommand request, CancellationToken ct)
     {
         var cat = await catRepo.GetByIdAsync(request.Id, ct);
         if (null == cat) return OperationCode.ObjectNotFound;
 
-        var pcs = await postCatRepo.GetAsync(pc => pc.CategoryId == request.Id);
-        if (pcs is not null) await postCatRepo.DeleteAsync(pcs, ct);
+        cat.PostCategory.Clear();
 
         await catRepo.DeleteAsync(cat, ct);
         cache.Remove(BlogCachePartition.General.ToString(), "allcats");
