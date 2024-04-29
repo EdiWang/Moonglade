312aa43c5ac42c49e2c986796a93afe23d5a1bf2
Edi Wang <Edi.Wang@outlook.com>
2024-04-29 14:11:37 +0800

    Update GetTagCountListQuery.cs

--- src/Moonglade.Core/TagFeature/GetTagCountListQuery.cs
+++ src/Moonglade.Core/TagFeature/GetTagCountListQuery.cs
@@ -2,16 +2,11 @@
 
 namespace Moonglade.Core.TagFeature;
 
-public record GetTagCountListQuery : IRequest<List<KeyValuePair<Tag, int>>>;
+public record GetTagCountListQuery : IRequest<List<KeyValuePair<TagEntity, int>>>;
 
-public class GetTagCountListQueryHandler(MoongladeRepository<TagEntity> repo) : IRequestHandler<GetTagCountListQuery, List<KeyValuePair<Tag, int>>>
+public class GetTagCountListQueryHandler(MoongladeRepository<TagEntity> repo) : IRequestHandler<GetTagCountListQuery, List<KeyValuePair<TagEntity, int>>>
 {
-    public Task<List<KeyValuePair<Tag, int>>> Handle(GetTagCountListQuery request, CancellationToken ct) =>
+    public Task<List<KeyValuePair<TagEntity, int>>> Handle(GetTagCountListQuery request, CancellationToken ct) =>
         repo.SelectAsync(t =>
-            new KeyValuePair<Tag, int>(new()
-            {
-                Id = t.Id,
-                DisplayName = t.DisplayName,
-                NormalizedName = t.NormalizedName
-            }, t.Posts.Count), ct);
+            new KeyValuePair<TagEntity, int>(t, t.Posts.Count), ct);
 }
