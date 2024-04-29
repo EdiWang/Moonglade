3955b7ba8548b7245b9509962be066dc7f955d44
Edi Wang <Edi.Wang@outlook.com>
2024-04-29 14:04:49 +0800

    use TagEntity

--- src/Moonglade.Core/TagFeature/GetHotTagsQuery.cs
+++ src/Moonglade.Core/TagFeature/GetHotTagsQuery.cs
@@ -3,22 +3,17 @@ using Moonglade.Data.Specifications;
 
 namespace Moonglade.Core.TagFeature;
 
-public record GetHotTagsQuery(int Top) : IRequest<List<KeyValuePair<Tag, int>>>;
+public record GetHotTagsQuery(int Top) : IRequest<List<KeyValuePair<TagEntity, int>>>;
 
-public class GetHotTagsQueryHandler(MoongladeRepository<TagEntity> repo) : IRequestHandler<GetHotTagsQuery, List<KeyValuePair<Tag, int>>>
+public class GetHotTagsQueryHandler(MoongladeRepository<TagEntity> repo) : IRequestHandler<GetHotTagsQuery, List<KeyValuePair<TagEntity, int>>>
 {
-    public async Task<List<KeyValuePair<Tag, int>>> Handle(GetHotTagsQuery request, CancellationToken ct)
+    public async Task<List<KeyValuePair<TagEntity, int>>> Handle(GetHotTagsQuery request, CancellationToken ct)
     {
         if (!await repo.AnyAsync(ct)) return [];
 
         var spec = new TagSpec(request.Top);
         var tags = await repo.SelectAsync(spec, t =>
-            new KeyValuePair<Tag, int>(new()
-            {
-                Id = t.Id,
-                DisplayName = t.DisplayName,
-                NormalizedName = t.NormalizedName
-            }, t.Posts.Count), ct);
+            new KeyValuePair<TagEntity, int>(t, t.Posts.Count), ct);
 
         return tags;
     }
