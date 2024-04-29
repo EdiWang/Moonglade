884fd723431455e8707058045203c2c15a4cfa21
Edi Wang <Edi.Wang@outlook.com>
2024-04-29 12:23:48 +0800

    remove css validation

--- src/Moonglade.Web/Controllers/PageController.cs
+++ src/Moonglade.Web/Controllers/PageController.cs
@@ -1,6 +1,5 @@
 ﻿using Moonglade.Core.PageFeature;
 using Moonglade.Web.Attributes;
-using NUglify;
 
 namespace Moonglade.Web.Controllers;
 
@@ -12,31 +11,20 @@ public class PageController(ICacheAside cache, IMediator mediator) : Controller
     [HttpPost]
     [TypeFilter(typeof(ClearBlogCache), Arguments = [BlogCacheType.SiteMap])]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public Task<IActionResult> Create(EditPageRequest model) =>
-        CreateOrEdit(model, async request => await mediator.Send(new CreatePageCommand(request)));
+    public async Task<IActionResult> Create(EditPageRequest model)
+    {
+        var uid = await mediator.Send(new CreatePageCommand(model));
+
+        cache.Remove(BlogCachePartition.Page.ToString(), model.Slug.ToLower());
+        return Ok(new { PageId = uid });
+    }
 
     [HttpPut("{id:guid}")]
     [TypeFilter(typeof(ClearBlogCache), Arguments = [BlogCacheType.SiteMap])]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public Task<IActionResult> Edit([NotEmpty] Guid id, EditPageRequest model) =>
-        CreateOrEdit(model, async request => await mediator.Send(new UpdatePageCommand(id, request)));
-
-    private async Task<IActionResult> CreateOrEdit(EditPageRequest model, Func<EditPageRequest, Task<Guid>> pageServiceAction)
+    public async Task<IActionResult> Edit([NotEmpty] Guid id, EditPageRequest model)
     {
-        if (!string.IsNullOrWhiteSpace(model.CssContent))
-        {
-            var uglifyTest = Uglify.Css(model.CssContent);
-            if (uglifyTest.HasErrors)
-            {
-                foreach (var err in uglifyTest.Errors)
-                {
-                    ModelState.AddModelError(model.CssContent, err.ToString());
-                }
-                return BadRequest(ModelState.CombineErrorMessages());
-            }
-        }
-
-        var uid = await pageServiceAction(model);
+        var uid = await mediator.Send(new UpdatePageCommand(id, model));
 
         cache.Remove(BlogCachePartition.Page.ToString(), model.Slug.ToLower());
         return Ok(new { PageId = uid });
