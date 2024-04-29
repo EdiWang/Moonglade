43e9571ee2db3b2f2da0000533c49f410ef0b11f
Edi Wang <Edi.Wang@outlook.com>
2024-04-29 12:02:58 +0800

    Update DeletePageCommand.cs

--- src/Moonglade.Core/PageFeature/DeletePageCommand.cs
+++ src/Moonglade.Core/PageFeature/DeletePageCommand.cs
@@ -2,17 +2,14 @@
 
 namespace Moonglade.Core.PageFeature;
 
-public record DeletePageCommand(Guid Id) : IRequest;
+public record DeletePageCommand(Guid Id) : IRequest<OperationCode>;
 
-public class DeletePageCommandHandler(MoongladeRepository<PageEntity> repo, IMediator mediator) : IRequestHandler<DeletePageCommand>
+public class DeletePageCommandHandler(MoongladeRepository<PageEntity> repo, IMediator mediator) : IRequestHandler<DeletePageCommand, OperationCode>
 {
-    public async Task Handle(DeletePageCommand request, CancellationToken ct)
+    public async Task<OperationCode> Handle(DeletePageCommand request, CancellationToken ct)
     {
         var page = await repo.GetByIdAsync(request.Id, ct);
-        if (page is null)
-        {
-            throw new InvalidOperationException($"PageEntity with Id '{request.Id}' not found.");
-        }
+        if (page == null) return OperationCode.ObjectNotFound;
 
         if (page.CssId != null)
         {
@@ -20,5 +17,6 @@ public class DeletePageCommandHandler(MoongladeRepository<PageEntity> repo, IMed
         }
 
         await repo.DeleteAsync(page, ct);
+        return OperationCode.Done;
     }
 }
