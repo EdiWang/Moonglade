7da7780fb666e37a6ed7d7e5d12f141d2a6341e8
Edi Wang <Edi.Wang@outlook.com>
2024-05-27 16:33:33 +0800

    rename to GetMentionsQuery

---
+++ src/Moonglade.Pingback/GetMentionsQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Moonglade.Data;
+using Moonglade.Data.Entities;
+using Moonglade.Data.Specifications;
+
+namespace Moonglade.Pingback;
+
+public record GetMentionsQuery : IRequest<List<MentionEntity>>;
+
+public class GetMentionsQueryHandler(MoongladeRepository<MentionEntity> repo) :
+    IRequestHandler<GetMentionsQuery, List<MentionEntity>>
+{
+    public Task<List<MentionEntity>> Handle(GetMentionsQuery request, CancellationToken ct) =>
+        repo.ListAsync(new PingbackReadOnlySpec(), ct);
+}
