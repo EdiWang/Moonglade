6002f62e69258474ef2e01400a58819444e7ed1e
Edi Wang <Edi.Wang@outlook.com>
2024-05-27 16:27:52 +0800

    Create WebmentionController.cs

---
+++ src/Moonglade.Web/WebmentionController.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Moonglade.Web;
+
+[ApiController]
+[Route("[controller]")]
+public class WebmentionController(
+    ILogger<WebmentionController> logger,
+    IBlogConfig blogConfig,
+    IMediator mediator) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> ReceiveWebmention(
+        [FromForm][Required] string source,
+        [FromForm][Required] string target)
+    {
+        // Verify that the source URL links to the target URL
+        // TODO
+
+        // Process the Webmention
+        // TODO
+
+        // For demonstration purposes, we'll just return a success message.
+        return Ok("Webmention received and verified.");
+    }
+}
