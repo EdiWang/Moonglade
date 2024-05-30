cf75e2d27c5064c731b2253959ddc30553200548
Edi Wang <Edi.Wang@outlook.com>
2024-05-30 12:04:38 +0800

    Create ReceiveWebmentionCommand.cs

---
+++ src/Moonglade.Webmention/ReceiveWebmentionCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Moonglade.Webmention;
+
+public record ReceiveWebmentionCommand(string Source, string Target, string RemoteIp) : IRequest<WebmentionResponse>;
+
