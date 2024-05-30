4dfd4cd8e4109ccd7580dd43fd8101055a270b6a
Edi Wang <Edi.Wang@outlook.com>
2024-05-30 15:43:25 +0800

    Create IWebmentionSender.cs

---
+++ src/Moonglade.Webmention/IWebmentionSender.cs
@@ -0,0 +1,14 @@
+﻿namespace Moonglade.Webmention;
+
+public interface IWebmentionSender
+{
+    Task<WebmentionSendResult> SendWebmentionAsync(string sourceUrl, string targetUrl);
+}
+
+public class WebmentionSendResult
+{
+    public bool IsSuccess { get; set; }
+    public int StatusCode { get; set; }
+    public string ResponseContent { get; set; }
+    public string AdditionalInfo { get; set; }
+}
