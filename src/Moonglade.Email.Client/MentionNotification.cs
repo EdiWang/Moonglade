8ec4f27b3d629acc40f015027e937546c4e5dfab
Edi Wang <Edi.Wang@outlook.com>
2024-05-30 12:09:01 +0800

    MentionNotification

--- src/Moonglade.Email.Client/MentionNotification.cs
+++ src/Moonglade.Email.Client/MentionNotification.cs
@@ -3,16 +3,16 @@ using Moonglade.Configuration;
 
 namespace Moonglade.Email.Client;
 
-public record PingbackNotification(
+public record MentionNotification(
     string TargetPostTitle,
     string Domain,
     string SourceIp,
     string SourceUrl,
     string SourceTitle) : INotification;
 
-public class PingbackNotificationHandler(IMoongladeEmailClient moongladeEmailClient, IBlogConfig blogConfig) : INotificationHandler<PingbackNotification>
+public class MentionNotificationHandler(IMoongladeEmailClient moongladeEmailClient, IBlogConfig blogConfig) : INotificationHandler<MentionNotification>
 {
-    public async Task Handle(PingbackNotification notification, CancellationToken ct)
+    public async Task Handle(MentionNotification notification, CancellationToken ct)
     {
         var dl = new[] { blogConfig.GeneralSettings.OwnerEmail };
         await moongladeEmailClient.SendEmail(MailMesageTypes.BeingPinged, dl, notification);
