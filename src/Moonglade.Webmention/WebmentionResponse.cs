4f6996eb744ab7dbf3280d4d4c974a02c1246390
Edi Wang <Edi.Wang@outlook.com>
2024-05-30 12:04:35 +0800

    Create WebmentionResponse.cs

---
+++ src/Moonglade.Webmention/WebmentionResponse.cs
@@ -0,0 +1,14 @@
+﻿using Moonglade.Data.Entities;
+
+namespace Moonglade.Webmention;
+
+public class WebmentionResponse
+{
+    public WebmentionStatus Status { get; set; }
+    public MentionEntity MentionEntity { get; set; }
+}
+
+public enum WebmentionStatus
+{
+    Success
+}
