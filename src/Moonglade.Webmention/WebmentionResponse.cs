554a85fa8f711457d340d96b917e4e3db3386b3a
Edi Wang <Edi.Wang@outlook.com>
2024-05-30 13:03:31 +0800

    Update WebmentionResponse.cs

--- src/Moonglade.Webmention/WebmentionResponse.cs
+++ src/Moonglade.Webmention/WebmentionResponse.cs
@@ -2,13 +2,26 @@
 
 namespace Moonglade.Webmention;
 
-public class WebmentionResponse
+public class WebmentionResponse(WebmentionStatus Status)
 {
     public WebmentionStatus Status { get; set; }
     public MentionEntity MentionEntity { get; set; }
+
+    public static WebmentionResponse GenericError => new(WebmentionStatus.GenericError);
+    public static WebmentionResponse InvalidWebmentionRequest => new(WebmentionStatus.InvalidWebmentionRequest);
+    public static WebmentionResponse ErrorTargetUriNotExist => new(WebmentionStatus.ErrorTargetUriNotExist);
+    public static WebmentionResponse ErrorWebmentionAlreadyRegistered => new(WebmentionStatus.ErrorWebmentionAlreadyRegistered);
+    public static WebmentionResponse ErrorSourceNotContainTargetUri => new(WebmentionStatus.ErrorSourceNotContainTargetUri);
+    public static WebmentionResponse SpamDetectedFakeNotFound => new(WebmentionStatus.SpamDetectedFakeNotFound);
 }
 
 public enum WebmentionStatus
 {
-    Success
+    Success,
+    GenericError,
+    InvalidWebmentionRequest,
+    ErrorTargetUriNotExist,
+    ErrorWebmentionAlreadyRegistered,
+    ErrorSourceNotContainTargetUri,
+    SpamDetectedFakeNotFound
 }
