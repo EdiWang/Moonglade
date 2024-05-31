2644cded80934642326f4baff757088924aa3ec5
Edi Wang <Edi.Wang@outlook.com>
2024-05-31 12:39:04 +0800

    Create WebmentionRequestor.cs

---
+++ src/Moonglade.Webmention/WebmentionRequestor.cs
@@ -0,0 +1,30 @@
+﻿namespace Moonglade.Webmention;
+
+public interface IWebmentionRequestor
+{
+    Task<HttpResponseMessage> Send(Uri sourceUrl, Uri targetUrl, Uri url);
+}
+
+public class WebmentionRequestor : IWebmentionRequestor
+{
+    private readonly HttpClient _httpClient;
+
+    public WebmentionRequestor(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        _httpClient.Timeout = TimeSpan.FromSeconds(30);
+        _httpClient.DefaultRequestHeaders.AcceptLanguage.ParseAdd("en-us");
+    }
+
+    public Task<HttpResponseMessage> Send(Uri sourceUrl, Uri targetUrl, Uri url)
+    {
+        var values = new Dictionary<string, string>
+        {
+            { "source", sourceUrl.ToString() },
+            { "target", targetUrl.ToString() }
+        };
+
+        var content = new FormUrlEncodedContent(values);
+        return _httpClient.PostAsync(url, content);
+    }
+}
