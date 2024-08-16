a969ebeb58ed1c81b2ad546fe1181ddcbac6e0c2
Edi Wang <Edi.Wang@outlook.com>
2024-08-16 21:09:44 +0800

    Update mssql-migration.sql

--- Deployment/mssql-migration.sql
+++ Deployment/mssql-migration.sql
@@ -80,13 +80,3 @@ BEGIN
     ALTER TABLE Mention ADD Worker NVARCHAR(16)
     UPDATE Mention SET Worker = N'Pingback'
 END
-
-
--- v14.8
-IF NOT EXISTS (SELECT * FROM sys.columns 
-               WHERE Name = N'RouteLink' AND Object_ID = Object_ID(N'Post'))
-BEGIN
-    ALTER TABLE Post ADD RouteLink NVARCHAR(256)
-    UPDATE Post SET RouteLink = FORMAT(PubDateUtc, 'yyyy/M/d') + '/' + Slug
-END
-
