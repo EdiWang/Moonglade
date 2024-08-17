f144ececb66067fa88bbf715a65487d985d20cd4
Edi Wang <Edi.Wang@outlook.com>
2024-08-17 08:34:14 +0800

    Update mssql-migration.vnext.sql

--- Deployment/mssql-migration.vnext.sql
+++ Deployment/mssql-migration.vnext.sql
@@ -6,3 +6,12 @@ BEGIN
     UPDATE Post SET RouteLink = FORMAT(PubDateUtc, 'yyyy/M/d') + '/' + Slug
 END
 
+IF EXISTS (
+    SELECT 1
+    FROM sys.columns c
+    JOIN sys.objects o ON c.object_id = o.object_id
+    WHERE o.name = 'Post' AND c.name = 'HashCheckSum'
+)
+BEGIN
+    ALTER TABLE Post DROP COLUMN HashCheckSum;
+END;
