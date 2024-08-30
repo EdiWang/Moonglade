7be9a3039faeadc8270f57c43aa1ef19a43882fc
Edi Wang <Edi.Wang@outlook.com>
2024-08-30 12:30:06 +0800

    Update linux-onekey-deploy.sh script
    Refactor linux-onekey-deploy.sh script to include necessary environment variable exports and improve readability by using color variables for console output.

--- Deployment/linux-onekey-deploy.sh
+++ Deployment/linux-onekey-deploy.sh
@@ -1,2 +1,19 @@
 #!/usr/bin/env bash
 
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+stty erase ^?
+
+cd "$(
+    cd "$(dirname "$0")" || exit
+    pwd
+)" || exit
+
+Green="\033[32m"
+Red="\033[31m"
+Yellow="\033[33m"
+Blue="\033[36m"
+Font="\033[0m"
+GreenBG="\033[42;37m"
+RedBG="\033[41;37m"
+OK="${Green}[OK]${Font}"
+ERROR="${Red}[ERROR]${Font}"
