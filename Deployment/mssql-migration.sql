2f623b76a87b021e826b790caacf551543b04245
Edi Wang <Edi.Wang@outlook.com>
2024-08-31 14:07:14 +0800

    move sql

--- Deployment/mssql-migration.sql
+++ Deployment/mssql-migration.sql
@@ -80,3 +80,20 @@ BEGIN
     EXEC sp_executesql N'UPDATE Mention SET Worker = N''Pingback'''
 END
 
+-- v14.8
+IF NOT EXISTS (SELECT * FROM sys.columns 
+               WHERE Name = N'RouteLink' AND Object_ID = Object_ID(N'Post'))
+BEGIN
+    EXEC sp_executesql N'ALTER TABLE Post ADD RouteLink NVARCHAR(256)'
+    EXEC sp_executesql N'UPDATE Post SET RouteLink = FORMAT(PubDateUtc, ''yyyy/M/d'') + ''/'' + Slug'
+END
+
+IF EXISTS (
+    SELECT 1
+    FROM sys.columns c
+    JOIN sys.objects o ON c.object_id = o.object_id
+    WHERE o.name = 'Post' AND c.name = 'HashCheckSum'
+)
+BEGIN
+    ALTER TABLE Post DROP COLUMN HashCheckSum;
+END;
