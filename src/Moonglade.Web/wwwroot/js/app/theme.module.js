6f5ef6372bfe6ec42b11c65dccb60dff7d916aef
Edi Wang <Edi.Wang@outlook.com>
2024-09-03 12:40:53 +0800

    Refactor theme switch to dropdown menu
    Updated _LightSwitch.cshtml to replace the theme toggle button with a dropdown menu for 'Auto', 'Light', and 'Dark' themes. Removed .lighticon-dark class from base.css and added .lightswitch class for background color. In darkmode.css, replaced .lighticon-light and .lighticon-dark rules with a .lightswitch rule for dark theme. Updated theme.module.js to remove theme from localStorage when set to 'auto' and removed the toggleTheme function.

--- src/Moonglade.Web/wwwroot/js/app/theme.module.js
+++ src/Moonglade.Web/wwwroot/js/app/theme.module.js
@@ -4,26 +4,18 @@ const setStoredTheme = theme => localStorage.setItem('theme', theme)
 export const getPreferredTheme = () => {
     const storedTheme = getStoredTheme()
     if (storedTheme) {
-        return storedTheme
+        return storedTheme;
     }
 
-    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 }
 
 export const setTheme = theme => {
     if (theme === 'auto') {
-        document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
+        document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'));
+        localStorage.removeItem('theme');
     } else {
-        document.documentElement.setAttribute('data-bs-theme', theme)
-    }
-}
-
-export function toggleTheme() {
-    if (getPreferredTheme() == 'dark') {
-        setTheme('light');
-        setStoredTheme('light');
-    } else {
-        setTheme('dark');
-        setStoredTheme('dark');
+        document.documentElement.setAttribute('data-bs-theme', theme);
+        setStoredTheme(theme);
     }
 }
