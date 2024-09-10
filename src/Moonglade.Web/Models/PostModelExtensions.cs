4437781de748dd1fe18a29fc328215c511c6af03
Sascha Manns <S.Manns@infas.de>
2024-09-10 20:15:48 +0200

    refactor: Add Estimated time to read.

---
+++ src/Moonglade.Web/Models/PostModelExtensions.cs
@@ -0,0 +1,29 @@
+using Moonglade.Data.Entities;
+
+namespace Moonglade.Web.Models;
+
+public static class PostModelExtensions
+{
+	/// <summary>
+	/// Gets the time to read.
+	/// </summary>
+	/// <param name="story">The story.</param>
+	/// <returns>TimeToRead object with calculated reading time.</returns>
+	public static TimeToRead GetTimeToRead(this PostEntity story)
+	{
+		string wordCount = story.PostContent; // Get all content of the story
+		int counts = wordCount.Count(ch => ch == ' ') + 1;
+		int minutes = counts / 200; // Calculate Minutes
+		int seconds = counts % 200 / (200 / 60); // Calculate Seconds
+		string strMinutes = (minutes == 1) ? "Minute" : "Minutes";
+		string strSeconds = (seconds == 1) ? "Second" : "Seconds";
+		TimeToRead timeToRead = new()
+		{
+			Minutes = minutes,
+			Seconds = seconds,
+			StrMinutes = strMinutes,
+			StrSeconds = strSeconds,
+		};
+		return timeToRead;
+	}
+}
