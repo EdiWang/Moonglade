071920e33f38562edd196a10b78cca42886c9d64
Edi Wang <Edi.Wang@outlook.com>
2024-09-16 10:37:41 +0800

    Add IndexNowMapHandler to handle API key retrieval
    Introduced the Moonglade.Web.Handlers namespace and added the IndexNowMapHandler class. This class includes a static property `Handler` for asynchronous HTTP request handling. The `Handle` method processes requests by checking for an API key in the configuration. If the API key is missing, it responds with a 404 status and a message. If the API key is present, it responds with the key in plain text.

---
+++ src/Moonglade.Web/Handlers/IndexNowMapHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace Moonglade.Web.Handlers;
+
+public class IndexNowMapHandler
+{
+    public static Delegate Handler => async (HttpContext httpContext, IConfiguration configuration) =>
+    {
+        await Handle(httpContext, configuration);
+    };
+
+    public static async Task Handle(HttpContext httpContext, IConfiguration configuration)
+    {
+        var apiKey = configuration["IndexNow:ApiKey"];
+        if (string.IsNullOrWhiteSpace(apiKey))
+        {
+            httpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+            await httpContext.Response.WriteAsync("No indexnowkey.txt is present.", httpContext.RequestAborted);
+        }
+        else
+        {
+            httpContext.Response.ContentType = "text/plain";
+            await httpContext.Response.WriteAsync(apiKey, Encoding.UTF8, httpContext.RequestAborted);
+        }
+    }
+}
