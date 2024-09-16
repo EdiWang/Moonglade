11b5d0da5fd4afc64dc333344ea23c23b516e130
Edi Wang <Edi.Wang@outlook.com>
2024-09-16 09:49:03 +0800

    Add IndexNowClient class with unimplemented SendRequestAsync
    Introduced the IndexNowClient class in the Moonglade.IndexNow.Client namespace, implementing the IIndexNowClient interface. Added the SendRequestAsync method, which currently throws a NotImplementedException. Included necessary using directives for various System namespaces.

---
+++ src/Moonglade.IndexNow.Client/IndexNowClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moonglade.IndexNow.Client;
+
+public class IndexNowClient : IIndexNowClient
+{
+    public Task SendRequestAsync(Uri url)
+    {
+        throw new NotImplementedException();
+    }
+}
