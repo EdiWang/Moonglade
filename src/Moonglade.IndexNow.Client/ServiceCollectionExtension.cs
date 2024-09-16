e59d9d2968384108e4464aedee2fe53d8adab67c
Edi Wang <Edi.Wang@outlook.com>
2024-09-16 09:51:07 +0800

    Add IndexNow client extension method for IServiceCollection
    Introduced a new namespace `Moonglade.IndexNow.Client` and a static
    class `ServiceCollectionExtension`. Added an extension method
    `AddIndexNowClient` to the `IServiceCollection` interface, which
    registers an `IIndexNowClient` implementation (`IndexNowClient`)
    with an HTTP client and a resilience handler. This enhances the
    integration of the IndexNow client into the application's
    dependency injection system.

---
+++ src/Moonglade.IndexNow.Client/ServiceCollectionExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Moonglade.IndexNow.Client;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddIndexNowClient(this IServiceCollection services)
+    {
+        services.AddHttpClient<IIndexNowClient, IndexNowClient>()
+                .AddStandardResilienceHandler();
+
+        return services;
+    }
+}
