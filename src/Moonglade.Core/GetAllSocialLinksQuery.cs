358ed45665a0d6f309342fc88ff2cbd7222c2a55
Edi Wang <Edi.Wang@outlook.com>
2024-10-21 10:18:33 +0800

    Create GetAllSocialLinksQuery.cs

---
+++ src/Moonglade.Core/GetAllSocialLinksQuery.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Moonglade.Configuration;
+
+namespace Moonglade.Core;
+
+public record GetAllSocialLinksQuery : IRequest<List<SocialLink>>;
+
+public class GetAllSocialLinksQueryHandler(IConfiguration configuration) : IRequestHandler<GetAllSocialLinksQuery, List<SocialLink>>
+{
+    public Task<List<SocialLink>> Handle(GetAllSocialLinksQuery request, CancellationToken cancellationToken)
+    {
+        var section = configuration.GetSection("Experimental:SocialLinks");
+
+        if (!section.Exists())
+        {
+            return Task.FromResult(new List<SocialLink>());
+        }
+
+        var links = section.Get<List<SocialLink>>();
+        return Task.FromResult(links ?? new List<SocialLink>());
+    }
+}
