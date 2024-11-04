7bdb24e2df66156b153697b937f0697f8db332cb
Edi Wang <Edi.Wang@outlook.com>
2024-11-04 14:02:42 +0800

    Refactor post creation logic for clarity
    Refactored code to improve readability and maintainability:
    - Extracted logic for adding categories and tags into `AddCategories` and `AddTags` methods.
    - Main method now calls `AddCategories` and `AddTags`.
    - Moved `postRepo.AddAsync(post, ct)` and logging to main method.
    - Removed redundant comments.

--- src/Moonglade.Core/PostFeature/CreatePostCommand.cs
+++ src/Moonglade.Core/PostFeature/CreatePostCommand.cs
@@ -66,10 +66,20 @@ public class CreatePostCommandHandler(
             logger.LogInformation($"Found conflict for post slug, generated new slug: {post.Slug}");
         }
 
-        // add categories
-        if (request.Payload.SelectedCatIds is { Length: > 0 })
+        AddCategories(request.Payload.SelectedCatIds, post);
+        await AddTags(request.Payload.Tags, post, ct);
+
+        await postRepo.AddAsync(post, ct);
+
+        logger.LogInformation($"Created post Id: {post.Id}, Title: '{post.Title}'");
+        return post;
+    }
+
+    private static void AddCategories(Guid[] selectedCatIds, PostEntity post)
+    {
+        if (selectedCatIds is { Length: > 0 })
         {
-            foreach (var id in request.Payload.SelectedCatIds)
+            foreach (var id in selectedCatIds)
             {
                 post.PostCategory.Add(new()
                 {
@@ -78,11 +88,13 @@ public class CreatePostCommandHandler(
                 });
             }
         }
+    }
 
-        // add tags
-        var tags = string.IsNullOrWhiteSpace(request.Payload.Tags) ?
+    private async Task AddTags(string tagString, PostEntity post, CancellationToken ct)
+    {
+        var tags = string.IsNullOrWhiteSpace(tagString) ?
             [] :
-            request.Payload.Tags.Split(',').ToArray();
+            tagString.Split(',').ToArray();
 
         if (tags is { Length: > 0 })
         {
@@ -94,11 +106,6 @@ public class CreatePostCommandHandler(
                 post.Tags.Add(tag);
             }
         }
-
-        await postRepo.AddAsync(post, ct);
-
-        logger.LogInformation($"Created post Id: {post.Id}, Title: '{post.Title}'");
-        return post;
     }
 
     private async Task<TagEntity> CreateTag(string item)
