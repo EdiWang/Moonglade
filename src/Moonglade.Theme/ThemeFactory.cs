2d7e51e56595e245a402631e16e840746b680b78
Edi Wang <Edi.Wang@outlook.com>
2024-11-06 12:38:06 +0800

    Update theme IDs and rename default theme
    The `GetSystemThemes` method in the `ThemeFactory` class has been updated to modify the IDs of the themes from a sequence starting at 1 to a sequence starting at 100. Additionally, the name of the first theme has been updated to "Word Blue (Default)".

--- src/Moonglade.Theme/ThemeFactory.cs
+++ src/Moonglade.Theme/ThemeFactory.cs
@@ -8,12 +8,12 @@ public static class ThemeFactory
     {
         return new List<BlogThemeEntity>
         {
-            CreateTheme(1, "Word Blue", "#2a579a", "#1a365f", "#3e6db5"),
-            CreateTheme(2, "Excel Green", "#165331", "#0E351F", "#0E703A"),
-            CreateTheme(3, "PowerPoint Orange", "#983B22", "#622616", "#C43E1C"),
-            CreateTheme(4, "OneNote Purple", "#663276", "#52285E", "#7719AA"),
-            CreateTheme(5, "Outlook Blue", "#035AA6", "#032B51", "#006CBF"),
-            CreateTheme(6, "Metal Blue", "#4E5967", "#333942", "#6e7c8e")
+            CreateTheme(100, "Word Blue (Default)", "#2a579a", "#1a365f", "#3e6db5"),
+            CreateTheme(101, "Excel Green", "#165331", "#0E351F", "#0E703A"),
+            CreateTheme(102, "PowerPoint Orange", "#983B22", "#622616", "#C43E1C"),
+            CreateTheme(103, "OneNote Purple", "#663276", "#52285E", "#7719AA"),
+            CreateTheme(104, "Outlook Blue", "#035AA6", "#032B51", "#006CBF"),
+            CreateTheme(105, "Metal Blue", "#4E5967", "#333942", "#6e7c8e")
         };
     }
 
