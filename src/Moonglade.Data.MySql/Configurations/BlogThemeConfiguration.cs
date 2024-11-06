6107a1ac75eae15f4a1eb6a39611126173ce1810
Edi Wang <Edi.Wang@outlook.com>
2024-11-06 12:37:52 +0800

    Configure Id generation for BlogThemeEntity on add
    Added configuration in BlogThemeConfiguration to ensure the Id
    property of BlogThemeEntity is automatically generated when a
    new entity is added to the database using ValueGeneratedOnAdd.

--- src/Moonglade.Data.MySql/Configurations/BlogThemeConfiguration.cs
+++ src/Moonglade.Data.MySql/Configurations/BlogThemeConfiguration.cs
@@ -10,6 +10,7 @@ public class BlogThemeConfiguration : IEntityTypeConfiguration<BlogThemeEntity>
     public void Configure(EntityTypeBuilder<BlogThemeEntity> builder)
     {
         builder.HasKey(e => e.Id);
+        builder.Property(e => e.Id).ValueGeneratedOnAdd();
         builder.Property(e => e.ThemeName).HasMaxLength(32);
     }
 }
