ad5dc1768dce4db63a487434553017353d761d4c
Edi Wang <Edi.Wang@outlook.com>
2024-11-06 12:23:08 +0800

    Add ID parameter to theme creation methods
    Updated GetSystemThemes and CreateTheme methods to include an ID parameter for each theme. Modified CreateTheme method signature to accept an int id and updated theme creation calls to pass unique IDs. The CreateTheme method now initializes the Id property of BlogThemeEntity with the provided id.

--- src/Moonglade.Theme/ThemeFactory.cs
+++ src/Moonglade.Theme/ThemeFactory.cs
@@ -8,19 +8,20 @@ public static class ThemeFactory
     {
         return new List<BlogThemeEntity>
         {
-            CreateTheme("Word Blue", "#2a579a", "#1a365f", "#3e6db5"),
-            CreateTheme("Excel Green", "#165331", "#0E351F", "#0E703A"),
-            CreateTheme("PowerPoint Orange", "#983B22", "#622616", "#C43E1C"),
-            CreateTheme("OneNote Purple", "#663276", "#52285E", "#7719AA"),
-            CreateTheme("Outlook Blue", "#035AA6", "#032B51", "#006CBF"),
-            CreateTheme("Metal Blue", "#4E5967", "#333942", "#6e7c8e")
+            CreateTheme(1, "Word Blue", "#2a579a", "#1a365f", "#3e6db5"),
+            CreateTheme(2, "Excel Green", "#165331", "#0E351F", "#0E703A"),
+            CreateTheme(3, "PowerPoint Orange", "#983B22", "#622616", "#C43E1C"),
+            CreateTheme(4, "OneNote Purple", "#663276", "#52285E", "#7719AA"),
+            CreateTheme(5, "Outlook Blue", "#035AA6", "#032B51", "#006CBF"),
+            CreateTheme(6, "Metal Blue", "#4E5967", "#333942", "#6e7c8e")
         };
     }
 
-    private static BlogThemeEntity CreateTheme(string themeName, string color1, string color2, string color3)
+    private static BlogThemeEntity CreateTheme(int id, string themeName, string color1, string color2, string color3)
     {
         return new()
         {
+            Id = id,
             ThemeName = themeName,
             CssRules = $"{{\"--accent-color1\": \"{color1}\",\"--accent-color2\": \"{color2}\",\"--accent-color3\": \"{color3}\"}}",
             ThemeType = 0
