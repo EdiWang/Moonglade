49f50248aba5231cf06432801f235167fa49cbbe
Edi Wang <Edi.Wang@outlook.com>
2024-11-12 10:45:45 +0800

    Create DatabaseTypeHelper.cs

---
+++ src/Moonglade.Utils/DatabaseTypeHelper.cs
@@ -0,0 +1,33 @@
+﻿namespace Moonglade.Utils;
+
+public class DatabaseTypeHelper
+{
+    public static DatabaseType DetermineDatabaseType(string connectionString)
+    {
+        if (connectionString.Contains("Server=") && connectionString.Contains("Database=") && !connectionString.Contains("Uid=") ||
+            connectionString.Contains("Initial Catalog") || connectionString.Contains("Data Source"))
+        {
+            return DatabaseType.SQLServer;
+        }
+
+        if (connectionString.Contains("Host=") && connectionString.Contains("Port="))
+        {
+            return DatabaseType.PostgreSQL;
+        }
+
+        if (connectionString.Contains("Server=") && connectionString.Contains("Uid="))
+        {
+            return DatabaseType.MySQL;
+        }
+
+        return DatabaseType.Unknown;
+    }
+}
+
+public enum DatabaseType
+{
+    Unknown = 0,
+    SQLServer,
+    PostgreSQL,
+    MySQL
+}
