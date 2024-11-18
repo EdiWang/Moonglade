f59dadd8e513925e32d44600764354525e51ce91
Edi Wang <Edi.Wang@outlook.com>
2024-11-18 12:14:18 +0800

    Refactor toMagicJson and add formatUtcTime function
    Refactored the toMagicJson function for better readability and efficiency. Updated the for...in loop to use const for the loop variable. Simplified the processing of each item in the value object by introducing key and val variables and streamlining conditional checks. Added a new function, formatUtcTime, to format UTC time elements in the document.

--- src/Moonglade.Web/wwwroot/js/app/utils.module.js
+++ src/Moonglade.Web/wwwroot/js/app/utils.module.js
@@ -1,28 +1,31 @@
 ﻿export function toMagicJson(value) {
     const newValue = {};
-    for (let item in value) {
+
+    for (const item in value) {
         if (Object.prototype.hasOwnProperty.call(value, item)) {
-            if (!value[item]) {
-                newValue[item.replace('ViewModel.', '')] = null;
-                newValue[item.replace('settings.', '')] = null;
-            }
-            else if (value[item] && !Array.isArray(value[item]) && value[item].toLowerCase() === 'true') {
-                newValue[item.replace('ViewModel.', '')] = true;
-                newValue[item.replace('settings.', '')] = true;
-            }
-            else if (value[item] && !Array.isArray(value[item]) && value[item].toLowerCase() === 'false') {
-                newValue[item.replace('ViewModel.', '')] = false;
-                newValue[item.replace('settings.', '')] = false;
-            }
-            else {
-                newValue[item.replace('ViewModel.', '')] = value[item];
-                newValue[item.replace('settings.', '')] = value[item];
+            const key = item.replace(/^ViewModel\.|^settings\./, '');
+            const val = value[item];
+
+            if (!val) {
+                newValue[key] = null;
+            } else if (typeof val === 'string') {
+                const lowerVal = val.toLowerCase();
+                if (lowerVal === 'true') {
+                    newValue[key] = true;
+                } else if (lowerVal === 'false') {
+                    newValue[key] = false;
+                } else {
+                    newValue[key] = val;
+                }
+            } else {
+                newValue[key] = val;
             }
         }
     }
 
     return newValue;
 }
+
 
 export function formatUtcTime(includeTime = true) {
     const timeElements = document.querySelectorAll('time');
