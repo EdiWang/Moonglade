a73634d0fba560bd23bf8b0ceb2ad495b6afe6cc
Edi Wang <Edi.Wang@outlook.com>
2024-12-12 08:49:18 +0800

    Set BlogThemeEntity Id to start identity at 1000
    Updated BlogThemeConfiguration to modify the UseIdentityColumn
    method for the Id property of BlogThemeEntity. New records will
    now have Id values starting at 1000 instead of the default.

--- src/Moonglade.Data.SqlServer/Configurations/BlogThemeConfiguration.cs
+++ src/Moonglade.Data.SqlServer/Configurations/BlogThemeConfiguration.cs
@@ -9,7 +9,7 @@ public class BlogThemeConfiguration : IEntityTypeConfiguration<BlogThemeEntity>
 {
     public void Configure(EntityTypeBuilder<BlogThemeEntity> builder)
     {
-        builder.Property(e => e.Id).UseIdentityColumn();
+        builder.Property(e => e.Id).UseIdentityColumn(1000);
         builder.Property(e => e.ThemeName).HasMaxLength(32);
     }
 }
