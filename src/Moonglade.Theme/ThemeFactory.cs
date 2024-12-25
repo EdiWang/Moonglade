3a78ed0bdf64581b6e07b6b83f9f33b188bba072
Edi Wang <Edi.Wang@outlook.com>
2024-12-25 13:07:51 +0800

    Update secondary color codes in GetSystemThemes
    Modified the color codes for the themes in the GetSystemThemes method within ThemeFactory.cs. Specifically, the secondary color codes for each theme have been updated to new values.

--- src/Moonglade.Theme/ThemeFactory.cs
+++ src/Moonglade.Theme/ThemeFactory.cs
@@ -8,14 +8,14 @@ public static class ThemeFactory
     {
         return new List<BlogThemeEntity>
         {
-            CreateTheme(100, "Word Blue (Default)", "#2a579a", "#3e6db5"),
-            CreateTheme(101, "Excel Green", "#165331",  "#0E703A"),
-            CreateTheme(102, "PowerPoint Orange", "#983B22",  "#C43E1C"),
-            CreateTheme(103, "OneNote Purple", "#663276",  "#7719AA"),
-            CreateTheme(104, "Outlook Blue", "#035AA6",  "#006CBF"),
-            CreateTheme(105, "Metal Blue", "#4E5967",  "#6e7c8e"),
-            CreateTheme(106, "Mars Green", "#008C8C", "#17b5b5"),
-            CreateTheme(107, "Prussian Blue", "#003153",  "#0061a5")
+            CreateTheme(100, "Word Blue (Default)", "#2A579A", "#5478AE"),
+            CreateTheme(101, "Excel Green", "#165331",  "#44755A"),
+            CreateTheme(102, "PowerPoint Orange", "#983B22",  "#AC624E"),
+            CreateTheme(103, "OneNote Purple", "#663276",  "#845B91"),
+            CreateTheme(104, "Outlook Blue", "#035AA6",  "#357BB7"),
+            CreateTheme(105, "Metal Blue", "#4E5967",  "#717A85"),
+            CreateTheme(106, "Mars Green", "#008C8C", "#33A3A3"),
+            CreateTheme(107, "Prussian Blue", "#003153",  "#335A75")
         };
     }
 
