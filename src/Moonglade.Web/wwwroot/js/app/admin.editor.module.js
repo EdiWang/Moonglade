53a69f5b71c2282aebc32ff97126d0babe984673
Edi Wang <Edi.Wang@outlook.com>
2025-01-07 13:31:45 +0800

    Refactor theme handling in admin.editor.module.js
    - Added import for `theme` in `slugify` function.
    - Updated `loadTinyMCE` to use `theme.getPreferredTheme()` instead of `window.getPreferredTheme()`.

--- src/Moonglade.Web/wwwroot/js/app/admin.editor.module.js
+++ src/Moonglade.Web/wwwroot/js/app/admin.editor.module.js
@@ -1,4 +1,6 @@
-﻿function slugify(text) {
+﻿import * as theme from './theme.module.mjs';
+
+function slugify(text) {
     if (!/^[A-Za-z][A-Za-z0-9 \(\)#,\.\?]*$/.test(text)) {
         return '';
     }
@@ -119,7 +121,7 @@ export function loadTinyMCE(textareaSelector) {
         return;
     }
 
-    const preferredTheme = window.getPreferredTheme();
+    const preferredTheme = theme.getPreferredTheme();
     const isDarkTheme = preferredTheme === 'dark';
 
     window.tinyMCE.init({
