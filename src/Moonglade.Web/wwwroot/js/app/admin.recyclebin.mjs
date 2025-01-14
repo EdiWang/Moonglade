0c6d0b93aa3e2f649e7efe8c104ebcdaae4ee536
Edi Wang <Edi.Wang@outlook.com>
2025-01-14 20:43:12 +0800

    Refactor: Move inline JS to external module
    Moved JavaScript from `RecycleBin.cshtml` to `admin.recyclebin.mjs`.
    Updated script import in `RecycleBin.cshtml` to use the new module with `type="module"` and `asp-append-version="true"`.
    The new module retains all previous functionality, including:
    - Importing `formatUtcTime` and `success` utilities.
    - Adding event listeners for delete, restore, and empty actions.
    - Defining `deletePost` and `restorePost` functions for API calls.
    - Formatting UTC time using `formatUtcTime`.

---
+++ src/Moonglade.Web/wwwroot/js/app/admin.recyclebin.mjs
@@ -0,0 +1,46 @@
+import { formatUtcTime } from '/js/app/utils.module.mjs'
+import { success } from '/js/app/blogtoast.module.mjs'
+
+document.querySelectorAll('.btn-delete').forEach(function (button) {
+    button.addEventListener('click', function () {
+        var cfm = confirm('Delete Confirmation?');
+        if (cfm) {
+            deletePost(this.getAttribute('data-postid'));
+        }
+    });
+});
+
+document.querySelectorAll('.btn-restore').forEach(function (button) {
+    button.addEventListener('click', function () {
+        restorePost(this.getAttribute('data-postid'));
+    });
+});
+
+document.querySelectorAll('.btn-empty-recbin').forEach(function (button) {
+    button.addEventListener('click', function () {
+        callApi('/api/post/recyclebin', 'DELETE', {}, function (resp) {
+            success('Cleared');
+            setTimeout(function () {
+                window.location.reload();
+            }, 800);
+        });
+    });
+});
+
+function deletePost(postid) {
+    callApi(`/api/post/${postid}/destroy`, 'DELETE', {},
+        (resp) => {
+            document.querySelector(`#post-${postid}`).remove();
+            success('Post deleted');
+        });
+}
+
+function restorePost(postid) {
+    callApi(`/api/post/${postid}/restore`, 'POST', {},
+        (resp) => {
+            document.querySelector(`#post-${postid}`).remove();
+            success('Post restored');
+        });
+}
+
+formatUtcTime();
