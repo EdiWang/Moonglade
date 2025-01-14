fce5e737c67fcd9748baa80d51c3fbf71338f920
Edi Wang <Edi.Wang@outlook.com>
2025-01-14 14:51:40 +0800

    Refactor post deletion script to external module
    Moved post deletion logic from inline script in `Post.cshtml` to `admin.post.mjs`. Removed inline script and added a new script tag for the external module with versioning. Replaced `onclick` attribute with `data-postId` for delete button. `admin.post.mjs` now imports `formatUtcTime`, defines `deleteConfirm` and `deletePost` functions, and adds event listeners for delete buttons.

---
+++ src/Moonglade.Web/wwwroot/js/app/admin.post.mjs
@@ -0,0 +1,33 @@
+import { formatUtcTime } from '/js/app/utils.module.mjs'
+
+function deleteConfirm(postid) {
+    var cfm = confirm("Delete Confirmation?");
+    if (cfm) {
+        deletePost(postid);
+    }
+}
+
+function deletePost(postid) {
+    callApi(`/api/post/${postid}/recycle`,
+        'DELETE',
+        {},
+        (resp) => {
+            const postElement = document.getElementById(`post-${postid}`);
+            if (postElement) {
+                postElement.remove();
+            }
+        });
+}
+
+formatUtcTime();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const exportButtons = document.querySelectorAll('.btn-delete');
+
+    exportButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const postId = button.getAttribute('data-postId');
+            deleteConfirm(postId);
+        });
+    });
+});
