abb139b15071a255c6c01ba54a473b375aa64dbf
Edi Wang <Edi.Wang@outlook.com>
2025-01-14 20:28:37 +0800

    Add JSON editor and form handling for settings
    Introduced a JSON content editor using the `vs/editor/editor.main` module. Initialized the `jsonContentEditor` variable and added functions to clear the editor content and handle form submission. Added event listeners for clearing menus and submitting the settings form.

---
+++ src/Moonglade.Web/wwwroot/js/app/admin.menu.mjs
@@ -0,0 +1,22 @@
+import { handleSettingsSubmit } from '/js/app/admin.settings.module.js';
+
+const jsonContentEditor = null;
+
+require(['vs/editor/editor.main'], function () {
+    jsonContentEditor = initEditor('JsonContentEditor', "#settings_MenuJson", 'json');
+});
+
+function clearMenus() {
+    jsonContentEditor.setValue("[]");
+    document.querySelector("#settings_MenuJson").value = "[]";
+}
+
+function handleSubmit(event) {
+    assignEditorValues(jsonContentEditor, "#settings_MenuJson");
+    handleSettingsSubmit(event);
+}
+
+document.getElementById('btn-clear-menus').addEventListener('click', clearMenus);
+
+const form = document.querySelector('#form-settings');
+form.addEventListener('submit', handleSubmit);
