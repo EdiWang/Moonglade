108c74bb3aae2e7ba94754c441838f83e469b775
Edi Wang <Edi.Wang@outlook.com>
2025-01-14 13:37:48 +0800

    Refactor theme management and centralize logic
    Removed `theme.module.mjs` inclusion from `_Layout.cshtml`, `_LayoutAdmin.cshtml`, and `SignIn.cshtml`. Added `base.js` inclusion in `SignIn.cshtml`. Updated `base.js` to import `getPreferredTheme` and `setTheme` from `theme.module.mjs` and set the theme on load. Modified `theme.module.mjs` to export `setTheme` and removed immediate invocation and `DOMContentLoaded` event listener. Centralized theme management logic for consistency and modularity.

--- src/Moonglade.Web/wwwroot/js/app/base.js
+++ src/Moonglade.Web/wwwroot/js/app/base.js
@@ -1,7 +1,11 @@
 ﻿import * as utils from '/js/app/utils.module.mjs'
 import * as blogToast from '/js/app/blogtoast.module.mjs'
+import { getPreferredTheme, setTheme } from '/js/app/theme.module.mjs'
 
 window.emptyGuid = '00000000-0000-0000-0000-000000000000';
 window.formatUtcTime = utils.formatUtcTime;
 
 window.blogToast = blogToast;
+window.getPreferredTheme = getPreferredTheme;
+
+setTheme(getPreferredTheme());
