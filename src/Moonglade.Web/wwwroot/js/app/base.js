891bf9a085b0218c08231ab5d5a7cee5670e67c6
Edi Wang <Edi.Wang@outlook.com>
2025-01-14 14:26:47 +0800

    Refactor time formatting and script handling
    Imported `formatUtcTime` into various .cshtml files:
    - Added to Comments.cshtml with delete button script.
    - Added to Draft.cshtml with deletePost function.
    - Added to Post.cshtml in main and additional scripts.
    - Added to RecycleBin.cshtml with delete confirmation script.
    
    Removed `formatUtcTime` import from base.js and its assignment to `window`.

--- src/Moonglade.Web/wwwroot/js/app/base.js
+++ src/Moonglade.Web/wwwroot/js/app/base.js
@@ -1,9 +1,7 @@
-﻿import * as utils from '/js/app/utils.module.mjs'
-import * as blogToast from '/js/app/blogtoast.module.mjs'
+﻿import * as blogToast from '/js/app/blogtoast.module.mjs'
 import { getPreferredTheme, setTheme } from '/js/app/theme.module.mjs'
 
 window.emptyGuid = '00000000-0000-0000-0000-000000000000';
-window.formatUtcTime = utils.formatUtcTime;
 
 window.blogToast = blogToast;
 window.getPreferredTheme = getPreferredTheme;
