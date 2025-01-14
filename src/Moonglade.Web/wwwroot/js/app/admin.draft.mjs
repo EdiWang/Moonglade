a2e10263e8ad19ba76b59773fa091a5c0aad895e
Edi Wang <Edi.Wang@outlook.com>
2025-01-14 14:58:30 +0800

    Refactor post deletion script to external module
    - Moved JavaScript for post deletion and UTC formatting from inline script in `Draft.cshtml` to `admin.draft.mjs`.
    - Replaced inline script in `Draft.cshtml` with a script tag referencing `admin.draft.mjs`.
    - `admin.draft.mjs` imports `formatUtcTime` and `success` functions, defines `deletePost` function, and sets up event listeners for delete buttons.
    - Added success message to `deletePost` function.

---
+++ src/Moonglade.Web/wwwroot/js/app/admin.draft.mjs
@@ -0,0 +1,20 @@
+import { formatUtcTime } from '/js/app/utils.module.mjs'
+import { success } from '/js/app/blogtoast.module.mjs'
+
+function deletePost(postid) {
+    callApi(`/api/post/${postid}/recycle`, 'DELETE', {}, function (resp) {
+        success('Post deleted.');
+        document.querySelector(`#post-${postid}`).style.display = 'none';
+    });
+}
+
+document.querySelectorAll(".btn-delete").forEach(function (button) {
+    button.addEventListener("click", function () {
+        var cfm = confirm("Delete Confirmation?");
+        if (cfm) {
+            deletePost(this.dataset.postid);
+        }
+    });
+});
+
+formatUtcTime();
