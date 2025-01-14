defcd5a706675a6f57ae19d4049e20da998d775c
Edi Wang <Edi.Wang@outlook.com>
2025-01-14 14:21:07 +0800

    Refactor Mention.cshtml to use external JS module
    Replaced inline JavaScript in Mention.cshtml with an external module script `mention.mjs` for better maintainability and cache busting. Refactored `deleteMention` and `clearMention` function calls to use data attributes and IDs, respectively. Added event listeners in `mention.mjs` to handle button clicks and input filtering. Defined `deleteMention` and `clearMention` functions in `mention.mjs` to make API calls.

---
+++ src/Moonglade.Web/wwwroot/js/app/mention.mjs
@@ -0,0 +1,46 @@
+import { formatUtcTime } from './utils.module.mjs';
+import { success } from './blogtoast.module.mjs';
+
+function deleteMention(mentionId) {
+    callApi(`/api/mention/${mentionId}`, 'DELETE', {},
+        (resp) => {
+            document.querySelector(`#mention-box-${mentionId}`).remove();
+        });
+}
+
+function clearMention() {
+    callApi(`/api/mention/clear`, 'DELETE', {},
+        (resp) => {
+            success('Mention logs are cleared');
+            setTimeout(function () {
+                window.location.reload();
+            }, 800);
+        });
+}
+
+formatUtcTime();
+
+document.getElementById("mentionFilter").addEventListener("keyup", function () {
+    var value = this.value.toLowerCase();
+    var mentionItems = document.querySelectorAll(".mention-item-entry");
+
+    mentionItems.forEach(function (item) {
+        var text = item.textContent.toLowerCase();
+        item.style.display = text.indexOf(value) > -1 ? "" : "none";
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const exportButtons = document.querySelectorAll('.btn-delete');
+
+    exportButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const mentionId = button.getAttribute('data-mentionId');
+            deleteMention(mentionId);
+        });
+    });
+});
+
+document.getElementById('btn-clear-all').addEventListener('click', () => {
+    clearMention();
+});
