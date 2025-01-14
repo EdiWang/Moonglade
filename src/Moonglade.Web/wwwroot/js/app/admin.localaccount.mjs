3437ed3be43d4428d6a9cf051d4bafb3a889494a
Edi Wang <Edi.Wang@outlook.com>
2025-01-14 20:23:54 +0800

    Refactor reset password JS to external module
    Moved reset password functionality from inline script in `LocalAccount.cshtml` to `admin.localaccount.mjs`. Removed inline script and added a script tag for the external module with versioning. Functions `resetPassword` and `handleResetPasswordFormSubmit`, along with event listeners, are now in `admin.localaccount.mjs`. Imported `success` function from `blogtoast.module.mjs` for success messages. Changed `href` to `id` in reset password button for event listener.

---
+++ src/Moonglade.Web/wwwroot/js/app/admin.localaccount.mjs
@@ -0,0 +1,29 @@
+import { success } from '/js/app/blogtoast.module.mjs'
+
+const resetPasswordModal = new bootstrap.Modal('#resetPasswordModal');
+
+function resetPassword() {
+    document.querySelector('#resetpassword-form').reset();
+    resetPasswordModal.show();
+}
+
+function handleResetPasswordFormSubmit(event) {
+    event.preventDefault();
+
+    const data = new FormData(event.target);
+    const value = Object.fromEntries(data.entries());
+
+    callApi(`/api/settings/password/local`,
+        'PUT',
+        value,
+        (resp) => {
+            document.querySelector('#resetpassword-form').reset();
+            resetPasswordModal.hide();
+            success('Password updated.');
+        });
+}
+
+document.getElementById('btn-reset-password').addEventListener('click', resetPassword);
+
+const resetPasswordForm = document.querySelector('#resetpassword-form');
+resetPasswordForm.addEventListener('submit', handleResetPasswordFormSubmit);
