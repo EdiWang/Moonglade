a1b40d400e515df5333aa70e9dd459bd5385b055
Edi Wang <Edi.Wang@outlook.com>
2025-01-17 14:58:25 +0800

    Refactor Appearance.cshtml to use external JS module
    Replaced inline JavaScript in `Appearance.cshtml` with an external module script (`admin.settings.appearance.mjs`) to improve code organization and maintainability. The new script includes:
    
    - Import statements for `handleSettingsSubmit` and `success` functions.
    - A `handleSubmit` function for form submission.
    - Initialization of a Bootstrap modal for theme management.
    - `createTheme` and `deleteTheme` functions for API-based theme management.
    - Initialization of the CSS content editor using the Monaco editor.
    - An event listener for form submission.

---
+++ src/Moonglade.Web/wwwroot/js/app/admin.settings.appearance.mjs
@@ -0,0 +1,43 @@
+import { handleSettingsSubmit } from './admin.settings.js';
+import { success } from './toastService.mjs';
+
+function handleSubmit(event) {
+    assignEditorValues(cssContentEditor, "#ViewModel_CssCode");
+    handleSettingsSubmit(event);
+}
+
+var themeModal = new bootstrap.Modal(document.getElementById('thememodal'));
+
+window.createTheme = function (oFormElement) {
+    callApi(oFormElement.action,
+        'POST',
+        {
+            name: document.querySelector('#Name').value,
+            accentColor: document.querySelector('#AccentColor').value
+        },
+        async (resp) => {
+            themeModal.hide();
+            window.location.reload();
+        });
+}
+
+window.deleteTheme = function (id) {
+    callApi(`/api/theme/${id}`,
+        'DELETE',
+        {},
+        (resp) => {
+            var col = document.getElementById(`user-theme-col-${id}`);
+            col.remove();
+
+            success('Theme deleted.');
+        });
+}
+
+let cssContentEditor = null;
+
+require(['vs/editor/editor.main'], function () {
+    cssContentEditor = initEditor('CssContentEditor', "#ViewModel_CssCode", 'css');
+});
+
+const form = document.querySelector('#form-settings');
+form.addEventListener('submit', handleSubmit);
