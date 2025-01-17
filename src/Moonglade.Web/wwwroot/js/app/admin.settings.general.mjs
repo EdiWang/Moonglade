21c6514594f0d98f2ddcd3863e8a86b1e182ba27
Edi Wang <Edi.Wang@outlook.com>
2025-01-17 14:48:52 +0800

    Refactor: Move inline JS to external module script
    - Removed inline JavaScript from `<script type="module">` tag in `General.cshtml`.
    - Created `admin.settings.general.mjs` to contain the previously inline JavaScript.
    - Updated `General.cshtml` to reference the new `admin.settings.general.mjs` module script.
    - `admin.settings.general.mjs` imports `settings` module from `admin.settings.js` and initializes `ImageUploader` instances for avatar and site icon uploads.
    - Moved event listeners for avatar and site icon upload buttons, and form submission to `admin.settings.general.mjs`.

---
+++ src/Moonglade.Web/wwwroot/js/app/admin.settings.general.mjs
@@ -0,0 +1,19 @@
+import * as settings from '/js/app/admin.settings.js';
+
+var avatarUploader = new ImageUploader('avatar', 300, 'image/jpeg');
+avatarUploader.bindEvents();
+
+var siteiconUploader = new ImageUploader('siteicon', 512, 'image/png');
+siteiconUploader.bindEvents();
+
+bsCustomFileInput.init();
+document.getElementById('btn-upload-avatar').addEventListener('click', function () {
+    avatarUploader.uploadImage('/avatar');
+});
+
+document.getElementById('btn-upload-siteicon').addEventListener('click', function () {
+    siteiconUploader.uploadImage('/siteicon');
+});
+
+const form = document.querySelector('#form-settings');
+form.addEventListener('submit', settings.handleSettingsSubmit);
