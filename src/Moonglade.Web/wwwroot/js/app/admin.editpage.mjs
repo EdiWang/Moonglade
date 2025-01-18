7870895e439dfa7b5639a3be141f56ae61b4c5ea
Edi Wang <Edi.Wang@outlook.com>
2025-01-18 16:30:48 +0800

    Refactor EditPage.js and add meta tag for page-id
    Moved inline JS from EditPage.cshtml to admin.editpage.mjs.
    Added <meta> tag for page-id in EditPage.cshtml.
    Refactored JS to handle editors, form submission, and API calls.
    Imported utilities and toast service in the new module.
    Implemented keyboard shortcuts and event listeners.

---
+++ src/Moonglade.Web/wwwroot/js/app/admin.editpage.mjs
@@ -0,0 +1,103 @@
+import { parseMetaContent } from './utils.module.mjs'
+import { success } from './toastService.mjs'
+
+let isPreviewRequired = false;
+let pageId = parseMetaContent('page-id');
+const btnSubmitPage = '#btn-submit';
+
+let htmlContentEditor = null;
+let cssContentEditor = null;
+let hasCssEditorInitialized = false;
+
+require(['vs/editor/editor.main'], function () {
+    htmlContentEditor = initEditor('RawHtmlContentEditor', "#EditPageRequest_RawHtmlContent", 'html');
+});
+
+function assignEditorValues2() {
+    assignEditorValues(htmlContentEditor, "#EditPageRequest_RawHtmlContent")
+
+    // Edit Mode, preserve old value when user has not clicked on CSS tab
+    let oldCssValue = document.querySelector("#EditPageRequest_CssContent").value;
+    let cssValue = hasCssEditorInitialized ? cssContentEditor.getValue() : oldCssValue;
+
+    document.querySelector("#EditPageRequest_CssContent").value = cssValue;
+}
+
+function onPageCreateEditBegin() {
+    document.querySelector(btnSubmitPage).classList.add('disabled');
+    document.querySelector(btnSubmitPage).disabled = true;
+};
+
+function onPageCreateEditComplete() {
+    document.querySelector(btnSubmitPage).classList.remove('disabled');
+    document.querySelector(btnSubmitPage).removeAttribute('disabled');
+};
+
+function postCreateOrEdit() {
+    onPageCreateEditBegin();
+
+    const apiAddress = pageId == window.emptyGuid ? `/api/page` : `/api/page/${pageId}`;
+    const verb = pageId == window.emptyGuid ? 'POST' : 'PUT';
+
+    callApi(apiAddress, verb,
+        {
+            title: document.querySelector("#EditPageRequest_Title").value,
+            slug: document.querySelector("#EditPageRequest_Slug").value,
+            metaDescription: document.querySelector("#EditPageRequest_MetaDescription").value,
+            rawHtmlContent: document.querySelector("#EditPageRequest_RawHtmlContent").value,
+            cssContent: document.querySelector("#EditPageRequest_CssContent").value,
+            hideSidebar: document.querySelector('#EditPageRequest_HideSidebar').checked,
+            isPublished: document.querySelector('#EditPageRequest_IsPublished').checked
+        },
+        async (resp) => {
+            onPageCreateEditComplete();
+            const data = await resp.json();
+
+            if (data.pageId) {
+                pageId = data.pageId;
+                success('Page saved successfully.');
+
+                if (document.querySelector('#EditPageRequest_IsPublished').checked) {
+                    if (document.querySelector('#btn-preview')) {
+                        document.querySelector('#btn-preview').style.display = 'none';
+                    }
+                }
+
+                if (isPreviewRequired) {
+                    isPreviewRequired = false;
+                    window.open(`/admin/page/preview/${data.pageId}`);
+                }
+            }
+        });
+}
+
+document.querySelectorAll('a[data-bs-toggle="tab"]').forEach(function (element) {
+    element.addEventListener('shown.bs.tab', function (e) {
+        var isCssTab = e.target.id === "csscontent-tab";
+        if (isCssTab && !hasCssEditorInitialized) {
+            cssContentEditor = initEditor('CssContentEditor', "#EditPageRequest_CssContent", 'css');
+            hasCssEditorInitialized = true;
+        }
+    });
+});
+
+const handleKeyboardShortcuts = (event) => {
+    if ((event.ctrlKey || event.metaKey) && event.key.toLowerCase() === 's') {
+        event.preventDefault();
+        document.querySelector(btnSubmitPostSelector).click();
+    }
+};
+
+window.addEventListener('keydown', handleKeyboardShortcuts);
+
+function handleSubmit(e) {
+    e.preventDefault();
+
+    isPreviewRequired = e.submitter.id == 'btn-preview';
+    assignEditorValues2();
+
+    postCreateOrEdit();
+}
+
+const form = document.querySelector('#page-edit-form');
+form.addEventListener('submit', handleSubmit);
