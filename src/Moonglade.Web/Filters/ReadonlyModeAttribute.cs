263d62b38f5122be22bb09f74f1257eec274cdad
Edi Wang <Edi.Wang@outlook.com>
2025-01-20 10:32:00 +0800

    Add new endpoints and readonly mode attribute
    Updated AssetsController with Avatar and UpdateSiteIcon endpoints, both restricted by the new ReadonlyModeAttribute. Avatar endpoint handles POST requests to "avatar" and clears the blog cache, while UpdateSiteIcon handles POST requests to "siteicon". Added ReadonlyModeAttribute to enforce read-only mode restrictions.

---
+++ src/Moonglade.Web/Filters/ReadonlyModeAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Moonglade.Web.Filters;
+
+public class ReadonlyModeAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        if (Helper.GetAppDomainData<bool>("IsReadonlyMode"))
+        {
+            context.Result = new ForbidResult();
+        }
+    }
+}
