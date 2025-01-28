dcaea1076856dc00c3d395f59d5bb270d6da4373
Edi Wang <Edi.Wang@outlook.com>
2025-01-28 11:19:14 +0800

    Drop 'Revision' column and create 'PostView' table
    The code first checks if the column 'Revision' exists in the table '[dbo].[Post]'. If it does, the column is dropped.
    The code then checks if the table '[dbo].[PostView]' does not exist. If it does not exist, the code creates the table '[dbo].[PostView]' with the following columns:
      * 'PostId' of type 'uniqueidentifier' which is not nullable.
      * 'RequestCount' of type 'int' which is not nullable.
      * 'ViewCount' of type 'int' which is not nullable.
    The table '[dbo].[PostView]' is created with a primary key constraint on the 'PostId' column, and various table options are set for the primary key.

--- Deployment/mssql-migration.sql
+++ Deployment/mssql-migration.sql
@@ -144,3 +144,17 @@ IF EXISTS (
 BEGIN
     ALTER TABLE [dbo].[Post] DROP COLUMN Revision;
 END
+
+IF NOT EXISTS (SELECT * FROM sys.objects 
+               WHERE object_id = OBJECT_ID(N'[dbo].[PostView]') AND type in (N'U'))
+BEGIN
+	CREATE TABLE [dbo].[PostView](
+		[PostId] [uniqueidentifier] NOT NULL,
+		[RequestCount] [int] NOT NULL,
+		[ViewCount] [int] NOT NULL,
+	 CONSTRAINT [PK_PostView] PRIMARY KEY CLUSTERED 
+	(
+		[PostId] ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+	) ON [PRIMARY]
+END
