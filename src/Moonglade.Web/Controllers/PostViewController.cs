aacc308baa0bbe7cb93302036eb44793994bed49
Edi Wang <Edi.Wang@outlook.com>
2025-01-28 14:46:09 +0800

    Add PostViewController for API endpoints
    Introduce PostViewController in Moonglade.Web.Controllers.
    Decorate with [Route("api/[controller]")] and [ApiController].
    Inherit from ControllerBase for API functionality.
    Add using directives for ASP.NET Core support.

---
+++ src/Moonglade.Web/Controllers/PostViewController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Moonglade.Web.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PostViewController : ControllerBase
+{
+}
