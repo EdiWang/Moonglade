31f3e6e232e27c9ead6ca958f882c12eb008e014
Edi Wang <Edi.Wang@outlook.com>
2025-01-28 13:44:13 +0800

    Add command and handler for post request count
    Introduce AddRequestCountCommand and its handler in Moonglade.Core.PostFeature.
    - Define AddRequestCountCommand to handle PostId and implement IRequest<int>.
    - Implement AddRequestCountCommandHandler to manage request counts.
    - Use MoongladeRepository<PostViewEntity> for data operations.
    - Add logging for new requests and updates to request counts.

---
+++ src/Moonglade.Core/PostFeature/AddRequestCountCommand.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using Moonglade.Data;
+
+namespace Moonglade.Core.PostFeature;
+
+public record AddRequestCountCommand(Guid PostId) : IRequest<int>;
+
+public class AddRequestCountCommandHandler(
+    MoongladeRepository<PostViewEntity> postViewRepo,
+    ILogger<AddRequestCountCommandHandler> logger) : IRequestHandler<AddRequestCountCommand, int>
+{
+    public async Task<int> Handle(AddRequestCountCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await postViewRepo.GetByIdAsync(request.PostId);
+        if (entity is null)
+        {
+            entity = new PostViewEntity
+            {
+                PostId = request.PostId,
+                RequestCount = 1
+            };
+
+            await postViewRepo.AddAsync(entity);
+
+            logger.LogInformation("New request added for {PostId}", request.PostId);
+            return 1;
+        }
+
+        entity.RequestCount++;
+        await postViewRepo.UpdateAsync(entity);
+
+        logger.LogInformation("Request count updated for {PostId}, {RequestCount}", request.PostId, entity.RequestCount);
+
+        return entity.RequestCount;
+    }
+}
