0edbe0e1cec16ad671015bf4884592fdb0726fde
Edi Wang <Edi.Wang@outlook.com>
2025-01-28 14:44:08 +0800

    Add view count command handler for posts
    Introduce AddViewCountCommand and AddViewCountCommandHandler to handle view count updates for posts. Add necessary namespaces for logging, data access, and concurrency. Implement concurrency control using ConcurrentDictionary and SemaphoreSlim. Handle exceptions and log updates.

---
+++ src/Moonglade.Core/PostFeature/AddViewCountCommand.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Logging;
+using Moonglade.Data;
+using System.Collections.Concurrent;
+
+namespace Moonglade.Core.PostFeature;
+
+public record AddViewCountCommand(Guid PostId) : IRequest<int>;
+
+public class AddViewCountCommandHandler(
+    MoongladeRepository<PostViewEntity> postViewRepo,
+    ILogger<AddViewCountCommandHandler> logger) : IRequestHandler<AddViewCountCommand, int>
+{
+    // Ugly code to prevent race condition, which will make Moonglade a single instance application, shit
+    private static readonly ConcurrentDictionary<Guid, SemaphoreSlim> _locks = new();
+
+    public async Task<int> Handle(AddViewCountCommand request, CancellationToken cancellationToken)
+    {
+        var postLock = _locks.GetOrAdd(request.PostId, _ => new SemaphoreSlim(1, 1));
+        await postLock.WaitAsync(cancellationToken);
+
+        try
+        {
+            var entity = await postViewRepo.GetByIdAsync(request.PostId, cancellationToken);
+            if (entity is null) return 0;
+
+            entity.ViewCount++;
+            await postViewRepo.UpdateAsync(entity, cancellationToken);
+
+            logger.LogInformation("View count updated for {PostId}, {ViewCount}", request.PostId, entity.ViewCount);
+
+            return entity.ViewCount;
+        }
+        catch (Exception ex)
+        {
+            // Not fatal error, eat it and do not block application from running
+            logger.LogError(ex, "Failed to add view count for {PostId}", request.PostId);
+            return -1;
+        }
+        finally
+        {
+            postLock.Release();
+
+            if (postLock.CurrentCount == 1)
+            {
+                _locks.TryRemove(request.PostId, out _);
+            }
+        }
+    }
+}
