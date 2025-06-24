18ce07159920098193da7f1011dcf77d2411dc1d
Edi Wang <Edi.Wang@outlook.com>
2025-06-24 10:15:33 +0800

    Enhance post data export with additional properties
    Added properties `RouteLink`, `HeroImageUrl`, `LastModifiedUtc`, `ScheduledPublishTimeUtc`, `IsFeatured`, and `IsOutdated` to the exported data structure in the `Handle` method of `ExportPostDataCommandHandler`. This improves the comprehensiveness of the post data export, providing more context and metadata for each post.

--- src/Moonglade.Data/Exporting/ExportPostDataCommand.cs
+++ src/Moonglade.Data/Exporting/ExportPostDataCommand.cs
@@ -12,17 +12,25 @@ public class ExportPostDataCommandHandler(MoongladeRepository<PostEntity> repo) 
         var exporter = new ZippedJsonExporter<PostEntity>(repo, "moonglade-posts", Path.GetTempPath());
         var data = exporter.ExportData(p => new
         {
+            p.Id,
             p.Title,
             p.Slug,
+            p.RouteLink,
+            p.Author,
             p.ContentAbstract,
             p.PostContent,
+            p.HeroImageUrl,
             p.CreateTimeUtc,
+            p.LastModifiedUtc,
+            p.ScheduledPublishTimeUtc,
             p.CommentEnabled,
             p.PubDateUtc,
             p.ContentLanguageCode,
             p.IsDeleted,
             p.IsFeedIncluded,
+            p.IsFeatured,
             p.PostStatus,
+            p.IsOutdated,
             Categories = p.PostCategory.Select(pc => pc.Category.DisplayName),
             Tags = p.Tags.Select(pt => pt.DisplayName)
         }, ct);
