25abd0b8245d6967acaa5f6fcbae0981b71d5ec9
Edi Wang <Edi.Wang@outlook.com>
2025-06-24 12:55:14 +0800

    Refactor post management into RecycleBinController
    Moved `Restore`, `DeleteFromRecycleBin`, and `EmptyRecycleBin` methods from `PostController` to a new `RecycleBinController`. This change organizes recycle bin operations under a dedicated controller and includes necessary attributes for authorization and routing.

---
+++ src/Moonglade.Web/Controllers/RecycleBinController.cs
@@ -0,0 +1,44 @@
+﻿using Moonglade.Core.PostFeature;
+using Moonglade.Web.Attributes;
+
+namespace Moonglade.Web.Controllers;
+
+[Authorize]
+[Route("api/post")]
+[ApiController]
+public class RecycleBinController(IMediator mediator) : ControllerBase
+{
+    [TypeFilter(typeof(ClearBlogCache), Arguments =
+    [
+        BlogCacheType.SiteMap |
+        BlogCacheType.Subscription
+    ])]
+    [HttpPost("{postId:guid}/restore")]
+    [ReadonlyMode]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> Restore([NotEmpty] Guid postId)
+    {
+        await mediator.Send(new RestorePostCommand(postId));
+        return NoContent();
+    }
+
+    [TypeFilter(typeof(ClearBlogCache), Arguments = [BlogCacheType.Subscription | BlogCacheType.SiteMap])]
+    [HttpDelete("{postId:guid}/destroy")]
+    [ReadonlyMode]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> DeleteFromRecycleBin([NotEmpty] Guid postId)
+    {
+        await mediator.Send(new DeletePostCommand(postId));
+        return NoContent();
+    }
+
+    [TypeFilter(typeof(ClearBlogCache), Arguments = [BlogCacheType.Subscription | BlogCacheType.SiteMap])]
+    [HttpDelete("recyclebin")]
+    [ReadonlyMode]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> EmptyRecycleBin()
+    {
+        await mediator.Send(new EmptyRecycleBinCommand());
+        return NoContent();
+    }
+}
