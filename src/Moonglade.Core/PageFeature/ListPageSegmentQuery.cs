24e897e631a3c6d568502c20f0a1a8cf45576783
Edi Wang <Edi.Wang@outlook.com>
2025-07-16 10:46:06 +0800

    Refactor ListPageSegmentQuery to use LiteBus IQuery
    Updated ListPageSegmentQuery and its handler to implement LiteBus IQuery and IQueryHandler interfaces instead of IRequest and IRequestHandler. Changed method names to align with LiteBus conventions. Updated BlogPage.cshtml to use QueryMediator.QueryAsync for querying page segments.

--- src/Moonglade.Core/PageFeature/ListPageSegmentQuery.cs
+++ src/Moonglade.Core/PageFeature/ListPageSegmentQuery.cs
@@ -1,12 +1,13 @@
-﻿using Moonglade.Data;
+﻿using LiteBus.Queries.Abstractions;
+using Moonglade.Data;
 using Moonglade.Data.Specifications;
 
 namespace Moonglade.Core.PageFeature;
 
-public record ListPageSegmentQuery : IRequest<List<PageSegment>>;
+public record ListPageSegmentQuery : IQuery<List<PageSegment>>;
 
-public class ListPageSegmentQueryHandler(MoongladeRepository<PageEntity> repo) : IRequestHandler<ListPageSegmentQuery, List<PageSegment>>
+public class ListPageSegmentQueryHandler(MoongladeRepository<PageEntity> repo) : IQueryHandler<ListPageSegmentQuery, List<PageSegment>>
 {
-    public Task<List<PageSegment>> Handle(ListPageSegmentQuery request, CancellationToken ct) =>
+    public Task<List<PageSegment>> HandleAsync(ListPageSegmentQuery request, CancellationToken ct) =>
         repo.ListAsync(new PageSegmentSpec(), ct);
 }
