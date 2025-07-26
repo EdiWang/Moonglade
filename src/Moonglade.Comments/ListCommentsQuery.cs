87b62d63ebcb8609ce22971d8c9239489a118abd
Edi Wang <Edi.Wang@outlook.com>
2025-07-26 13:52:54 +0800

    Rename GetCommentsQuery to ListCommentsQuery
    Refactored GetCommentsQuery and its handler to ListCommentsQuery for improved clarity and consistency. Updated usage in Comments.cshtml to reflect the new naming.

--- src/Moonglade.Comments/ListCommentsQuery.cs
+++ src/Moonglade.Comments/ListCommentsQuery.cs
@@ -5,11 +5,11 @@ using Moonglade.Data.Specifications;
 
 namespace Moonglade.Comments;
 
-public record GetCommentsQuery(int PageSize, int PageIndex) : IQuery<List<CommentDetailedItem>>;
+public record ListCommentsQuery(int PageSize, int PageIndex) : IQuery<List<CommentDetailedItem>>;
 
-public class GetCommentsQueryHandler(MoongladeRepository<CommentEntity> repo) : IQueryHandler<GetCommentsQuery, List<CommentDetailedItem>>
+public class ListCommentsQueryHandler(MoongladeRepository<CommentEntity> repo) : IQueryHandler<ListCommentsQuery, List<CommentDetailedItem>>
 {
-    public Task<List<CommentDetailedItem>> HandleAsync(GetCommentsQuery request, CancellationToken ct)
+    public Task<List<CommentDetailedItem>> HandleAsync(ListCommentsQuery request, CancellationToken ct)
     {
         var spec = new CommentPagingSepc(request.PageSize, request.PageIndex);
         var comments = repo.SelectAsync(spec, CommentDetailedItem.EntitySelector, ct);
