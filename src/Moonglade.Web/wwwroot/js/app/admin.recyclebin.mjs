71b08f81f8c99be8bcd4fda4298ff961e944beae
Edi Wang <Edi.Wang@outlook.com>
2025-08-01 09:06:07 +0800

    Refactor recycle bin JS to use moongladeFetch2 and async/await
    Replaces moongladeFetch with moongladeFetch2 and updates related functions to use async/await for better readability and consistency. Event handlers for delete, restore, and empty recycle bin actions are now asynchronous.

--- src/Moonglade.Web/wwwroot/js/app/admin.recyclebin.mjs
+++ src/Moonglade.Web/wwwroot/js/app/admin.recyclebin.mjs
@@ -1,46 +1,44 @@
-import { moongladeFetch } from './httpService.mjs'
+import { moongladeFetch2 } from './httpService.mjs'
 import { formatUtcTime } from './utils.module.mjs'
 import { success } from './toastService.mjs'
 
-function deletePost(postid) {
-    moongladeFetch(`/api/post/${postid}/destroy`, 'DELETE', {},
-        (resp) => {
-            document.querySelector(`#post-${postid}`).remove();
-            success('Post deleted');
-        });
+async function deletePost(postid) {
+    await moongladeFetch2(`/api/post/${postid}/destroy`, 'DELETE', {});
+
+    document.querySelector(`#post-${postid}`).remove();
+    success('Post deleted');
 }
 
-function restorePost(postid) {
-    moongladeFetch(`/api/post/${postid}/restore`, 'POST', {},
-        (resp) => {
-            document.querySelector(`#post-${postid}`).remove();
-            success('Post restored');
-        });
+async function restorePost(postid) {
+    await moongladeFetch2(`/api/post/${postid}/restore`, 'POST', {});
+
+    document.querySelector(`#post-${postid}`).remove();
+    success('Post restored');
 }
 
 document.querySelectorAll('.btn-delete').forEach(function (button) {
-    button.addEventListener('click', function () {
+    button.addEventListener('click', async function () {
         var cfm = confirm('Delete Confirmation?');
         if (cfm) {
-            deletePost(this.getAttribute('data-postid'));
+            await deletePost(this.getAttribute('data-postid'));
         }
     });
 });
 
 document.querySelectorAll('.btn-restore').forEach(function (button) {
-    button.addEventListener('click', function () {
-        restorePost(this.getAttribute('data-postid'));
+    button.addEventListener('click', async function () {
+        await restorePost(this.getAttribute('data-postid'));
     });
 });
 
 document.querySelectorAll('.btn-empty-recbin').forEach(function (button) {
-    button.addEventListener('click', function () {
-        moongladeFetch('/api/post/recyclebin', 'DELETE', {}, function (resp) {
-            success('Cleared');
-            setTimeout(function () {
-                window.location.reload();
-            }, 800);
-        });
+    button.addEventListener('click', async function () {
+        await moongladeFetch2('/api/post/recyclebin', 'DELETE', {});
+
+        success('Cleared');
+        setTimeout(function () {
+            window.location.reload();
+        }, 800);
     });
 });
 
