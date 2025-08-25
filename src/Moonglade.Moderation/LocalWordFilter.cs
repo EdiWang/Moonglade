a74bf752c0196e0eee05b3417c77a5b488782680
Edi Wang <Edi.Wang@outlook.com>
2025-08-25 09:29:31 +0800

    Add LocalWordFilter for content moderation
    Introduces the LocalWordFilter class using Edi.WordFilter to detect and mask inappropriate words. Updates the project file to include Edi.WordFilter as a dependency.

---
+++ src/Moonglade.Moderation/LocalWordFilter.cs
@@ -0,0 +1,20 @@
+﻿using Edi.WordFilter;
+
+namespace Moonglade.Moderation;
+
+public class LocalWordFilter
+{
+    private readonly IMaskWordFilter _filter;
+
+    public LocalWordFilter(string words)
+    {
+        var sw = new StringWordSource(words);
+
+        IMaskWordFilter filter = new TrieTreeWordFilter(sw);
+        _filter = filter;
+    }
+
+    public string ModerateContent(string input) => _filter.FilterContent(input);
+
+    public bool HasBadWord(params string[] input) => input.Any(s => _filter.ContainsAnyWord(s));
+}
