075073ddfc4c4d53dbdd5cae51ef164b45036b97
Edi Wang <Edi.Wang@outlook.com>
2025-10-05 14:07:05 +0800

    Move PostEntityToSegmentSpec to its own file
    Extracted PostEntityToSegmentSpec from PostPagingSpec.cs into a new file PostEntityToSegmentSpec.cs for better separation of concerns and code organization.

--- src/Moonglade.Data/Specifications/PostPagingSpec.cs
+++ src/Moonglade.Data/Specifications/PostPagingSpec.cs
@@ -1,5 +1,4 @@
-﻿using Moonglade.Data.DTO;
-using Moonglade.Data.Entities;
+﻿using Moonglade.Data.Entities;
 
 namespace Moonglade.Data.Specifications;
 
@@ -46,27 +45,5 @@ public sealed class PostPagingByStatusSpec : Specification<PostEntity>
             Query.Skip(offset).Take(pageSize);
             Query.OrderByDescending(p => p.PubDateUtc);
         }
-    }
-}
-
-public class PostEntityToSegmentSpec : Specification<PostEntity, PostSegment>
-{
-    public PostEntityToSegmentSpec()
-    {
-        Query.Select(p => new()
-        {
-            Id = p.Id,
-            Title = p.Title,
-            Slug = p.Slug,
-            PubDateUtc = p.PubDateUtc,
-            PostStatus = p.PostStatus,
-            IsFeatured = p.IsFeatured,
-            IsDeleted = p.IsDeleted,
-            IsOutdated = p.IsOutdated,
-            CreateTimeUtc = p.CreateTimeUtc,
-            LastModifiedUtc = p.LastModifiedUtc,
-            ScheduledPublishTimeUtc = p.ScheduledPublishTimeUtc,
-            ContentAbstract = p.ContentAbstract
-        });
     }
 }
