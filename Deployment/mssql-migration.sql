dd7b8c488ab89ddc07aca5d1a72e7045abb87264
Edi Wang <Edi.Wang@outlook.com>
2025-12-02 14:55:57 +0800

    Add Widget and WidgetLinkItem tables
    Introduces new Widget and WidgetLinkItem tables to support widget functionality. Includes foreign key constraint from WidgetLinkItem to Widget with cascade delete.

--- Deployment/mssql-migration.sql
+++ Deployment/mssql-migration.sql
@@ -53,3 +53,42 @@ BEGIN
     ALTER TABLE dbo.Post ADD Keywords NVARCHAR(256) NULL;
 END
 GO
+
+-- v15.0
+CREATE TABLE [Widget](
+	[Id] [uniqueidentifier] NOT NULL,
+	[Title] [nvarchar](100) NOT NULL,
+	[WidgetType] [nvarchar](50) NOT NULL,
+	[DisplayOrder] [int] NOT NULL,
+	[IsEnabled] [bit] NOT NULL,
+	[CreatedTimeUtc] [datetime] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [WidgetLinkItem](
+	[Id] [uniqueidentifier] NOT NULL,
+	[WidgetId] [uniqueidentifier] NOT NULL,
+	[Title] [nvarchar](100) NOT NULL,
+	[Url] [nvarchar](500) NOT NULL,
+	[IconName] [nvarchar](50) NULL,
+	[OpenInNewWindow] [bit] NOT NULL,
+	[DisplayOrder] [int] NOT NULL,
+	[IsEnabled] [bit] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [WidgetLinkItem]  WITH CHECK ADD  CONSTRAINT [FK_WidgetLinkItem_Widget] FOREIGN KEY([WidgetId])
+REFERENCES [Widget] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [WidgetLinkItem] CHECK CONSTRAINT [FK_WidgetLinkItem_Widget]
+GO
