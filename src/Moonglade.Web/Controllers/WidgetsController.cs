7822d75ce6fd6ab797ecf640e8740d39f227ce64
Edi Wang <Edi.Wang@outlook.com>
2025-12-02 14:48:40 +0800

    Add WidgetsController with GET endpoint
    Introduces WidgetsController to handle API requests for widgets. Adds a GET endpoint to retrieve a widget by its GUID, returning 200 OK with the widget or 404 Not Found if not present.

---
+++ src/Moonglade.Web/Controllers/WidgetsController.cs
@@ -0,0 +1,27 @@
+﻿using LiteBus.Commands.Abstractions;
+using LiteBus.Queries.Abstractions;
+using Moonglade.Data.Entities;
+using Moonglade.Web.Attributes;
+using Moonglade.Widgets;
+
+namespace Moonglade.Web.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class WidgetsController(
+    ICacheAside cache,
+    IQueryMediator queryMediator,
+    ICommandMediator commandMediator) : ControllerBase
+{
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType<WidgetEntity>(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Get([NotEmpty] Guid id)
+    {
+        var widget = await queryMediator.QueryAsync(new GetWidgetQuery(id));
+        if (null == widget) return NotFound();
+
+        return Ok(widget);
+    }
+}
