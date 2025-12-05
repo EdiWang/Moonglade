bb9a912a7b08228b5805442d3c34adf7c9526703
Edi Wang <Edi.Wang@outlook.com>
2025-12-05 20:45:34 +0800

    Add CRUD endpoints for widget link items
    Introduces Create, Read, Update, and Delete API endpoints for managing widget link items in WidgetsController. These endpoints support operations on link items associated with widgets, improving widget extensibility and management.

--- src/Moonglade.Web/Controllers/WidgetsController.cs
+++ src/Moonglade.Web/Controllers/WidgetsController.cs
@@ -1,8 +1,10 @@
 ﻿using LiteBus.Commands.Abstractions;
 using LiteBus.Queries.Abstractions;
 using Moonglade.Data.Entities;
+using Moonglade.Utils;
 using Moonglade.Web.Attributes;
 using Moonglade.Widgets;
+using Moonglade.Widgets.Types.LinkList;
 
 namespace Moonglade.Web.Controllers;
 
@@ -14,6 +16,8 @@ public class WidgetsController(
     IQueryMediator queryMediator,
     ICommandMediator commandMediator) : ControllerBase
 {
+    #region Widget CRUD Operations
+
     [HttpGet("{id:guid}")]
     [ProducesResponseType<WidgetEntity>(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -57,4 +61,56 @@ public class WidgetsController(
         await commandMediator.SendAsync(new DeleteWidgetCommand(id));
         return NoContent();
     }
+
+    #endregion
+
+    #region Widget Link Item CRUD Operations
+
+    [HttpPost("linkitem")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> CreateLinkItem(EditWidgetLinkItemRequest request)
+    {
+        var result = await commandMediator.SendAsync(new CreateWidgetLinkItemCommand(request));
+
+        if (result == OperationCode.ObjectNotFound)
+        {
+            return NotFound($"Widget with ID {request.WidgetId} not found");
+        }
+
+        return Created();
+    }
+
+    [HttpGet("linkitem/list/{widgetId:guid}")]
+    [ProducesResponseType<List<WidgetLinkItemEntity>>(StatusCodes.Status200OK)]
+    public async Task<IActionResult> ListLinkItems([NotEmpty] Guid widgetId)
+    {
+        var list = await queryMediator.QueryAsync(new ListWidgetLinkItemsByWidgetIdQuery(widgetId));
+        return Ok(list);
+    }
+
+    [HttpPut("linkitem/{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> UpdateLinkItem([NotEmpty] Guid id, EditWidgetLinkItemRequest request)
+    {
+        var result = await commandMediator.SendAsync(new UpdateWidgetLinkItemCommand(id, request));
+
+        if (result == OperationCode.ObjectNotFound)
+        {
+            return NotFound($"Widget link item with ID {id} not found");
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("linkitem/{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> DeleteLinkItem([NotEmpty] Guid id)
+    {
+        await commandMediator.SendAsync(new DeleteWidgetLinkItemCommand(id));
+        return NoContent();
+    }
+
+    #endregion
 }
