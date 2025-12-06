ca4f999f54ca816c03a9d2c788ea2b2c0ad12997
Edi Wang <Edi.Wang@outlook.com>
2025-12-06 14:39:54 +0800

    Add caching to WidgetsViewComponent
    Introduced ICacheAside and IConfiguration dependencies to WidgetsViewComponent. Widget data is now cached using a configurable expiration time, improving performance by reducing repeated queries.

--- src/Moonglade.Web/ViewComponents/WidgetsViewComponent.cs
+++ src/Moonglade.Web/ViewComponents/WidgetsViewComponent.cs
@@ -3,11 +3,22 @@ using Moonglade.Widgets;
 
 namespace Moonglade.Web.ViewComponents;
 
-public class WidgetsViewComponent(IQueryMediator queryMediator) : ViewComponent
+public class WidgetsViewComponent(
+    IQueryMediator queryMediator,
+    ICacheAside cache,
+    IConfiguration configuration) : ViewComponent
 {
     public async Task<IViewComponentResult> InvokeAsync()
     {
-        var widgets = await queryMediator.QueryAsync(new ListWidgetsQuery());
+        var widgets = await cache.GetOrCreateAsync(BlogCachePartition.General.ToString(), "widgets", async p =>
+        {
+            var exp = int.Parse(configuration["WidgetCacheMinutes"]);
+            p.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(exp);
+
+            var data = await queryMediator.QueryAsync(new ListWidgetsQuery());
+            return data;
+        });
+
         return View(widgets);
     }
 }
