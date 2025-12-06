5f0cd6f221c51a0018b062242bf2678601e1c94b
Edi Wang <Edi.Wang@outlook.com>
2025-12-06 13:45:11 +0800

    Add widget support with link list to sidebar
    Introduced a new WidgetsViewComponent and Default.cshtml view to render widgets in the sidebar, including support for link list widgets using the new LinkListItem type. Updated _Aside.cshtml to invoke the new component.

---
+++ src/Moonglade.Web/ViewComponents/WidgetsViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using LiteBus.Queries.Abstractions;
+using Moonglade.Widgets;
+
+namespace Moonglade.Web.ViewComponents;
+
+public class WidgetsViewComponent(IQueryMediator queryMediator) : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var widgets = await queryMediator.QueryAsync(new ListWidgetsQuery());
+        return View(widgets);
+    }
+}
---
+++ src/Moonglade.Widgets/Types/LinkListItem.cs
@@ -0,0 +1,14 @@
+﻿namespace Moonglade.Widgets.Types;
+
+public class LinkListItem
+{
+    public string Name { get; set; }
+
+    public string Icon { get; set; }
+
+    public string Url { get; set; }
+
+    public bool OpenInNewTab { get; set; }
+
+    public int Order { get; set; }
+}
