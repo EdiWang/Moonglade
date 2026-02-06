64abe2f03f0745dfb46b59fb0093c1e36337ee32
Edi Wang <Edi.Wang@outlook.com>
2026-02-06 10:58:08 +0800

    Use IRepositoryBase for widget handlers
    Replace concrete MoongladeRepository<WidgetEntity> with the IRepositoryBase<WidgetEntity> abstraction across Create, Delete, Update handlers and List/Get query handlers to decouple handlers from a specific repository implementation. Remove now-unused Moonglade.Data usings from query/command files. Add an Ardalis.Specification Using entry to the project file.

--- src/Moonglade.Widgets/ListWidgetsQuery.cs
+++ src/Moonglade.Widgets/ListWidgetsQuery.cs
@@ -1,12 +1,11 @@
 using LiteBus.Queries.Abstractions;
-using Moonglade.Data;
 using Moonglade.Data.Entities;
 
 namespace Moonglade.Widgets;
 
 public record ListWidgetsQuery : IQuery<List<WidgetEntity>>;
 
-public class ListWidgetsQueryHandler(MoongladeRepository<WidgetEntity> repo) : IQueryHandler<ListWidgetsQuery, List<WidgetEntity>>
+public class ListWidgetsQueryHandler(IRepositoryBase<WidgetEntity> repo) : IQueryHandler<ListWidgetsQuery, List<WidgetEntity>>
 {
     public Task<List<WidgetEntity>> HandleAsync(ListWidgetsQuery request, CancellationToken ct) => repo.ListAsync(ct);
 }
