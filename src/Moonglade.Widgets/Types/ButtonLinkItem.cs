f9fa96c620632b09b5f1292db274f8e7b96b7bd4
Edi Wang <Edi.Wang@outlook.com>
2026-02-20 18:32:48 +0800

    Create ButtonLinkItem.cs

---
+++ src/Moonglade.Widgets/Types/ButtonLinkItem.cs
@@ -0,0 +1,12 @@
+namespace Moonglade.Widgets.Types;
+
+public class ButtonLinkItem
+{
+    public string Text { get; set; }
+
+    public string Url { get; set; }
+
+    public string CssClass { get; set; }
+
+    public bool OpenInNewTab { get; set; }
+}
